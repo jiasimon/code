474190c9d1ccbbd834e6fbee13386020f33bfeac
Simon Jia <sjia@nisum.com>
2020-07-12 09:39:27 -0700

    Leetcode #23 divide and conquer, from K to 2

--- src/main/java/com/sjia/Leetcode/MergeTwoSortedLists.java
+++ src/main/java/com/sjia/Leetcode/MergeTwoSortedLists.java
@@ -32,6 +32,18 @@ public class MergeTwoSortedLists {
 
     }*/
 
+/*    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1,l2.next);
+            return l2;
+        }*/
+
+
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null || (l2 != null && l1.val > l2.val)) {
