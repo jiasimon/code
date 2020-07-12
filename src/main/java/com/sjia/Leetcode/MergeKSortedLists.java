e5be44b2764b32053d718bc0058caa51718d3a29
Simon Jia <sjia@nisum.com>
2020-07-12 10:32:18 -0700

    Leetcode #23 Merge k Sorted Lists, priority queue

--- src/main/java/com/sjia/Leetcode/MergeKSortedLists.java
+++ src/main/java/com/sjia/Leetcode/MergeKSortedLists.java
@@ -7,6 +7,9 @@ public class MergeKSortedLists {
     // Runtime: 3 ms, faster than 85.34% of Java online submissions for Merge k Sorted Lists.
     // Memory Usage: 45.1 MB, less than 5.86% of Java online submissions for Merge k Sorted Lists.
 
+    // PriorityQueue Runtime: 4 ms, faster than 81.92% of Java online submissions for Merge k Sorted Lists.
+    //Memory Usage: 41.6 MB, less than 36.78% of Java online submissions for Merge k Sorted Lists.
+
 
     public class ListNode {
         int val;
@@ -28,7 +31,7 @@ public class MergeKSortedLists {
         ListNode l2 = merge(lists, mid + 1, right);
         return mergeTwoLists(l1, l2);
     }
-
+    
     private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null) return l2;
         if (l2 == null) return l1;
@@ -36,9 +39,36 @@ public class MergeKSortedLists {
             l1.next = mergeTwoLists(l1.next, l2);
             return l1;
         } else {
-            l2.next = mergeTwoLists(l1,l2.next);
+            l2.next = mergeTwoLists(l1, l2.next);
             return l2;
         }
     }
+
+
+ /*       public ListNode mergeKLists(ListNode[] lists) {
+            if (lists == null || lists.length == 0) return null;
+            PriorityQueue<ListNode> queue = new PriorityQueue<>(lists.length, new Comparator<ListNode>() {
+                @Override
+                public int compare(ListNode o1, ListNode o2) {
+                    if (o1.val < o2.val) return -1;
+                    else if (o1.val == o2.val) return 0;
+                    else return 1;
+                }
+            });
+
+            ListNode dummy = new ListNode(0);
+            ListNode p = dummy;
+            for (ListNode node : lists) {
+                if (node != null) queue.add(node);
+            }
+            while (!queue.isEmpty()) {
+                p.next = queue.poll();
+                p = p.next;
+                if (p.next != null) queue.add(p.next);
+            }
+            return dummy.next;
+        }*/
+
+
 
 }
