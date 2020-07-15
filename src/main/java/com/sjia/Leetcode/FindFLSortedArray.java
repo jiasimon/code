cf641cfadb610bb0fbf4e10d6b5b2a18aba4695e
Simon Jia <sjia@nisum.com>
2020-07-15 16:04:13 -0700

    Leetcode #34 First and Last Position

---
+++ src/main/java/com/sjia/Leetcode/FindFLSortedArray.java
@@ -0,0 +1,66 @@
+package com.sjia.Leetcode;
+
+import java.util.Arrays;
+
+public class FindFLSortedArray {
+
+    // #34 https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+    // Runtime: 0 ms, faster than 100.00% of Java online submissions for Find First and Last Position of Element in Sorted Array.
+    //Memory Usage: 42.6 MB, less than 72.07% of Java online submissions for Find First and Last Position of Element in Sorted Array.
+
+    // Need follow up
+
+
+    static int[] searchRange(int[] nums, int target) {
+        int [] result = {-1 ,-1};
+        int [] single = {1,1};
+        if (nums.length == 0) return result;
+        if (nums.length == 1) return nums[0] == target ? single:result;
+        int l = 0, r = nums[nums.length-1];
+
+        int left = twoDivide(nums, target, true);
+        if (left == nums.length || nums[left] != target) {
+            return result;
+        }
+
+        result[0] = left;
+        result[1] = twoDivide(nums, target, false)-1;
+
+        return result;
+
+        }
+
+
+        static int twoDivide(int[] nums, int target, boolean flag) {
+            int lo = 0;
+            int hi = nums.length;
+
+            while (lo < hi) {
+                int mid = (lo + hi) / 2;
+                if (nums[mid] > target || (flag && target == nums[mid])) {
+                    hi = mid;
+                }
+                else {
+                    lo = mid+1;
+                }
+            }
+
+            return lo;
+        }
+
+
+
+
+    }
+
+
+    public static void main(String[] args) {
+        int[] testData = {5,7,7,8,8,10};
+        int target = 8;
+        int[] result = searchRange(testData, target);
+        System.out.println("result is: " + Arrays.toString(result));
+    }
+
+
+}
