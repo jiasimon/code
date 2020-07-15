bfc82a823fd698b708c7ca120e31464b9cceb7a1
Simon Jia <sjia@nisum.com>
2020-07-15 08:56:20 -0700

    Leetcode #32 Longest Valid Parentheses  Stack

--- src/main/java/com/sjia/Leetcode/LongestValidParentheses.java
+++ src/main/java/com/sjia/Leetcode/LongestValidParentheses.java
@@ -1,5 +1,7 @@
 package com.sjia.Leetcode;
 
+import java.util.Stack;
+
 public class LongestValidParentheses {
 
     //#32 https://leetcode.com/problems/longest-valid-parentheses/
@@ -11,6 +13,7 @@ public class LongestValidParentheses {
     // Runtime: 2 ms, faster than 75.83% of Java online submissions for Longest Valid Parentheses.
     //Memory Usage: 38.3 MB, less than 89.24% of Java online submissions for Longest Valid Parentheses.
 
+/*
     static int longestValidParentheses(String s) {
         int left = 0, right = 0, maxlength = 0;
         for (int i = 0; i < s.length(); i++) {
@@ -40,7 +43,32 @@ public class LongestValidParentheses {
             }
         }
         return maxlength;
+    }*/
+
+
+    // Stack solution
+    // Runtime: 7 ms, faster than 16.34% of Java online submissions for Longest Valid Parentheses.
+    //Memory Usage: 40.7 MB, less than 7.76% of Java online submissions for Longest Valid Parentheses.
+
+    static int longestValidParentheses(String s) {
+        int maxans = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.empty()) {
+                    stack.push(i);
+                } else {
+                    maxans = Math.max(maxans, i - stack.peek());
+                }
+            }
+        }
+        return maxans;
     }
+
 
     public static void main(String[] args) {
         // int range:  -2147483648～2147483647
