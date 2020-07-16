aac4ea7fd3a34ff6984b43b65ee48806f4761e0d
Simon Jia <sjia@nisum.com>
2020-07-15 21:20:17 -0700

    Leetcode #35 Search Insert Position,  memory still high

--- src/main/java/com/sjia/Leetcode/SearchInsertPosition.java
+++ src/main/java/com/sjia/Leetcode/SearchInsertPosition.java
@@ -13,18 +13,34 @@ public class SearchInsertPosition {
     // second run Runtime: 0 ms, faster than 100.00% of Java online submissions for Search Insert Position.
     //Memory Usage: 40.9 MB, less than 9.35% of Java online submissions for Search Insert Position.
 
+/*
     static int searchInsert(int[] nums, int target) {
         for (int i = 0; i< nums.length ; i++) {
             if (nums[i] >= target) return i;
         }
         return nums.length;
-    }
+    }*/
 
+
+    // Runtime: 0 ms, faster than 100.00% of Java online submissions for Search Insert Position.
+    //Memory Usage: 41 MB, less than 7.61% of Java online submissions for Search Insert Position.
+
+    static int searchInsert(int[] nums, int target) {
+        if (nums.length == 0 ) return 0;
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right-left)/2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] > target) right = mid-1;
+            if (nums[mid] < target) left = mid+1;
+        }
+        return left;
+    }
 
 
     public static void main(String[] args) {
         int[] testData = {1,3,5,6};
-        int target = 4;
+        int target = 3;
         int result = searchInsert(testData, target);
         System.out.printf( "Number %d insert at : %d ", target, result);
     }
