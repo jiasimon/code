ff19becdf03082fce24068e6c001aa60b733af9f
Simon Jia <sjia@nisum.com>
2020-07-18 10:06:59 -0700

    Leetcode #125 PalindromeBreak toCharArray

--- src/main/java/com/sjia/Leetcode/PalindromeBreak.java
+++ src/main/java/com/sjia/Leetcode/PalindromeBreak.java
@@ -7,6 +7,7 @@ public class PalindromeBreak {
     //Runtime: 4 ms, faster than 12.96% of Java online submissions for Break a Palindrome.
     //Memory Usage: 39.2 MB, less than 7.77% of Java online submissions for Break a Palindrome.
 
+/*
     static String breakPalindrome(String palindrome) {
         if (palindrome.length()<=1) return "";
         int N = palindrome.length();
@@ -22,7 +23,29 @@ public class PalindromeBreak {
             }
         }
         return result;
+    }*/
+
+
+    // Runtime: 0 ms, faster than 100.00% of Java online submissions for Break a Palindrome.
+    //Memory Usage: 37.2 MB, less than 88.59% of Java online submissions for Break a Palindrome.
+
+    static String breakPalindrome(String palindrome) {
+        if (palindrome.length()<=1) return "";
+        int N = palindrome.length();
+        char[] s = palindrome.toCharArray();
+        for (int i=0; i<=N/2; i++) {
+            if(i==N/2) {
+                s[N-1] = 'b';
+                break;
+            }
+            if (palindrome.charAt(i) != 'a') {
+                s[i] = 'a';
+                break;
+            }
+        }
+        return String.valueOf(s);
     }
+
 
 
     public static void main(String[] args) {
