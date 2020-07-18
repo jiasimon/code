e34812afc45637e78de3aac8aae81ef9d3b6e803
Simon Jia <sjia@nisum.com>
2020-07-18 15:21:24 -0700

    Leetcode #214 Palindrome Shortest, KMP Next table

--- src/main/java/com/sjia/Leetcode/PalindromeShortest.java
+++ src/main/java/com/sjia/Leetcode/PalindromeShortest.java
@@ -12,7 +12,7 @@ public class PalindromeShortest {
 
     // second run, Runtime: 213 ms, faster than 33.64% of Java online submissions for Shortest Palindrome.
     //Memory Usage: 40.2 MB, less than 17.58% of Java online submissions for Shortest Palindrome.
-
+/*
     static String shortestPalindrome(String s) {
         String reverse = new StringBuilder(s).reverse().toString();
         int n = s.length();
@@ -22,13 +22,47 @@ public class PalindromeShortest {
             }
         }
         return "";
+    }*/
+
+
+    // Convert to "find the longest palindrome substring starts from index 0"
+    // KMP
+    //Runtime: 5 ms, faster than 90.09% of Java online submissions for Shortest Palindrome.
+    //Memory Usage: 39.6 MB, less than 60.10% of Java online submissions for Shortest Palindrome.
+
+    static String shortestPalindrome(String s) {
+        String ss = s + '#' + new StringBuilder(s).reverse();
+        int max = getLastKMP(ss);
+        return new StringBuilder(s.substring(max)).reverse() + s;
+    }
+
+    static int getLastKMP(String s) {
+        int n = s.length();
+        char[] c = s.toCharArray();
+        int[] nextTable = new int[n + 1];
+        nextTable[0] = -1;
+        nextTable[1] = 0;
+        int k = 0;
+        int i = 2;
+        while (i <= n) {
+            if (k == -1 || c[i - 1] == c[k]) {
+                nextTable[i] = k + 1;
+                k++;
+                i++;
+            } else {
+                k = nextTable[k];
+            }
+        }
+        return nextTable[n];
     }
 
 
     public static void main(String[] args) {
+
+        // abbacd
         //String testData = "aacecaaa";
-        //String testData = "abacd";
-        String testData  = "abcd";
+        //String testData = "abacd"; dcaba
+        String testData  = "abbacd";
         String result = shortestPalindrome(testData);
 
         System.out.printf(" input %s , PalindromeShortest is %s ", testData, result);
