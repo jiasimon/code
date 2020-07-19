0488bc5e5c6bcc575a5da540ab241546623a03ab
Simon Jia <sjia@nisum.com>
2020-07-19 10:33:39 -0700

    Leetcode #5 Combination Sum  Recursive backtracking

--- src/main/java/com/sjia/Leetcode/CombinationSum.java
+++ src/main/java/com/sjia/Leetcode/CombinationSum.java
@@ -17,6 +17,7 @@ public class CombinationSum {
     // Runtime: 3 ms, faster than 81.69% of Java online submissions for Combination Sum.
     //Memory Usage: 42 MB, less than 6.55% of Java online submissions for Combination Sum.
 
+/*
     static List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
         if (candidates.length < 1) return result;
@@ -42,7 +43,38 @@ public class CombinationSum {
             dfs(candidates, remain-candidates[i], i, path, result);
             path.removeLast();
         }
+    }*/
 
+
+    // Runtime: 5 ms, faster than 50.66% of Java online submissions for Combination Sum.
+    //Memory Usage: 41.6 MB, less than 11.95% of Java online submissions for Combination Sum.
+
+    // 
+    // Runtime: 3 ms, faster than 81.69% of Java online submissions for Combination Sum.
+    //Memory Usage: 39.6 MB, less than 68.37% of Java online submissions for Combination Sum.
+
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, target, 0);
+        return list;
+    }
+
+    private void backtrack(List<List<Integer>> result, ArrayList<Integer> tmp, int[] nums, int remain, int start) {
+        if (remain < 0) return;
+        else if ( remain == 0) {
+            result.add(new ArrayList<>(tmp));
+            return;
+        } else {
+            for ( int i = start; i< nums.length; i++) {
+                if (remain < nums[i]) {
+                    break;
+                }
+                tmp.add(nums[i]);
+                backtrack(result, tmp, nums, remain-nums[i], i);
+                tmp.remove(tmp.size()-1);
+            }
+        }
     }
 
 
@@ -51,7 +83,8 @@ public class CombinationSum {
         // int[] testData  = {2,3,6,7}; 7
         int[] testData  = {2,3,6,7};
         int target = 7;
-        List<List<Integer>> result = combinationSum(testData, target);
+        CombinationSum solution = new CombinationSum();
+        List<List<Integer>> result = solution.combinationSum(testData, target);
 
         System.out.printf(" target %d Three sum result is %s \n",target, result);
     }
