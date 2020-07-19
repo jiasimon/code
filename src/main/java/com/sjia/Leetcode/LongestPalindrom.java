10faf031d9b949e0c5bedd93d3b323b25b01b083
Simon Jia <sjia@nisum.com>
2020-07-18 18:16:48 -0700

    Leetcode #5 longest Palindrome  中心扩展

--- src/main/java/com/sjia/Leetcode/LongestPalindrom.java
+++ src/main/java/com/sjia/Leetcode/LongestPalindrom.java
@@ -10,7 +10,7 @@ public class LongestPalindrom {
 
     // Cut point,  Runtime: 23 ms, faster than 84.80% of Java online submissions for Longest Palindromic Substring.
     //Memory Usage: 39.9 MB, less than 41.01% of Java online submissions for Longest Palindromic Substring.
-
+/*
     static String longestPalindrome(String s) {
         if ( s == null || s.length() < 1) return "";
         int maxL = 0, L = s.length();
@@ -28,7 +28,39 @@ public class LongestPalindrom {
             }
         }
         return result;
+    }*/
+
+
+
+    // extend 
+    // Runtime: 22 ms, faster than 91.29% of Java online submissions for Longest Palindromic Substring.
+    //Memory Usage: 38 MB, less than 71.62% of Java online submissions for Longest Palindromic Substring.
+
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
     }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+
+
 
 
     // Manacher's Algorithm
