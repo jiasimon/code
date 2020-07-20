6c8cb2f0f62ac7fd487633b77305b0b0548b42de
Simon Jia <sjia@nisum.com>
2020-07-20 16:01:10 -0700

    Leetcode #53 Maximum subarray Sum

---
+++ src/main/java/com/sjia/Leetcode/MaximumSubarray.java
@@ -0,0 +1,34 @@
+package com.sjia.Leetcode;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MaximumSubarray {
+
+    // #53 https://leetcode.com/problems/maximum-subarray/
+
+    // Runtime: 2 ms, faster than 17.21% of Java online submissions for Maximum Subarray.
+    //Memory Usage: 41.7 MB, less than 5.39% of Java online submissions for Maximum Subarray.
+
+    public int maxSubArray(int[] nums) {
+        int result = nums[0] , sum = 0;
+        for (int num : nums) {
+            sum = Math.max(sum+ num, num);
+            result = Math.max(result, sum);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        //int[] testData = {1, 1, 2, 4 ,8 ,10};
+        int[] testData  = {-2,1,-3,4,-1,2,1,-5,4};
+
+
+        MaximumSubarray solution = new MaximumSubarray();
+        int result = solution.maxSubArray(testData);
+        System.out.printf(" testData %s MaximumSubarray is %s \n", Arrays.toString(testData), result);
+    }
+
+
+}
