ac32c3da44407a41a20c5a7f19b2defccc99576e
Simon Jia <sjia@nisum.com>
2020-07-20 12:04:15 -0700

    Leetcode #131  PalindromePartitioning

---
+++ src/main/java/com/sjia/Leetcode/PalindromePartitioning.java
@@ -0,0 +1,53 @@
+package com.sjia.Leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PalindromePartitioning {
+
+    // #131 https://leetcode.com/problems/palindrome-partitioning/
+    // Given a string s, partition s such that every substring of the partition is a palindrome.
+    //Return all possible palindrome partitioning of s.
+
+
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), s, 0);
+        return result;
+
+    }
+
+    public void backtrack(List<List<String>> list, List<String> tempList, String s, int start){
+        if(start == s.length())
+            list.add(new ArrayList<>(tempList));
+        else{
+            for(int i = start; i < s.length(); i++){
+                if(isPalindrome(s, start, i)){
+                    tempList.add(s.substring(start, i + 1));
+                    backtrack(list, tempList, s, i + 1);
+                    tempList.remove(tempList.size() - 1);
+                }
+            }
+        }
+    }
+    public boolean isPalindrome(String s, int low, int high){
+        while(low < high)
+            if(s.charAt(low++) != s.charAt(high--)) return false;
+        return true;
+    }
+
+
+
+    public static void main(String[] args) {
+
+        //String testData = "aBa";
+        //String testData  = " a  ";
+        String testData  = " a  ";
+        PalindromePartitioning solution = new PalindromePartitioning();
+        List<List<String>> result = solution.partition(testData);
+
+        System.out.printf(" input %s Palindrome Partitioning: %s ", testData, result);
+    }
+
+}
--- src/main/java/com/sjia/Leetcode/TwoSum.java
+++ src/main/java/com/sjia/Leetcode/TwoSum.java
@@ -21,6 +21,19 @@ public class TwoSum {
         return res;
     }
 
+/*
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i=0; i< nums.length; i++)
+            for (int j=i+1; j< nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] =i;
+                    result[1] =j;
+                    return result;
+                }
+            }
+        return result;
+    }*/
 
 
     public static void main(String[] args) {
