e4c445a0237167c60a4a156a8877f80c2c71b99b
Simon Jia <sjia@nisum.com>
2020-07-21 14:18:50 -0700

    Leetcode #70 Climbing Stairs , while loop

--- src/main/java/com/sjia/Leetcode/ClimbingStairs.java
+++ src/main/java/com/sjia/Leetcode/ClimbingStairs.java
@@ -6,8 +6,8 @@ public class ClimbingStairs {
 
     // Runtime: 0 ms, faster than 100.00% of Java online submissions for Climbing Stairs.
     //Memory Usage: 38 MB, less than 5.03% of Java online submissions for Climbing Stairs.
-
-    public int climbStairs(int n) {
+/*
+    static int climbStairs(int n) {
         if (n == 1 || n==2) return n;
         int x=1, y=2, result=3;
         for (int i= 3; i<=n; i++ ) {
@@ -16,6 +16,28 @@ public class ClimbingStairs {
             y = result;
         }
         return result;
+    }*/
+
+
+    static int climbStairs(int n) {
+        if (n == 1 || n==2) return n;
+        int x=1, y=2, result=3;
+        while ( n-- >=3) {
+            result = x + y;
+            x = y;
+            y = result;
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+
+        //testData  = 10 :89, 6 :13
+        int testData  = 6;
+        int result = climbStairs(testData);
+
+        System.out.printf(" testData %s th fibo is %s \n", testData,result);
     }
 
 }
