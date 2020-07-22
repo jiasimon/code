232f5ac1a8021c27fc55ceb3115551b60a9b188e
Simon Jia <sjia@nisum.com>
2020-07-21 19:19:27 -0700

    Leetcode #88 Merge Sorted Array, three pointer

--- src/main/java/com/sjia/Leetcode/MergeSortedArray.java
+++ src/main/java/com/sjia/Leetcode/MergeSortedArray.java
@@ -7,17 +7,42 @@ public class MergeSortedArray {
 
     // #88 https://leetcode.com/problems/merge-sorted-array/
 
+    //Runtime: 1 ms, faster than 20.93% of Java online submissions for Merge Sorted Array.
+    //Memory Usage: 40.3 MB, less than 5.07% of Java online submissions for Merge Sorted Array.
+/*
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         System.arraycopy(nums2, 0, nums1, m, n);
         Arrays.sort(nums1);
+    }*/
+
+
+    // Runtime: 0 ms, faster than 100.00% of Java online submissions for Merge Sorted Array.
+    //Memory Usage: 39.8 MB, less than 18.65% of Java online submissions for Merge Sorted Array.
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+
     }
+
 
     public static void main(String[] args) {
         int[] m = {1, 2, 3, 0, 0, 0};
         int[] n = {2, 5, 6};
 
         MergeSortedArray solution = new MergeSortedArray();
-        solution.merge(m, m.length, n, n.length);
+        solution.merge(m, 3, n, 3);
 
         System.out.printf(" After merge is %s \n", Arrays.toString(m));
     }
