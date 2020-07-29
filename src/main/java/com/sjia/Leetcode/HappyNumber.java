942801bb52935223c62b0ee356df76524c368766
Simon Jia <sjia@nisum.com>
2020-07-29 16:40:56 -0700

    Leetcode #202 Happy Number, iterative while loop

--- src/main/java/com/sjia/Leetcode/HappyNumber.java
+++ src/main/java/com/sjia/Leetcode/HappyNumber.java
@@ -8,7 +8,7 @@ public class HappyNumber {
 
     //Runtime: 2 ms, faster than 41.71% of Java online submissions for Happy Number.
     //Memory Usage: 38.4 MB, less than 8.40% of Java online submissions for Happy Number.
-    HashSet<Integer> tmp = new HashSet<>();
+/*    HashSet<Integer> tmp = new HashSet<>();
 
     public boolean isHappy(int n) {
         int result =0;
@@ -25,9 +25,35 @@ public class HappyNumber {
                 return isHappy(result);
             }
         }
+    }*/
+
+
+    // 4, 16, 37, 58, 89,
+
+    //Runtime: 4 ms, faster than 13.94% of Java online submissions for Happy Number.
+    //Memory Usage: 38.6 MB, less than 6.83% of Java online submissions for Happy Number.
+    public boolean isHappy(int n) {
+        HashSet<Integer> tmp = new HashSet<>();
+        while(true) {
+            int result = getNextNumber(n);
+            if ( result ==1) return true;
+            else if (tmp.contains(result)) return false;
+            else {
+                tmp.add(result);
+                n = result;
+            }
+        }
     }
 
-    
+    private int getNextNumber(int n) {
+        int result =0;
+        while (n >0) {
+            result +=  (n %10)*(n %10);
+            n /= 10 ;
+        }
+        return result;
+    }
+
 
     public static void main(String[] args) {
 
