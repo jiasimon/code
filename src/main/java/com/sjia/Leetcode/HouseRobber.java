bfdbd0668715348098f701b46e615e4323a1f005
Simon Jia <sjia@nisum.com>
2020-07-29 15:36:29 -0700

    Leetcode #198 House Robber, DP

---
+++ src/main/java/com/sjia/Leetcode/HouseRobber.java
@@ -0,0 +1,33 @@
+package com.sjia.Leetcode;
+
+import java.util.Arrays;
+
+public class HouseRobber {
+    // #198 https://leetcode.com/problems/house-robber/
+
+    // DP, Runtime: 0 ms, faster than 100.00% of Java online submissions for House Robber.
+    //Memory Usage: 38.8 MB, less than 5.09% of Java online submissions for House Robber.
+
+    public int rob(int[] nums) {
+        int q=0, z=0, result=0;
+        for (int i=0; i<nums.length; i++ ) {
+            result = Math.max(q + nums[i], z);
+            q = z;
+            z = result;
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+
+        //int[] testData  = {0,0};
+        int[] testData = {2, 7, 9, 3 ,1};
+
+        HouseRobber solution = new HouseRobber();
+        int result = solution.rob(testData);
+
+        System.out.printf(" testData  %s , rob max is %s \n",Arrays.toString(testData), result);
+    }
+
+}
