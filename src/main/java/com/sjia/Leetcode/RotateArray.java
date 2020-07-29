e6ae0abd87f4c12b41b1345c5813fb736f50a272
Simon Jia <sjia@nisum.com>
2020-07-29 09:27:29 -0700

    Leetcode #189 Rotate Array, three reverse

--- src/main/java/com/sjia/Leetcode/RotateArray.java
+++ src/main/java/com/sjia/Leetcode/RotateArray.java
@@ -8,7 +8,7 @@ public class RotateArray {
 
     // Runtime: 1 ms, faster than 53.97% of Java online submissions for Rotate Array.
     //Memory Usage: 43 MB, less than 5.08% of Java online submissions for Rotate Array.
-
+/*
     public void rotate(int[] nums, int k) {
         k %= nums.length;
         int result[] = new int[nums.length];
@@ -18,12 +18,37 @@ public class RotateArray {
         for (int i = 0; i < nums.length; i++) {
             nums[i] = result[i];
         }
+    }*/
+
+
+    // three reverse, Runtime: 0 ms, faster than 100.00% of Java online submissions for Rotate Array.
+    //Memory Usage: 39.9 MB, less than 53.45% of Java online submissions for Rotate Array.
+
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverseArray(nums, 0, nums.length - 1);
+        reverseArray(nums, 0, k - 1);
+        reverseArray(nums, k, nums.length - 1);
+
     }
+
+
+    public void reverseArray(int[] nums, int left, int right) {
+        int tmp;
+        while (left < right) {
+            tmp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = tmp;
+            left++;
+            right--;
+        }
+    }
+
 
     public static void main(String[] args) {
 
         //int[] testData  = {0,0};
-        int[] testData  = {3,30,34,5,9};
+        int[] testData = {3, 30, 34, 5, 9}; // 5, 9, 3, 30, 34
         int step = 2;
 
         RotateArray solution = new RotateArray();
