b12cc8796e527eff19ef035886d25373e5db1cae
Simon Jia <sjia@nisum.com>
2020-07-29 15:41:30 -0700

    Leetcode #198 House Robber, recursive

--- src/main/java/com/sjia/Leetcode/HouseRobber.java
+++ src/main/java/com/sjia/Leetcode/HouseRobber.java
@@ -18,6 +18,18 @@ public class HouseRobber {
         return result;
     }
 
+    // Recursive Time Limit Exceeded
+/*
+    public int rob(int[] nums) {
+        return rob(nums, nums.length - 1);
+    }
+    private int rob(int[] nums, int i) {
+        if (i < 0) {
+            return 0;
+        }
+        return Math.max(rob(nums, i - 2) + nums[i], rob(nums, i - 1));
+    }*/
+
 
     public static void main(String[] args) {
 
