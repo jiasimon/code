6af18ef2cc8df7e4169375c30ad452d07046d218
Simon Jia <sjia@nisum.com>
2020-07-29 07:29:35 -0700

    Leetcode #181 sql,  Employees Earning More

---
+++ src/main/java/com/sjia/Leetcode/SQLEmployeesEarningMore.sql
@@ -0,0 +1,19 @@
+
+select a.Name as Employee
+From Employee a right outer join Employee b
+On a.ManagerId = b.Id
+Where a.Salary > b.Salary
+
+
+
+SELECT
+     a.NAME AS Employee
+FROM Employee AS a JOIN Employee AS b
+     ON a.ManagerId = b.Id
+     AND a.Salary > b.Salary;
+
+
+select e.Name as Employee
+from Employee e, Employee m
+where e.ManagerId is not NULL and
+e.ManagerId = m.ID and e.Salary > m.Salary
