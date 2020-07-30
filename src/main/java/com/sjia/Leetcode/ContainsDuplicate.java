aa1f1799fe3c349d31d77dbbceccfe41fbad6d12
Simon Jia <sjia@nisum.com>
2020-07-30 15:19:43 -0700

    Leetcode #217, Contains Duplicate, HashSet

--- src/main/java/com/sjia/Leetcode/ContainsDuplicate.java
+++ src/main/java/com/sjia/Leetcode/ContainsDuplicate.java
@@ -1,6 +1,7 @@
 package com.sjia.Leetcode;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class ContainsDuplicate {
     // #217 check duplicate in array
@@ -9,14 +10,27 @@ public class ContainsDuplicate {
     // Sort changes original array, may work on a copy
     // Runtime: 4 ms, faster than 88.85% of Java online submissions for Contains Duplicate.
     //Memory Usage: 43.1 MB, less than 94.99% of Java online submissions for Contains Duplicate.
-
+/*
     public boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
         for(int i=1; i<nums.length; i++) {
             if(nums[i] == nums[i-1]) return true;
         }
         return false;
+    }*/
+
+    // HashSet
+    // Runtime: 5 ms, faster than 82.22% of Java online submissions for Contains Duplicate.
+    //Memory Usage: 46.1 MB, less than 11.65% of Java online submissions for Contains Duplicate.
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> tmp = new HashSet<>();
+        for(int i=0; i<nums.length; i++) {
+            if(tmp.contains(nums[i])) return true;
+            else tmp.add(nums[i]);
+        }
+        return false;
     }
+
 
 
     public static void main(String[] args) {
