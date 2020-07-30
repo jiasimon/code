9d7aab4c6bf75af3c18f07d6336fff0e54d14a4d
Simon Jia <sjia@nisum.com>
2020-07-30 12:26:25 -0700

    Leetcode #206, reverse LinkedList, recursive

--- src/main/java/com/sjia/Leetcode/ReverseLinkedList.java
+++ src/main/java/com/sjia/Leetcode/ReverseLinkedList.java
@@ -1,11 +1,13 @@
 package com.sjia.Leetcode;
 
+import java.util.List;
+
 public class ReverseLinkedList {
     // #206 https://leetcode.com/problems/reverse-linked-list/
 
     // iterative, Runtime: 0 ms, faster than 100.00% of Java online submissions for Reverse Linked List.
     //Memory Usage: 40.5 MB, less than 5.01% of Java online submissions for Reverse Linked List.
-
+/*
     public ListNode reverseList(ListNode head) {
         ListNode pre = null;
         while (head != null) {
@@ -15,9 +17,17 @@ public class ReverseLinkedList {
             head = tmp;
         }
         return pre;
+    }*/
+
+    // recursive Runtime: 0 ms, faster than 100.00% of Java online submissions for Reverse Linked List.
+    //Memory Usage: 40.7 MB, less than 5.01% of Java online submissions for Reverse Linked List.
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode tmp = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return tmp;
     }
 
-
-    
 
 }
