f645e5e33d6fb1db5de129e79010e4ec6face3ff
Simon Jia <sjia@nisum.com>
2020-07-31 08:29:46 -0700

    Leetcode #231,  Power of Two, check >0 and Integer.bitCount(n)==1

---
+++ src/main/java/com/sjia/Leetcode/PowerofTwo.java
@@ -0,0 +1,13 @@
+package com.sjia.Leetcode;
+
+public class PowerofTwo {
+    // #231 https://leetcode.com/problems/power-of-two/
+
+    // Runtime: 1 ms, faster than 100.00% of Java online submissions for Power of Two.
+    //Memory Usage: 36.9 MB, less than 11.45% of Java online submissions for Power of Two.
+    public boolean isPowerOfTwo(int n) {
+        if (n<=0) return false;
+        else return Integer.bitCount(n)==1 ;
+    }
+
+}
