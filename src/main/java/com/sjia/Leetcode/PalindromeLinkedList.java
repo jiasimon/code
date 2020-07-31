d1cabf82644cfb79aae0233e461efdf156f8ab09
Simon Jia <sjia@nisum.com>
2020-07-31 11:19:04 -0700

    Leetcode #234, PalindromeLinkedList, fast slow pointers

--- src/main/java/com/sjia/Leetcode/PalindromeLinkedList.java
+++ src/main/java/com/sjia/Leetcode/PalindromeLinkedList.java
@@ -9,6 +9,7 @@ public class PalindromeLinkedList {
 
     // Runtime: 3 ms, faster than 33.04% of Java online submissions for Palindrome Linked List.
     //Memory Usage: 43.3 MB, less than 26.87% of Java online submissions for Palindrome Linked List.
+/*
     public boolean isPalindrome(ListNode head) {
         List<Integer> tmpList = new ArrayList<>();
         ListNode cur = head;
@@ -25,6 +26,44 @@ public class PalindromeLinkedList {
             }
         }
         return true;
+    }*/
+
+
+    // fast, slow pointers Runtime: 1 ms, faster than 97.19% of Java online submissions for Palindrome Linked List.
+    //Memory Usage: 45.4 MB, less than 11.27% of Java online submissions for Palindrome Linked List.
+
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast =head, slow = head;
+        while(fast !=null && fast.next !=null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        if (fast !=null) {
+            slow = slow.next;
+        }
+        slow = reverse(slow);
+        fast = head;
+
+        while( slow!= null) {
+            if(fast.val != slow.val ) return false;
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return true;
     }
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode tmpNext = head.next;
+            head.next = prev;
+            prev = head;
+            head = tmpNext;
+        }
+        return prev;
+    }
+
+
 
 }
