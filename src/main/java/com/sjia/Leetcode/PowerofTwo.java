1db12b22c17f6688ce4b0e5e29a4d1928f150928
Simon Jia <sjia@nisum.com>
2020-07-31 08:54:01 -0700

    Leetcode #231,  Power of Two, (x & (-x)) == x

--- src/main/java/com/sjia/Leetcode/PowerofTwo.java
+++ src/main/java/com/sjia/Leetcode/PowerofTwo.java
@@ -5,9 +5,19 @@ public class PowerofTwo {
 
     // Runtime: 1 ms, faster than 100.00% of Java online submissions for Power of Two.
     //Memory Usage: 36.9 MB, less than 11.45% of Java online submissions for Power of Two.
+/*
     public boolean isPowerOfTwo(int n) {
         if (n<=0) return false;
         else return Integer.bitCount(n)==1 ;
+    }*/
+
+    // Runtime: 2 ms, faster than 40.55% of Java online submissions for Power of Two.
+    //Memory Usage: 38.7 MB, less than 5.42% of Java online submissions for Power of Two.
+    public boolean isPowerOfTwo(int n) {
+        if (n==0) return false;
+        long x = (long) n;
+        return (x & (-x)) == x;
     }
+
 
 }
