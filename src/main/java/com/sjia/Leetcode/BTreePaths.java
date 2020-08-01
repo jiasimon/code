8b605d23782b465f76cabc27a8deba0e89dfbf81
Simon Jia <sjia@nisum.com>
2020-08-01 08:18:23 -0700

    Leetcode #257 Binary Tree Paths,  put null check out of recursive

--- src/main/java/com/sjia/Leetcode/BTreePaths.java
+++ src/main/java/com/sjia/Leetcode/BTreePaths.java
@@ -8,6 +8,7 @@ public class BTreePaths {
 
     // Runtime: 10 ms, faster than 33.26% of Java online submissions for Binary Tree Paths.
     //Memory Usage: 40.9 MB, less than 5.41% of Java online submissions for Binary Tree Paths.
+/*
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> result = new ArrayList<>();
         pathHelper(root,"",result);
@@ -25,6 +26,23 @@ public class BTreePaths {
                 pathHelper(root.right,path,result);
             }
         }
+    }*/
+
+
+    // put null check out of recursive
+    // Runtime: 8 ms, faster than 62.42% of Java online submissions for Binary Tree Paths.
+    //Memory Usage: 39.9 MB, less than 10.07% of Java online submissions for Binary Tree Paths.
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> result = new ArrayList<>();
+        if(root!=null) pathMaker(root,"",result);
+        return result;
     }
+
+    private void pathMaker(TreeNode root, String path, List<String> result) {
+        if (root.left == null && root.right == null)  result.add(path+root.val);
+        if (root.left != null) pathMaker(root.left, path+root.val + "->", result);
+        if (root.right != null) pathMaker(root.right, path+root.val + "->", result);
+    }
+
 
 }
