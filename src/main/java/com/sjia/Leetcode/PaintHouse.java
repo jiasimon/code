ad7e5041145c1cc3b8f10236426f3d1c5ed396e9
Simon Jia <sjia@nisum.com>
2020-08-01 07:23:38 -0700

    Leetcode #256  Paint House ( Locked ) , DP

---
+++ src/main/java/com/sjia/Leetcode/PaintHouse.java
@@ -0,0 +1,30 @@
+package com.sjia.Leetcode;
+
+public class PaintHouse {
+    // #256 https://leetcode.com/problems/paint-house/
+    // There are a row of n houses, each house can be painted with one of the three colors: red, blue or green.
+    // The cost of painting each house with a certain color is different.
+    // You have to paint all the houses such that no two adjacent houses have the same color.
+
+    public static int getMinimumPrice(int[][] cost) {
+        int result = 0;
+
+        for (int i = 1; i < cost.length; i++) {
+            cost[i][0] += Math.min(cost[i - 1][1], cost[i - 1][2]);
+            cost[i][1] += Math.min(cost[i - 1][0], cost[i - 1][2]);
+            cost[i][2] += Math.min(cost[i - 1][1], cost[i - 1][0]);
+        }
+
+        result = Math.min(Math.min(cost[cost.length - 1][0], cost[cost.length - 1][1]), cost[cost.length - 1][2]);
+        return result;
+    }
+
+    public static void main(String args[]) {
+        int[][] cost = {
+                {17, 2, 1}, {16, 16, 1}, {14, 3, 19}, {3, 1, 8}
+        };
+        int minCost = getMinimumPrice(cost);
+        System.out.println(minCost);
+
+    }
+}
