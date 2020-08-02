ec3dbd47b6cdd45b1eae2dd870c89856f3744337
Simon Jia <sjia@nisum.com>
2020-08-02 16:27:05 -0700

    Leetcode #292 Nim game

---
+++ src/main/java/com/sjia/Leetcode/NimGame.java
@@ -0,0 +1,22 @@
+package com.sjia.Leetcode;
+
+public class NimGame {
+    // #292 https://leetcode.com/problems/nim-game/
+
+    // Runtime: 0 ms, faster than 100.00% of Java online submissions for Nim Game.
+    //Memory Usage: 36.4 MB, less than 9.65% of Java online submissions for Nim Game.
+    public boolean canWinNim(int n) {
+        if (n%4 ==0 ) return false;
+        else  return true;
+    }
+
+
+    // Runtime: 0 ms, faster than 100.00% of Java online submissions for Nim Game.
+    //Memory Usage: 36.2 MB, less than 32.89% of Java online submissions for Nim Game.
+/*
+    public boolean canWinNim(int n) {
+        return (n % 4 != 0);
+    }*/
+
+
+}
