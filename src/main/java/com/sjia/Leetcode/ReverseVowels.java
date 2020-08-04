4788ebb3db1163b7a0ff4be6c3468892f5bd2b8f
Simon Jia <sjia@nisum.com>
2020-08-03 20:48:52 -0700

    Leetcode #345  Reverse Vowels, init HashSet, Arrays.asList

--- src/main/java/com/sjia/Leetcode/ReverseVowels.java
+++ src/main/java/com/sjia/Leetcode/ReverseVowels.java
@@ -1,5 +1,9 @@
 package com.sjia.Leetcode;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class ReverseVowels {
 
     // #345 https://leetcode.com/problems/reverse-vowels-of-a-string/
@@ -7,6 +11,7 @@ public class ReverseVowels {
 
     // Runtime: 5 ms, faster than 59.04% of Java online submissions for Reverse Vowels of a String.
     //Memory Usage: 40.7 MB, less than 14.72% of Java online submissions for Reverse Vowels of a String.
+/*
     public String reverseVowels(String s) {
         if (s ==null || s.length()==0)  return s;
         String vowel = "aeiouAEIOU";
@@ -25,8 +30,33 @@ public class ReverseVowels {
             }
         }
         return new String(tmp);
+    }*/
 
+
+
+    // Runtime: 5 ms, faster than 59.04% of Java online submissions for Reverse Vowels of a String.
+    //Memory Usage: 39.8 MB, less than 38.58% of Java online submissions for Reverse Vowels of a String.
+    public String reverseVowels(String s) {
+        if (s ==null || s.length()==0)  return s;
+        //String vowel = "aeiouAEIOU";
+        char [] tmp = s.toCharArray();
+        Set<Character> vowelSet = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
+        int left =0, right = tmp.length-1;
+        while (left < right) {
+            if (!vowelSet.contains(tmp[left]) ) left++;
+            else if (!vowelSet.contains(tmp[right]) ) right--;
+            else if (vowelSet.contains(tmp[left]) &&
+                    vowelSet.contains(tmp[right]) ) {
+                char a = tmp[left];
+                tmp[left] = tmp[right];
+                tmp[right] = a;
+                left++;
+                right--;
+            }
+        }
+        return new String(tmp);
     }
+
 
 
 }
