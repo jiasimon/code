c5a0289b4ff2a14d0fe8abb06a48dd2550e6390a
Simon Jia <sjia@nisum.com>
2020-08-05 08:16:58 -0700

    Leetcode #383 , Ransom Note, int[128]

--- src/main/java/com/sjia/Leetcode/RansomNote.java
+++ src/main/java/com/sjia/Leetcode/RansomNote.java
@@ -9,6 +9,7 @@ public class RansomNote {
 
     // Runtime: 32 ms, faster than 9.31% of Java online submissions for Ransom Note.
     //Memory Usage: 49.6 MB, less than 5.02% of Java online submissions for Ransom Note.
+/*
     public boolean canConstruct(String ransomNote, String magazine) {
         if (ransomNote.length() > magazine.length()) return false;
 
@@ -27,7 +28,32 @@ public class RansomNote {
             if (r.get(key) >  m.getOrDefault(key,0)) return false;
         }
         return true;
+    }*/
 
+
+
+
+    // Runtime: 8 ms, faster than 46.60% of Java online submissions for Ransom Note.
+    //Memory Usage: 42.6 MB, less than 5.02% of Java online submissions for Ransom Note.
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) return false;
+
+        int[] r = new int[128];
+        int[] m = new int[128];
+
+        for( int i=0; i < ransomNote.length(); i++ ) {
+            r[ransomNote.charAt(i)  ] += 1;
+        }
+
+        for (int i=0; i <magazine.length(); i++ ) {
+            r[magazine.charAt(i)  ] += 1;
+        }
+
+        for (int i=0; i < ransomNote.length(); i++ ) {
+            if (r[ransomNote.charAt(i)] >  m[ransomNote.charAt(i)] ) return false;
+        }
+        return true;
     }
+
 
 }
