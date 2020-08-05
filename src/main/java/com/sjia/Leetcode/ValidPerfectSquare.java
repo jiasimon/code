5c52f3374bca7c20291b75ca5a4429a9b9f283f4
Simon Jia <sjia@nisum.com>
2020-08-04 17:14:56 -0700

    Leetcode #367 Valid Perfect Square, refer #69

---
+++ src/main/java/com/sjia/Leetcode/ValidPerfectSquare.java
@@ -0,0 +1,23 @@
+package com.sjia.Leetcode;
+
+public class ValidPerfectSquare {
+    // #367 https://leetcode.com/problems/valid-perfect-square/
+
+    // Runtime: 1 ms, faster than 20.90% of Java online submissions for Valid Perfect Square.
+    //Memory Usage: 38.8 MB, less than 5.15% of Java online submissions for Valid Perfect Square.
+/*
+    public boolean isPerfectSquare(int num) {
+        if (num < 0) return false;
+        int i=1;
+        while (num > 0) {
+            num -= i;
+            i +=2;
+        }
+        return num==0;
+    }*/
+
+
+
+
+
+}
