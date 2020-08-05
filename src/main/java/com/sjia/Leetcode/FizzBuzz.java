185c3c7e2cafc843abbf9497e2314a57ae219fac
Simon Jia <sjia@nisum.com>
2020-08-05 15:43:46 -0700

    #412 Fizz Buzz,  for more condition

--- src/main/java/com/sjia/Leetcode/FizzBuzz.java
+++ src/main/java/com/sjia/Leetcode/FizzBuzz.java
@@ -9,7 +9,8 @@ public class FizzBuzz {
 
     // Runtime: 2 ms, faster than 56.23% of Java online submissions for Fizz Buzz.
     //Memory Usage: 45.8 MB, less than 5.21% of Java online submissions for Fizz Buzz.
-    public List<String> fizzBuzz(int n) {
+
+/*    public List<String> fizzBuzz(int n) {
         List <String> result = new ArrayList<>();
         for ( int i=1; i <=n; i++ ) {
             if ( (i %3 ==0) && (i %5 ==0) ) {
@@ -23,7 +24,30 @@ public class FizzBuzz {
             }
         }
         return result;
+    }*/
 
+
+    // Runtime: 8 ms, faster than 12.59% of Java online submissions for Fizz Buzz.
+    //Memory Usage: 46.2 MB, less than 5.21% of Java online submissions for Fizz Buzz.
+
+    public List<String> fizzBuzz(int n) {
+        List <String> result = new ArrayList<>();
+        for ( int i=1; i <=n; i++ ) {
+            String tmp = "";
+            if(i %3==0) {
+                tmp += "Fizz";
+            }
+            if (i %5==0) {
+                tmp += "Buzz";
+            }
+            if (tmp.length()==0){
+                tmp += String.valueOf(i);
+            }
+            result.add(tmp);
+        }
+        return result;
     }
+
+
 
 }
