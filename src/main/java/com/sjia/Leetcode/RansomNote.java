e6e60f8a72b8466698d80ec813e203007eedbc2c
Simon Jia <sjia@nisum.com>
2020-08-05 08:40:16 -0700

    Leetcode #383 , Ransom Note, int[26] ,one array

--- src/main/java/com/sjia/Leetcode/RansomNote.java
+++ src/main/java/com/sjia/Leetcode/RansomNote.java
@@ -1,5 +1,6 @@
 package com.sjia.Leetcode;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -35,6 +36,7 @@ public class RansomNote {
 
     // Runtime: 8 ms, faster than 46.60% of Java online submissions for Ransom Note.
     //Memory Usage: 42.6 MB, less than 5.02% of Java online submissions for Ransom Note.
+/*
     public boolean canConstruct(String ransomNote, String magazine) {
         if (ransomNote.length() > magazine.length()) return false;
 
@@ -46,14 +48,48 @@ public class RansomNote {
         }
 
         for (int i=0; i <magazine.length(); i++ ) {
-            r[magazine.charAt(i)  ] += 1;
+            m[magazine.charAt(i)  ] += 1;
         }
 
         for (int i=0; i < ransomNote.length(); i++ ) {
             if (r[ransomNote.charAt(i)] >  m[ransomNote.charAt(i)] ) return false;
         }
         return true;
+    }*/
+
+
+
+    // Runtime: 3 ms, faster than 84.41% of Java online submissions for Ransom Note.
+    //Memory Usage: 43.1 MB, less than 5.02% of Java online submissions for Ransom Note.
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) return false;
+
+        int[] m = new int[26];
+
+        for (int i=0; i <magazine.length(); i++ ) {
+            m[magazine.charAt(i) - 'a' ] += 1;
+        }
+
+        for (int i=0; i < ransomNote.length(); i++ ) {
+           m[ransomNote.charAt(i) - 'a'] -= 1;
+           if (m[ransomNote.charAt(i) - 'a'] <0) {
+               return false;
+           }
+        }
+        return true;
     }
 
+
+    public static void main(String[] args) {
+
+
+        String ransomNote  = "aa";
+        String magazine = "ab";
+        RansomNote solution = new RansomNote();
+        boolean result = solution.canConstruct(ransomNote, magazine);
+
+        System.out.printf("ransomNote: %s  and magazine %s canConstruct %s \n",
+                ransomNote,magazine, result);
+    }
 
 }
