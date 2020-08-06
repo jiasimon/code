6340f8433eb94f2bf426bff1985d75a75a19909d
Simon Jia <sjia@nisum.com>
2020-08-06 15:31:49 -0700

    #414,  Third Maximum Number flag for Integer.MIN_VALUE

--- src/main/java/com/sjia/Leetcode/FizzBuzz.java
+++ src/main/java/com/sjia/Leetcode/FizzBuzz.java
@@ -49,5 +49,31 @@ public class FizzBuzz {
     }
 
 
+    // Runtime: 1 ms, faster than 99.56% of Java online submissions for Fizz Buzz.
+    //Memory Usage: 40.9 MB, less than 9.00% of Java online submissions for Fizz Buzz.
+    // use  instead of mode
+/*
+    public List<String> fizzBuzz(int n) {
+        List<String> ret = new ArrayList<String>(n);
+        for(int i=1,fizz=0,buzz=0;i<=n ;i++){
+            fizz++;
+            buzz++;
+            if(fizz==3 && buzz==5){
+                ret.add("FizzBuzz");
+                fizz=0;
+                buzz=0;
+            }else if(fizz==3){
+                ret.add("Fizz");
+                fizz=0;
+            }else if(buzz==5){
+                ret.add("Buzz");
+                buzz=0;
+            }else{
+                ret.add(String.valueOf(i));
+            }
+        }
+        return ret;
+    }*/
+
 
 }
---
+++ src/main/java/com/sjia/Leetcode/ThirdMaximumNumber.java
@@ -0,0 +1,39 @@
+package com.sjia.Leetcode;
+
+public class ThirdMaximumNumber {
+
+    // #414 https://leetcode.com/problems/third-maximum-number/
+    // [2, 2, 3, 1] , return Output: 1
+    // Given a non-empty array of integers,return the third maximum number
+    // If it does not exist, return the maximum number.
+
+    //Runtime: 0 ms, faster than 100.00% of Java online submissions for Third Maximum Number.
+    //Memory Usage: 39.5 MB, less than 18.33% of Java online submissions for Third Maximum Number.
+
+    // add flag for [1,2,-2147483648]
+/*
+    public int thirdMax(int[] nums) {
+        int first = Integer.MIN_VALUE, second =Integer.MIN_VALUE, third = Integer.MIN_VALUE;
+        boolean flag = true;
+        for (int n : nums) {
+            if ( n > first) {
+                third = second;
+                second =first;
+                first = n;
+            } else if ( n < first && n > second) {
+                third = second;
+                second = n;
+            } else if ( n < second && n > third) {
+                third = n;
+            } else if ( n== Integer.MIN_VALUE) flag = false;
+        }
+        if ((third == Integer.MIN_VALUE && flag ) || third == second) return first;
+        else return third;
+    }*/
+
+
+
+
+
+
+}
