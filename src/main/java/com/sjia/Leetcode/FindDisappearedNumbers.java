9e19419de733b33813f3e823778b18b02eee443e
Simon Jia <sjia@nisum.com>
2020-08-07 19:44:24 -0700

    Leetcode #448 FindDisappearedNumbers , nums[val] = -nums[val]

--- src/main/java/com/sjia/Leetcode/FindDisappearedNumbers.java
+++ src/main/java/com/sjia/Leetcode/FindDisappearedNumbers.java
@@ -23,4 +23,29 @@ public class FindDisappearedNumbers {
         return result;
     }
 
+
+
+    // smart solution
+    // Runtime: 5 ms, faster than 89.23% of Java online submissions for Find All Numbers Disappeared in an Array.
+    //Memory Usage: 48.5 MB, less than 77.54% of Java online submissions for Find All Numbers Disappeared in an Array.
+/*
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> result = new ArrayList<Integer>();
+
+        for(int i = 0; i < nums.length; i++) {
+            int val = Math.abs(nums[i]) - 1;
+            if(nums[val] > 0) {
+                nums[val] = -nums[val];
+            }
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] > 0) {
+                result.add(i+1);
+            }
+        }
+        return result;
+    }*/
+
+
 }
