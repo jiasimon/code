45b97382b0483ff0982c709cfe8e2d61a8f9ed71
Simon Jia <sjia@nisum.com>
2020-08-13 13:33:54 -0700

    Leetcode #509, Fibonacci Number, iterative

--- src/main/java/com/sjia/Leetcode/FibonacciNumber.java
+++ src/main/java/com/sjia/Leetcode/FibonacciNumber.java
@@ -3,6 +3,7 @@ package com.sjia.Leetcode;
 public class FibonacciNumber {
 
     // #509 https://leetcode.com/problems/fibonacci-number/
+    // refer to #62, 70 ,91
 
     // recursive
     // Runtime: 6 ms, faster than 32.51% of Java online submissions for Fibonacci Number.
@@ -15,8 +16,19 @@ public class FibonacciNumber {
 
 
 
+    //iterative, faster than recursive
+    // Runtime: 0 ms, faster than 100.00% of Java online submissions for Fibonacci Number.
+    //Memory Usage: 36.1 MB, less than 77.98% of Java online submissions for Fibonacci Number.
+    public int fib(int N) {
+        if (N <=1) return N;
+        int prev=0, pre=1, current=0;
+        for (int i=2; i<=N; i++) {
+            current = prev + pre;
+            prev = pre;
+            pre = current;
+        }
+        return current;
+    }
 
-
-
-
+    
 }
