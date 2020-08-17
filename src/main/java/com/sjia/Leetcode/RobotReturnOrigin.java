836934364bbac4652bad3461f126d28638c3e4bd
Simon Jia <sjia@nisum.com>
2020-08-17 09:58:46 -0700

    Leetcode #657,  RobotReturnOrigin  toCharArray() vs charAt(i)

--- src/main/java/com/sjia/Leetcode/RobotReturnOrigin.java
+++ src/main/java/com/sjia/Leetcode/RobotReturnOrigin.java
@@ -5,6 +5,7 @@ public class RobotReturnOrigin {
 
     // Runtime: 7 ms, faster than 46.91% of Java online submissions for Robot Return to Origin.
     //Memory Usage: 39.3 MB, less than 87.86% of Java online submissions for Robot Return to Origin.
+ /*
     public boolean judgeCircle(String moves) {
         if (moves.length() %2 ==1) return false;
         int x=0, y=0;
@@ -15,7 +16,24 @@ public class RobotReturnOrigin {
             else if (moves.charAt(i) == 'R') x++;
         }
         return x==0 && y==0;
+    }*/
+
+
+
+    public boolean judgeCircle(String moves) {
+        if (moves.length() %2 ==1) return false;
+        int x=0, y=0;
+        char[] tmp = moves.toCharArray();
+        for (int i=0; i < moves.length(); i++) {
+            if (tmp[i] == 'U') y++;
+            else if (tmp[i] == 'D') y--;
+            else if (tmp[i] == 'L') x--;
+            else if (tmp[i] == 'R') x++;
+        }
+        return x==0 && y==0;
     }
+
+
 
 
 }
