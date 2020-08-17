3dc0fa1500ff731d5287ff55a004a6553dca95b2
Simon Jia <sjia@nisum.com>
2020-08-17 08:44:42 -0700

    Leetcode #633, SumSquareNumbers,  two pointer, while (L <= R)

--- src/main/java/com/sjia/Leetcode/SumSquareNumbers.java
+++ src/main/java/com/sjia/Leetcode/SumSquareNumbers.java
@@ -5,10 +5,27 @@ public class SumSquareNumbers {
 
     // Runtime: 3 ms, faster than 76.02% of Java online submissions for Sum of Square Numbers.
     //Memory Usage: 36 MB, less than 92.75% of Java online submissions for Sum of Square Numbers.
+/*
     public boolean judgeSquareSum(int c) {
         for (long i =0; i*i <= c; i++ ) {
             double j = Math.sqrt(c -i*i);
             if ( j == (int)j ) return true;
+        }
+        return false;
+    }*/
+
+
+    // two pointer
+    // Runtime: 2 ms, faster than 97.69% of Java online submissions for Sum of Square Numbers.
+    //Memory Usage: 35.9 MB, less than 93.86% of Java online submissions for Sum of Square Numbers.
+    public boolean judgeSquareSum(int c) {
+        if (c <0) return false;
+        int left=0, right = (int)Math.sqrt(c);
+        while(left <= right) {
+            int sum = left*left + right * right;
+            if (sum<c) left++;
+            else if (sum>c) right--;
+            else return true;
         }
         return false;
     }
