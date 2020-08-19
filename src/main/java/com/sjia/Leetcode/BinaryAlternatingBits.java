d8bc5690e2e2940dec57e9347c45c013f67179ab
Simon Jia <sjia@nisum.com>
2020-08-19 13:09:07 -0700

    Leetcode #693,  Integer.toBinaryString(n), regex or contains

--- src/main/java/com/sjia/Leetcode/BinaryAlternatingBits.java
+++ src/main/java/com/sjia/Leetcode/BinaryAlternatingBits.java
@@ -7,13 +7,28 @@ public class BinaryAlternatingBits {
     // #bit , right shift 1
     // Runtime: 0 ms, faster than 100.00% of Java online submissions for Binary Number with Alternating Bits.
     //Memory Usage: 36 MB, less than 88.40% of Java online submissions for Binary Number with Alternating Bits.
+/*
     public boolean hasAlternatingBits(int n) {
         n = n ^ (n >>1) ;
         return (n & (n+1) ) ==0;
-    }
+    }*/
 
 
-    
 
+    // regex,
+    // Runtime: 6 ms, faster than 7.47% of Java online submissions for Binary Number with Alternating Bits.
+    //Memory Usage: 36.6 MB, less than 46.72% of Java online submissions for Binary Number with Alternating Bits.
+/*    public boolean hasAlternatingBits(int n) {
+        return Integer.toBinaryString(n).matches("(10)*1?");
+    }*/
+
+
+
+    // Runtime: 1 ms, faster than 33.60% of Java online submissions for Binary Number with Alternating Bits.
+    //Memory Usage: 36.4 MB, less than 59.74% of Java online submissions for Binary Number with Alternating Bits.
+/*    public boolean hasAlternatingBits(int n) {
+        return  (!Integer.toBinaryString(n).contains("00") ) &&
+                (!Integer.toBinaryString(n).contains("11") );
+    }*/
 
 }
