fdd39a2edee18db030e34310899ca0257f0955db
Simon Jia <sjia@nisum.com>
2020-08-22 19:18:47 -0700

    Leetcode #704, BinarySearch, while(left <= right)

---
+++ src/main/java/com/sjia/Leetcode/BinarySearch.java
@@ -0,0 +1,24 @@
+package com.sjia.Leetcode;
+
+public class BinarySearch {
+
+    // #704 https://leetcode.com/problems/binary-search/
+    // assume that all elements in nums are unique.
+
+    // Runtime: 0 ms, faster than 100.00% of Java online submissions for Binary Search.
+    //Memory Usage: 53.5 MB, less than 5.02% of Java online submissions for Binary Search.
+    public int search(int[] nums, int target) {
+        int left=0, right= nums.length-1;
+        while(left <= right) {
+            int mid = (left + right) /2;
+            if(nums[mid] == target) return mid;
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right =mid-1;
+            }
+        }
+        return -1;
+    }
+
+}
