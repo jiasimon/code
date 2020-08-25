93b5aa05f18baaf15dde316ec87aaddd00cf56c2
Simon Jia <sjia@nisum.com>
2020-08-25 15:20:23 -0700

    Leetcode #724, PivotIndex,  use stream for sum, much slower

--- src/main/java/com/sjia/Leetcode/PivotIndex.java
+++ src/main/java/com/sjia/Leetcode/PivotIndex.java
@@ -1,5 +1,7 @@
 package com.sjia.Leetcode;
 
+import java.util.Arrays;
+
 public class PivotIndex {
     // #724 https://leetcode.com/problems/find-pivot-index/
 
@@ -10,11 +12,18 @@ public class PivotIndex {
 
     // Runtime: 2 ms, faster than 54.30% of Java online submissions for Find Pivot Index.
     //Memory Usage: 50 MB, less than 9.05% of Java online submissions for Find Pivot Index.
+
+    // Use Stream, Runtime: 12 ms, faster than 12.00% of Java online submissions for Find Pivot Index.
+    //Memory Usage: 50.1 MB, less than 8.33% of Java online submissions for Find Pivot Index.
+
     public int pivotIndex(int[] nums) {
         int total=0, leftSum=0;
-        for (int n : nums) {
+/*        for (int n : nums) {
             total += n;
-        }
+        }*/
+        total = Arrays.stream(nums).sum();
+
+
         for (int i =0; i< nums.length; i++) {
             if (leftSum *2 == total - nums[i]) {
                 return i;
