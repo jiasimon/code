da8ede93071c796c4026cf06b932b407321df05c
Simon Jia <sjia@nisum.com>
2020-09-08 09:48:06 -0700

    Leetcode #771 JewelsStones, use int[58]

--- src/main/java/com/sjia/Leetcode/JewelsStones.java
+++ src/main/java/com/sjia/Leetcode/JewelsStones.java
@@ -22,4 +22,22 @@ public class JewelsStones {
     }
 
 
+    // use array is faster
+    // Runtime: 0 ms, faster than 100.00% of Java online submissions for Jewels and Stones.
+    //Memory Usage: 37.9 MB, less than 73.41% of Java online submissions for Jewels and Stones.
+/*
+    public int numJewelsInStones(String J, String S) {
+        int result=0;
+        int[] tmp = new int[58];  // 'z' - 'A' is 58
+        for( char c : J.toCharArray()) {
+            tmp[c - 'A'] = 1;
+        }
+        for(char s : S.toCharArray()) {
+            result += tmp[s - 'A'];
+        }
+        return result;
+    }*/
+
+
+
 }
