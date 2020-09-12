bbdf65ac0aa8abf38144b8b0325d25a5214029a2
Simon Jia <sjia@nisum.com>
2020-09-12 08:28:39 -0700

    Leetcode #832  FlippingImage for (int[] row: A),   when row[i] == row[N-1-i]

--- src/main/java/com/sjia/Leetcode/FlippingImage.java
+++ src/main/java/com/sjia/Leetcode/FlippingImage.java
@@ -5,7 +5,7 @@ public class FlippingImage {
 
     // Runtime: 1 ms, faster than 39.15% of Java online submissions for Flipping an Image.
     //Memory Usage: 42 MB, less than 16.27% of Java online submissions for Flipping an Image.
-    public int[][] flipAndInvertImage(int[][] A) {
+/*    public int[][] flipAndInvertImage(int[][] A) {
         for (int i=0; i<A.length; i++) {
             int N = A[0].length;
             for (int j=0; j < (N +1 ) / 2 ; j++) {
@@ -15,7 +15,26 @@ public class FlippingImage {
             }
         }
         return A;
+    }*/
+
+
+
+    // Runtime: 0 ms, faster than 100.00% of Java online submissions for Flipping an Image.
+    //Memory Usage: 39.3 MB, less than 96.69% of Java online submissions for Flipping an Image.
+
+    public int[][] flipAndInvertImage(int[][] A) {
+        int N = A.length;
+        for (int[] row: A) {
+            for (int i=0; i < (N+1) /2 ; i++) {
+                if( row[i] == row[N-1-i]) {
+                    row[i] ^= 1;
+                    row[N-1-i] = row[i];
+                }
+            }
+        }
+        return A;
     }
+
 
 
 }
