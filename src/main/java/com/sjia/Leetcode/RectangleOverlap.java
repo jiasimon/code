608835387aaa810f8d5677517358544acd918d57
Simon Jia <sjia@nisum.com>
2020-09-12 09:26:43 -0700

    Leetcode #836 RectangleOverlap reverse non-overlap

---
+++ src/main/java/com/sjia/Leetcode/RectangleOverlap.java
@@ -0,0 +1,17 @@
+package com.sjia.Leetcode;
+
+public class RectangleOverlap {
+    // #836 https://leetcode.com/problems/rectangle-overlap/
+
+    // reverse non-overlap, project to X, Y
+    // Runtime: 0 ms, faster than 100.00% of Java online submissions for Rectangle Overlap.
+    //Memory Usage: 36.6 MB, less than 83.16% of Java online submissions for Rectangle Overlap.
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        return ! ( rec1[0] >=rec2[2] || rec1[2] <= rec2[0] ||
+                rec1[1] >= rec2[3] || rec1[3] <= rec2[1] );
+    }
+
+    
+
+
+}
