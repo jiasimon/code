65d86d522909f4a4a25c2c45cb41944ccbf40d46
Simon Jia <sjia@nisum.com>
2020-09-13 08:32:14 -0700

    Leetcode #252 MeetingRoom, Comparator.comparingInt(a -> a[0])

---
+++ src/main/java/com/sjia/Leetcode/MeetingRoom.java
@@ -0,0 +1,20 @@
+package com.sjia.Leetcode;
+
+import java.util.Arrays;
+
+public class MeetingRoom {
+    // #252 https://leetcode.com/problems/meeting-rooms/
+
+
+    public boolean canAttendMeetings(int[][] intervals) {
+        if (intervals.length <= 1) return true;
+        Arrays.sort(intervals, (a,b) -> a[0] -b[0]);
+
+        for (int i = 1 ; i< intervals.length; i++) {
+            if (intervals[i][0] < intervals[i-1][1]) return false;
+        }
+        return true;
+    }
+
+
+}
