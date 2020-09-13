634719834396f8e3c8e075ab3002887e91e92814
Simon Jia <sjia@nisum.com>
2020-09-13 09:18:29 -0700

    Leetcode #252 MeetingRoom, (a,b) -> a[0] -b[0]

--- src/main/java/com/sjia/Leetcode/MeetingRoom.java
+++ src/main/java/com/sjia/Leetcode/MeetingRoom.java
@@ -9,7 +9,7 @@ public class MeetingRoom {
     public boolean canAttendMeetings(int[][] intervals) {
         if (intervals.length <= 1) return true;
         Arrays.sort(intervals, (a,b) -> a[0] -b[0]);
-
+        // Arrays.sort(intervals, Comparator.comparingInt(a -> a[1]));
         for (int i = 1 ; i< intervals.length; i++) {
             if (intervals[i][0] < intervals[i-1][1]) return false;
         }
