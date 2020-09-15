a3efd4f644b30ff16136dfa7e61be799bd2893ec
Simon Jia <sjia@nisum.com>
2020-09-15 11:01:38 -0700

    Leetcode #859  BuddyStrings use Hashset check more than 2 count, less memory

--- src/main/java/com/sjia/Leetcode/BuddyStrings.java
+++ src/main/java/com/sjia/Leetcode/BuddyStrings.java
@@ -1,5 +1,8 @@
 package com.sjia.Leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class BuddyStrings {
     // #859 https://leetcode.com/problems/buddy-strings/
 
@@ -13,7 +16,7 @@ public class BuddyStrings {
 
     // Runtime: 2 ms, faster than 76.62% of Java online submissions for Buddy Strings.
     //Memory Usage: 40.4 MB, less than 10.77% of Java online submissions for Buddy Strings.
-    public boolean buddyStrings(String A, String B) {
+/*    public boolean buddyStrings(String A, String B) {
         if (A.length() != B.length()) return false;
         if (A.equals(B)) {
             if (A.length() > 26 ) return true;  // only of lowercase letters.
@@ -35,6 +38,38 @@ public class BuddyStrings {
             return ( (y!= -1) && A.charAt(x)== B.charAt(y) && A.charAt(y) == B.charAt(x) );
         }
 
+    }*/
+
+
+
+
+    // Use Hashset to check duplicate
+    // Runtime: 1 ms, faster than 98.92% of Java online submissions for Buddy Strings.
+    //Memory Usage: 39.3 MB, less than 76.39% of Java online submissions for Buddy Strings.
+    public boolean buddyStrings(String A, String B) {
+        int len = A.length();
+        if (len != B.length()) return false;
+        if (A.equals(B)) {
+            if (len > 26 ) return true;  // only of lowercase letters.
+            Set<Character> count = new HashSet<>();
+            for (char c : A.toCharArray()) {
+                count.add(c);
+            }
+            return count.size() < len;
+        } else {    // only two positions can be different
+            int x= -1, y= -1;
+            for (int i = 0; i< len; i++) {
+                if (A.charAt(i) !=B.charAt(i)) {
+                    if (x== -1) x=i;
+                    else if ( y ==-1) y=i;
+                    else return false;
+                }
+            }
+            return ( (y!= -1) && A.charAt(x)== B.charAt(y) && A.charAt(y) == B.charAt(x) );
+        }
+
     }
+
+
 
 }
