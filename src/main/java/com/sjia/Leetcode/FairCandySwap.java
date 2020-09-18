b68640d2f72a194dc61cac30c64ec66ac2fb2e41
Simon Jia <sjia@nisum.com>
2020-09-18 16:15:08 -0700

    Leetcode #888  FairCandySwap,  Stream

--- src/main/java/com/sjia/Leetcode/FairCandySwap.java
+++ src/main/java/com/sjia/Leetcode/FairCandySwap.java
@@ -2,6 +2,7 @@ package com.sjia.Leetcode;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class FairCandySwap {
@@ -25,6 +26,24 @@ public class FairCandySwap {
         }
         return new int[] {-1, -1};
     }
+
+
+    // Stream sum
+    // Runtime: 13 ms, faster than 55.60% of Java online submissions for Fair Candy Swap.
+    //Memory Usage: 41.8 MB, less than 31.71% of Java online submissions for Fair Candy Swap.
+/*    public int[] fairCandySwap(int[] A, int[] B) {
+        Set<Integer> setB = new HashSet<>();
+        int diff = (IntStream.of(B).sum() - IntStream.of(A).sum() ) / 2;
+        for (int i: B) setB.add(i);
+        for (int i : A) {
+            if (setB.contains(i + diff)) {
+                return new int[]{i, i + diff};
+            }
+        }
+        return new int[] {-1};
+    }*/
+
+
 
 
 }
