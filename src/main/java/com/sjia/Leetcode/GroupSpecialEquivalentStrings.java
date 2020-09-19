de3cf3f576fe7bb996f310017355e86b0b49487a
Simon Jia <sjia@nisum.com>
2020-09-18 17:58:01 -0700

    Leetcode #893 GroupSpecialEquivalentStrings, ArrayList sort

--- src/main/java/com/sjia/Leetcode/GroupSpecialEquivalentStrings.java
+++ src/main/java/com/sjia/Leetcode/GroupSpecialEquivalentStrings.java
@@ -1,16 +1,18 @@
 package com.sjia.Leetcode;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class GroupSpecialEquivalentStrings {
     // #893 https://leetcode.com/problems/groups-of-special-equivalent-strings/
+    // 1 <= A.length <= 1000
+    // 1 <= A[i].length <= 20
+    // All A[i] have the same length.
+    // All A[i] consist of only lowercase letters.
 
 
     // Runtime: 14 ms, faster than 46.47% of Java online submissions for Groups of Special-Equivalent Strings.
     //Memory Usage: 39.6 MB, less than 56.90% of Java online submissions for Groups of Special-Equivalent Strings.
-    public int numSpecialEquivGroups(String[] A) {
+    /*public int numSpecialEquivGroups(String[] A) {
         Set<String> tmp = new HashSet<>();
         for (String str : A) {
             int[] odd = new int[26];
@@ -24,7 +26,52 @@ public class GroupSpecialEquivalentStrings {
         }
         System.out.println(tmp);
         return tmp.size();
+    }*/
+
+
+    // Runtime: 11 ms, faster than 63.55% of Java online submissions for Groups of Special-Equivalent Strings.
+    //Memory Usage: 39.4 MB, less than 69.07% of Java online submissions for Groups of Special-Equivalent Strings.
+    public int numSpecialEquivGroups(String[] A) {
+        Set<String> tmp = new HashSet<>();
+        for (String str : A) {
+            List<Character> odd = new ArrayList<>();
+            List<Character> even = new ArrayList<>();
+            int i=0;
+            for (char c : str.toCharArray()) {
+                if (i++%2==1) odd.add(c);
+                else even.add(c);
+            }
+            Collections.sort(odd);
+            Collections.sort(even);
+            String key = String.valueOf(odd) + String.valueOf(even);
+            tmp.add(key);
+        }
+        System.out.println(tmp);
+        return tmp.size();
     }
+
+
+    // Runtime: 12 ms, faster than 62.41% of Java online submissions for Groups of Special-Equivalent Strings.
+    //Memory Usage: 39.9 MB, less than 40.69% of Java online submissions for Groups of Special-Equivalent Strings.
+/*    public int numSpecialEquivGroups(String[] A) {
+        Set<String> tmp = new HashSet<>();
+        for (String str : A) {
+            List<Character> odd = new ArrayList<>();
+            List<Character> even = new ArrayList<>();
+            for (int i=0; i<str.length(); i++) {
+                if (i%2==1) odd.add(str.charAt(i));
+                else even.add(str.charAt(i));
+            }
+            Collections.sort(odd);
+            Collections.sort(even);
+            String key = String.valueOf(odd) + String.valueOf(even);
+            tmp.add(key);
+        }
+        System.out.println(tmp);
+        return tmp.size();
+    }*/
+
+
 
     public static void main(String[] args) {
         String[] testData  = new String[] {"abcd","cdab","cbad","xyzz","zzxy","zzyx"};
