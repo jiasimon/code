4ca543712e2adcfb2ac3b4823b25c02b03512775
Simon Jia <sjia@nisum.com>
2020-09-22 07:03:18 -0700

    Leetcode #908 Stream, loop Math.min max

--- src/main/java/com/sjia/Leetcode/SmallestRange.java
+++ src/main/java/com/sjia/Leetcode/SmallestRange.java
@@ -16,12 +16,12 @@ public class SmallestRange {
     // Stream
     // Runtime: 3 ms, faster than 43.40% of Java online submissions for Smallest Range I.
     // Memory Usage: 39.5 MB, less than 98.78% of Java online submissions for Smallest Range I.
-    public int smallestRangeI(int[] A, int K) {
+/*    public int smallestRangeI(int[] A, int K) {
         int minA = Arrays.stream(A).min().getAsInt();
         int maxA = Arrays.stream(A).max().getAsInt();
         if (maxA- minA > K*2) return maxA - minA - K*2;
         return 0;
-    }
+    }*/
 
 
     // Runtime: 3 ms, faster than 43.40% of Java online submissions for Smallest Range I.
@@ -33,15 +33,16 @@ public class SmallestRange {
     }*/
 
 
+    // Math.min
     // Runtime: 2 ms, faster than 86.28% of Java online submissions for Smallest Range I.
     // Memory Usage: 39.7 MB, less than 88.37% of Java online submissions for Smallest Range I.
- /*   public int smallestRangeI(int[] A, int K) {
+    public int smallestRangeI(int[] A, int K) {
         int minA=A[0], maxA = A[0];
         for (int a : A) {
             minA = Math.min(a, minA);
             maxA = Math.max(a, maxA);
         }
         return Math.max(maxA - minA - K*2 , 0);
-    }*/
+    }
 
 }
