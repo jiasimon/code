8dc5dadd0c905fee309a92bf9cefdac0bb453e05
Simon Jia <sjia@nisum.com>
2020-09-23 08:20:18 -0700

    Leetcode #917 ReverseOnlyLetters  two pointers, StringBuilder

--- src/main/java/com/sjia/Leetcode/ReverseOnlyLetters.java
+++ src/main/java/com/sjia/Leetcode/ReverseOnlyLetters.java
@@ -14,7 +14,7 @@ public class ReverseOnlyLetters {
 
     // Runtime: 5 ms, faster than 8.00% of Java online submissions for Reverse Only Letters.
     //Memory Usage: 37.8 MB, less than 48.67% of Java online submissions for Reverse Only Letters.
-    public String reverseOnlyLetters(String S) {
+/*    public String reverseOnlyLetters(String S) {
         int j=S.length()-1;
         String res ="";
         for (int i=0; i < S.length(); i++) {
@@ -28,8 +28,46 @@ public class ReverseOnlyLetters {
             }
         }
         return res;
+    }*/
+
+
+    // Runtime: 0 ms, faster than 100.00% of Java online submissions for Reverse Only Letters.
+    // Memory Usage: 37.6 MB, less than 68.78% of Java online submissions for Reverse Only Letters.
+    public String reverseOnlyLetters(String S) {
+        int j=S.length()-1;
+        StringBuilder res = new StringBuilder();
+        for (int i=0; i < S.length(); i++) {
+            if(Character.isLetter(S.charAt(i))) {
+                while (!Character.isLetter(S.charAt(j))) {
+                    j--;
+                }
+                res.append(S.charAt(j--));
+            } else {
+                res.append(S.charAt(i)) ;
+            }
+        }
+        return res.toString();
     }
 
+
+    // Runtime: 0 ms, faster than 100.00% of Java online submissions for Reverse Only Letters.
+    //Memory Usage: 37.4 MB, less than 82.84% of Java online submissions for Reverse Only Letters.
+    public String reverseOnlyLetters(String S) {
+        int j=S.length()-1;
+        StringBuilder res = new StringBuilder();
+        char[] tmp = S.toCharArray();
+        for (int i=0; i < S.length(); i++) {
+            if(Character.isLetter(tmp[i])) {
+                while (!Character.isLetter(tmp[j])) {
+                    j--;
+                }
+                res.append(tmp[j--]);
+            } else {
+                res.append(tmp[i]) ;
+            }
+        }
+        return res.toString();
+    }
 
 
 }
