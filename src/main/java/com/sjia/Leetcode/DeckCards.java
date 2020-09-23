f00555db815fc6040ea7c59a85ea83eb097e33a6
Simon Jia <sjia@nisum.com>
2020-09-23 07:53:43 -0700

    Leetcode #914 DeckCards  new int[10000] gcd faster

--- src/main/java/com/sjia/Leetcode/DeckCards.java
+++ src/main/java/com/sjia/Leetcode/DeckCards.java
@@ -8,7 +8,7 @@ public class DeckCards {
 
     // Runtime: 11 ms, faster than 20.44% of Java online submissions for X of a Kind in a Deck of Cards.
     //Memory Usage: 49.3 MB, less than 17.44% of Java online submissions for X of a Kind in a Deck of Cards.
-    public boolean hasGroupsSizeX(int[] deck) {
+/*    public boolean hasGroupsSizeX(int[] deck) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int x : deck) {
             map.put(x,map.getOrDefault(x,0)+1);
@@ -24,7 +24,7 @@ public class DeckCards {
     private int gcd(int a, int b) {
         if (b==0) return a;
         else return gcd(b, a%b);
-    }
+    }*/
 
 
     // Runtime: 7 ms, faster than 67.23% of Java online submissions for X of a Kind in a Deck of Cards.
@@ -32,6 +32,26 @@ public class DeckCards {
 /*    private int gcd(int a, int b) {
         return b==0 ? a : gcd (b, a%b);
     }*/
+
+
+    // Runtime: 2 ms, faster than 99.01% of Java online submissions for X of a Kind in a Deck of Cards.
+    // Memory Usage: 40.2 MB, less than 36.44% of Java online submissions for X of a Kind in a Deck of Cards.
+    public boolean hasGroupsSizeX(int[] deck) {
+        int[] count = new int[10000];
+        for (int x : deck) {
+            count[x]++;
+        }
+        int result = count[0];
+        for (int i=1; i < count.length; i++) {
+            result = gcd(result, count[i]);
+            if (result ==1 ) return false;
+        }
+        return result > 1;
+    }
+
+    private int gcd(int a, int b) {
+        return b==0 ? a : gcd (b, a%b);
+    }
 
 
 
