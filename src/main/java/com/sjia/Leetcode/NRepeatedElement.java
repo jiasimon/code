b049cfc881dbfabd8567375e2bbf706287c953e1
Simon Jia <sjia@nisum.com>
2020-10-01 15:23:04 -0700

    Leetcode #961 NRepeatedElement, HashSet

---
+++ src/main/java/com/sjia/Leetcode/NRepeatedElement.java
@@ -0,0 +1,26 @@
+package com.sjia.Leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class NRepeatedElement {
+    // #961 https://leetcode.com/problems/n-repeated-element-in-size-2n-array/
+    // Return the element repeated N times.
+    // 4 <= A.length <= 10000
+    // 0 <= A[i] < 10000
+    // A.length is even
+
+
+    // Runtime: 0 ms, faster than 100.00% of Java online submissions for N-Repeated Element in Size 2N Array.
+    //Memory Usage: 39.7 MB, less than 92.72% of Java online submissions for N-Repeated Element in Size 2N Array.
+    public int repeatedNTimes(int[] A) {
+        Set<Integer> tmp = new HashSet<>();
+        for (Integer n : A) {
+            if(tmp.contains(n)) return n;
+            else tmp.add(n);
+        }
+        return 0;
+    }
+
+
+}
