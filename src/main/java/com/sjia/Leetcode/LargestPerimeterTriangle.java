f2119e688baf6898f6cd0fc8feb45e3ff10d9701
Simon Jia <sjia@nisum.com>
2020-10-03 08:03:02 -0700

    Leetcode #976 LargestPerimeterTriangle return without tmp variable

--- src/main/java/com/sjia/Leetcode/LargestPerimeterTriangle.java
+++ src/main/java/com/sjia/Leetcode/LargestPerimeterTriangle.java
@@ -9,6 +9,8 @@ public class LargestPerimeterTriangle {
 
     // Runtime: 11 ms, faster than 10.36% of Java online submissions for Largest Perimeter Triangle.
     // Memory Usage: 52.2 MB, less than 6.61% of Java online submissions for Largest Perimeter Triangle.
+    // Second run: Runtime: 7 ms, faster than 58.45% of Java online submissions for Largest Perimeter Triangle.
+    //Memory Usage: 39.5 MB, less than 78.34% of Java online submissions for Largest Perimeter Triangle.
     public int largestPerimeter(int[] A) {
         int res =0;
         Arrays.sort(A);
@@ -20,5 +22,21 @@ public class LargestPerimeterTriangle {
         }
         return res;
     }
+
+
+
+    // Runtime: 6 ms, faster than 99.81% of Java online submissions for Largest Perimeter Triangle.
+    //Memory Usage: 39.9 MB, less than 52.60% of Java online submissions for Largest Perimeter Triangle.
+/*    public int largestPerimeter(int[] A) {
+        Arrays.sort(A);
+        for (int i = A.length-1; i>=2; i--){
+            if (A[i-2] + A[i-1] > A[i]) {
+                return A[i-2] + A[i-1] + A[i];
+            }
+        }
+        return 0;
+    }*/
+
+
 
 }
