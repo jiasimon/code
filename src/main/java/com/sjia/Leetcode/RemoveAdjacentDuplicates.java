6316a7a7085a292ca45aae6cf9b03ae2d7a670f5
Simon Jia <sjia@nisum.com>
2020-10-20 08:16:54 -0700

    Leetcode #1047 RemoveAdjacentDuplicates, two pointers  if( j > 0 && res[j]==res[j-1])

--- src/main/java/com/sjia/Leetcode/RemoveAdjacentDuplicates.java
+++ src/main/java/com/sjia/Leetcode/RemoveAdjacentDuplicates.java
@@ -10,7 +10,7 @@ public class RemoveAdjacentDuplicates {
     // sb.deleteCharAt(sb.length()-1)
     // Runtime: 7 ms, faster than 88.93% of Java online submissions for Remove All Adjacent Duplicates In String.
     //Memory Usage: 39.8 MB, less than 5.06% of Java online submissions for Remove All Adjacent Duplicates In String.
-    public String removeDuplicates(String S) {
+/*    public String removeDuplicates(String S) {
         StringBuilder sb = new StringBuilder();
         for (char c: S.toCharArray()) {
             if(sb.length()>0 && c== sb.charAt(sb.length()-1)) {
@@ -20,6 +20,24 @@ public class RemoveAdjacentDuplicates {
             }
         }
         return sb.toString();
+    }*/
+
+
+
+    // two pointers, i for original
+    // Runtime: 3 ms, faster than 99.58% of Java online submissions for Remove All Adjacent Duplicates In String.
+    //Memory Usage: 39.5 MB, less than 5.06% of Java online submissions for Remove All Adjacent Duplicates In String.
+    public String removeDuplicates(String S) {
+        int j=0, n=S.length();
+        char[] res = S.toCharArray();
+        for (int i=0; i<n; i++, j++) {
+            res[j] = res[i];
+            if( j > 0 && res[j]==res[j-1]) {  // when duplicate =2
+                j -= 2;
+            }
+        }
+        return new String(res,0,j);
     }
+
 
 }
