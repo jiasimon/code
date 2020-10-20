1563ef2fd0acec79ccd6a480e831f6b3219c68cf
Simon Jia <sjia@nisum.com>
2020-10-20 07:32:56 -0700

    Leetcode #1047 RemoveAdjacentDuplicates, StringBuilder, sb.deleteCharAt(sb.length()-1)

---
+++ src/main/java/com/sjia/Leetcode/RemoveAdjacentDuplicates.java
@@ -0,0 +1,25 @@
+package com.sjia.Leetcode;
+
+public class RemoveAdjacentDuplicates {
+    // #1047 https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+    // Given a string S of lowercase letters,
+    // a duplicate removal consists of choosing two adjacent and equal letters, and removing them.
+    // Input: "abbaca"  Output: "ca"
+
+
+    // sb.deleteCharAt(sb.length()-1)
+    // Runtime: 7 ms, faster than 88.93% of Java online submissions for Remove All Adjacent Duplicates In String.
+    //Memory Usage: 39.8 MB, less than 5.06% of Java online submissions for Remove All Adjacent Duplicates In String.
+    public String removeDuplicates(String S) {
+        StringBuilder sb = new StringBuilder();
+        for (char c: S.toCharArray()) {
+            if(sb.length()>0 && c== sb.charAt(sb.length()-1)) {
+                sb.deleteCharAt(sb.length()-1);
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+}
