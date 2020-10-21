11e05f3ef2ee50c4929d21c03b29d4983f10edcd
Simon Jia <sjia@nisum.com>
2020-10-20 20:10:59 -0700

    Leetcode #2 AddTwoNumbers dummy ListNode

--- src/main/java/com/sjia/Leetcode/AddTwoNumbers.java
+++ src/main/java/com/sjia/Leetcode/AddTwoNumbers.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 
 public class AddTwoNumbers {
     // #2 https://leetcode.com/problems/add-two-numbers/
+    // Input: l1 = [2,4,3], l2 = [5,6,4]    Output: [7,0,8], 342 + 465 = 807
+    // l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9] [8,9,9,9,0,0,0,1]
 
     static int[] addTwoNumbers(int[] input1, int[] input2) {
         int size = input1.length + 1 ;
@@ -50,6 +52,28 @@ public class AddTwoNumbers {
         System.out.println("The result is: " + Arrays.toString( result) );
 
     }
+
+
+    // Runtime: 1 ms, faster than 100.00% of Java online submissions for Add Two Numbers.
+    // Memory Usage: 39.3 MB, less than 17.05% of Java online submissions for Add Two Numbers.
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+        int carry =0;
+        while (l1 != null || l2 != null) {
+            int d1 = l1 == null ? 0 : l1.val;
+            int d2 = l2 == null ? 0 : l2.val;
+            int sum =d1 + d2 + carry;
+            carry = sum /10 ;
+            cur.next = new ListNode (sum %10);
+            cur = cur.next;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        if( carry ==1) cur.next = new ListNode(1);
+        return dummy.next;
+    }
+
 
 /*    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(-1);
