647a93620cbe020402ee2af8c98be99bbc0e032e
Simon Jia <sjia@nisum.com>
2020-10-24 10:26:12 -0700

    Leetcode #11  ContainerMostWater  two pointers

--- src/main/java/com/sjia/Leetcode/ContainerMostWater.java
+++ src/main/java/com/sjia/Leetcode/ContainerMostWater.java
@@ -2,12 +2,32 @@ package com.sjia.Leetcode;
 
 public class ContainerMostWater {
 
+    // #11 https://leetcode.com/problems/container-with-most-water/
+
     //Given n non-negative integers a1, a2, ..., an ,  n>=2
     // where each represents a point at coordinate (i, ai).
     // n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0).
     // Find two lines, which together with x-axis forms a container,
     // such that the container contains the most water.
 
+
+    // Runtime: 3 ms, faster than 41.47% of Java online submissions for Container With Most Water.
+    //Memory Usage: 40.5 MB, less than 13.80% of Java online submissions for Container With Most Water.
+    public int maxArea(int[] height) {
+        int l=0, r=height.length-1;
+        int res =0;
+        while ( l < r) {
+            res = Math.max(res, Math.min(height[l], height[r])*(r - l));
+            if (height[l] < height[r]) l++;
+            else r--;
+        }
+        return res;
+
+    }
+
+
+
+/*
     static int maxArea(int[] container) {
         int result = 0, i = 0, j = container.length - 1;
         while (i < j) {
@@ -17,7 +37,7 @@ public class ContainerMostWater {
         }
         return result;
 
-    }
+    }*/
 
 
 
@@ -26,7 +46,8 @@ public class ContainerMostWater {
         //int[] testData  = {2,5}
         //int[] testData  = {1,8,6,2,5,4,8,3,7};
         int[] testData  = {1,8,6,2,5,4,8,3,7,5};
-        int result = maxArea(testData);
+        ContainerMostWater solution = new ContainerMostWater();
+        int result = solution.maxArea(testData);
 
         System.out.printf(" max water is %d ", result);
     }
