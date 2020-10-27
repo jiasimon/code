d28b3712afdab7299b7ceebcacf7d5a48cc15e1b
Simon Jia <sjia@nisum.com>
2020-10-27 12:43:30 -0700

    Leetcode #961 ThreeSum,  without IDE

--- src/main/java/com/sjia/Leetcode/ThreeSum.java
+++ src/main/java/com/sjia/Leetcode/ThreeSum.java
@@ -10,6 +10,12 @@ public class ThreeSum {
     // Given an array nums of n integers, are there elements a, b, c in nums
     // such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
 
+
+    // 0 <= nums.length <= 3000
+    // -10^5 <= nums[i] <= 10^5
+
+
+
     //ArrayList, Runtime: 32 ms, faster than 51.21% of Java online submissions for 3Sum.
     //Memory Usage: 56.5 MB, less than 9.93% of Java online submissions for 3Sum.
 
@@ -22,7 +28,7 @@ public class ThreeSum {
 
     // if (num[0] > 0 || num[num.length -1] < 0 ) return new ArrayList<>();
     // Memory Usage: 43.4 MB, less than 74.51% of Java online submissions for 3Sum.
-
+/*
     static List<List<Integer>> threeSum(int[] num) {
         if (num.length < 2 ) return new ArrayList<>();
         Arrays.sort(num);
@@ -46,8 +52,36 @@ public class ThreeSum {
         }
         return res;
 
-    }
+    }*/
 
+
+
+
+    // Runtime: 20 ms, faster than 68.84% of Java online submissions for 3Sum.
+    //Memory Usage: 42.8 MB, less than 5.08% of Java online submissions for 3Sum.
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < n - 2; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) continue;
+            int l = i + 1, r = n - 1;
+            while (l < r) {
+                if (nums[l] + nums[r] == 0 - nums[i]) {
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    while (l < r && nums[l + 1] == nums[l]) l++;
+                    while (l < r && nums[r - 1] == nums[r]) r--;
+                    l++;
+                    r--;
+                } else if (nums[l] + nums[r] < 0 - nums[i]) {
+                    l++;
+                } else  {
+                    r--;
+                }
+            }
+        }
+        return res;
+    }
 
 
 
@@ -57,8 +91,8 @@ public class ThreeSum {
         // {-1, 0, 1, 2, -1, -4}
         // [ -1 -1 2 ][ -1 0 1 ]
 
-        int[] testData = {1, 1, 2, 4 ,8 ,10};
-        //int[] testData  = {-1, 0, 1, 2, -1, -4};
+        // int[] testData = {1, 1, 2, 4 ,8 ,10};
+        int[] testData  = {-1, 0, 1, 2, -1, -4};
         List<List<Integer>> result = threeSum(testData);
 
         System.out.printf(" Three sum result is %s \n", result);
