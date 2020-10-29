caeec9a4116f20b81a644ef3cf29a760a8836e25
Simon Jia <sjia@nisum.com>
2020-10-29 15:39:02 -0700

    Karat , CommonAncestor  list1.stream().anyMatch(list2::contains)

--- src/main/java/com/sjia/Leetcode/CommonAncestor.java
+++ src/main/java/com/sjia/Leetcode/CommonAncestor.java
@@ -63,57 +63,78 @@ hasCommonAncestor(parentChildPairs2, 1, 12) => false
 */
 
 
-
-    public boolean hasCommonAncestor( int[][] input, int c1, int c2) {
+    public static boolean hasCommonAncestor(int[][] input, int c1, int c2) {
         HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
-        for ( int[] row : input ) {
-            if (! map.containsKey (row[1])) {
+        for (int[] row : input) {
+            if (!map.containsKey(row[1])) {
                 ArrayList<Integer> tmp = new ArrayList<>();
                 tmp.add(row[0]);
-                map.put(row[1],tmp);
+                map.put(row[1], tmp);
             } else {
                 map.get(row[1]).add(row[0]);
             }
         }
-        System.out.println(map);
-        int current = c1;
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        getAncestor(map, current, list);
+        // System.out.println(map);
 
-        System.out.println(map);
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        getAncestor(map, c1, list1);
+        System.out.println("list1 is : " + list1);
 
-        return true;
+        ArrayList<Integer> list2 = new ArrayList<Integer>();
+        getAncestor(map, c2, list2);
+
+        System.out.println("list2 is : " + list2);
+
+        return list1.stream().anyMatch(list2::contains);
 
 
     }
 
 
-    public ArrayList<Integer> getAncestor(HashMap<Integer, ArrayList<Integer>> map , int c, ArrayList<Integer> list) {
-        if (map.containsKey(c)) {
-            for ( Integer k : map.get(c)) {
-                list.add(k);
-                getAncestor(map, k, list);
-            }
+    private static void getAncestor(HashMap<Integer, ArrayList<Integer>> map, int c, ArrayList<Integer> list) {
+        if (!map.containsKey(c)) {
+            // System.out.println(list);
+            return;
         }
-        System.out.println(list);
-        return list;
+
+        for (Integer k : map.get(c)) {
+            list.add(k);
+            getAncestor(map, k, list);
+        }
 
     }
 
 
     public static void main(String[] argv) {
-        int[][] parentChildPairs1 = new int[][] {
+        int[][] parentChildPairs1 = new int[][]{
                 {1, 3}, {2, 3}, {3, 6}, {5, 6}, {5, 7}, {4, 5},
                 {4, 8}, {4, 9}, {9, 11}, {14, 4}, {13, 12}, {12, 9}
         };
 
-        int[][] parentChildPairs2 = new int[][] {
+        int[][] parentChildPairs2 = new int[][]{
                 {1, 3}, {11, 10}, {11, 12}, {2, 3}, {10, 2},
                 {10, 5}, {3, 4}, {5, 6}, {5, 7}, {7, 8}
         };
 
-        CommonAncestor solution = new CommonAncestor();
-        solution.hasCommonAncestor(parentChildPairs2, 4, 12);
+        // Solution solution = new Solution();
+/*        hasCommonAncestor(parentChildPairs2, 4, 12);
+        System.out.println("4, 12 CommonAncestor is: " + hasCommonAncestor(parentChildPairs2, 4, 12));
+
+        System.out.println("1, 6 CommonAncestor is: " + hasCommonAncestor(parentChildPairs2, 1, 6));
+
+        System.out.println("1, 12 CommonAncestor is: " + hasCommonAncestor(parentChildPairs2, 1, 12));
+
+        // parentChildPairs1
+        System.out.println("3, 8 CommonAncestor is: " + hasCommonAncestor(parentChildPairs1, 3, 8));
+
+        System.out.println("5, 8 CommonAncestor is: " +
+                hasCommonAncestor(parentChildPairs1, 5, 8));
+
+        System.out.println("6, 8 CommonAncestor is: " +
+                hasCommonAncestor(parentChildPairs1, 6, 8));*/
+
+        System.out.println("4, 14 CommonAncestor is: " +
+                hasCommonAncestor(parentChildPairs1, 4, 14));
 
     }
 
