142479390165336a9285b3b140f3b34aaff0f394
Simon Jia <sjia@nisum.com>
2020-11-04 09:10:03 -0800

    Leetcode #39 CombinationSum2, if (i > start && nums[i] == nums[i-1])

--- src/main/java/com/sjia/Leetcode/CombinationSum2.java
+++ src/main/java/com/sjia/Leetcode/CombinationSum2.java
@@ -5,13 +5,42 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CombinationSum2 {
+    // Each number in candidates may only be used once in the combination.
 
     // #40 https://leetcode.com/problems/combination-sum-ii/
     // Each number in candidates may only be used once in the combination.
 
+
+
+
+    // backtrack i+1
+    // Runtime: 2 ms, faster than 98.50% of Java online submissions for Combination Sum II.
+    //Memory Usage: 39.3 MB, less than 10.59% of Java online submissions for Combination Sum II.
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(res, new ArrayList<>(), candidates,target, 0);
+        return res;
+    }
+
+    private void backtrack(List<List<Integer>> res, ArrayList<Integer> tmp, int[] nums, int target, int start) {
+        if (target == 0) {
+            res.add(new ArrayList<> (tmp));
+        } else {
+            for ( int i = start; i < nums.length; i++) {
+                if (nums[i] > target) break;
+                if (i > start && nums[i] == nums[i-1]) continue;
+                tmp.add(nums[i]);
+                backtrack(res, tmp, nums, target - nums[i], i+1);
+                tmp.remove(tmp.size() - 1);
+            }
+        }
+    }
+
+
     // Runtime: 3 ms, faster than 84.34% of Java online submissions for Combination Sum II.
     //Memory Usage: 39.6 MB, less than 66.59% of Java online submissions for Combination Sum II.
-
+/*
     public List<List<Integer>> combinationSum2(int[] nums, int target) {
         List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
@@ -35,7 +64,7 @@ public class CombinationSum2 {
                 tmp.remove(tmp.size()-1);
             }
         }
-    }
+    }*/
 
 
     public static void main(String[] args) {
