432875335a9b04d93b206862c7683eecfd9d0c74
Simon Jia <sjia@nisum.com>
2020-11-04 20:21:50 -0800

    Leetcode #42 TrappingRainWater  leftmax = Math.max(leftmax, height[a])

---
+++ src/main/java/com/sjia/Leetcode/TrappingRainWater.java
@@ -0,0 +1,34 @@
+package com.sjia.Leetcode;
+
+public class TrappingRainWater {
+    // #42 https://leetcode.com/problems/trapping-rain-water/
+
+    // n == height.length
+    //0 <= n <= 3 * 104
+    //0 <= height[i] <= 105
+
+
+    // Runtime: 1 ms, faster than 87.43% of Java online submissions for Trapping Rain Water.
+    //Memory Usage: 38.3 MB, less than 6.63% of Java online submissions for Trapping Rain Water.
+    public int trap(int[] height){
+        int a = 0;
+        int b = height.length - 1;
+        int max = 0;
+        int leftmax = 0;
+        int rightmax = 0;
+        while (a <= b) {
+            leftmax = Math.max(leftmax, height[a]);
+            rightmax = Math.max(rightmax, height[b]);
+            if (leftmax < rightmax) {
+                max += (leftmax - height[a]);   // leftmax is smaller than rightmax, so the (leftmax-A[a]) water can be stored
+                a++;
+            } else {
+                max += (rightmax - height[b]);
+                b--;
+            }
+        }
+        return max;
+    }
+
+
+}
