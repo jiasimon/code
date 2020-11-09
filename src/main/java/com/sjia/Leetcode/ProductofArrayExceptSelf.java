20d9014d6d29796b5ed18a7ecf8463a4c40e45dc
Simon Jia <sjia@nisum.com>
2020-11-09 09:25:16 -0800

    Leetcode #238 #fb ProductofArrayExceptSelf, total / nums[i] if no 0 element, left side multiple right side

--- src/main/java/com/sjia/Leetcode/ProductofArrayExceptSelf.java
+++ src/main/java/com/sjia/Leetcode/ProductofArrayExceptSelf.java
@@ -4,8 +4,15 @@ import java.util.Arrays;
 
 public class ProductofArrayExceptSelf {
     // #238 https://leetcode.com/problems/product-of-array-except-self/ #fb
+    // Given an array nums of n integers where n > 1,
+    // return the product of all the elements of nums except nums[i]
+    // Note: Please solve it without division and in O(n).
+
+    // Input:  [1,2,3,4]    Output: [24,12,8,6]
 
 
+    // Runtime: 1 ms, faster than 99.99% of Java online submissions for Product of Array Except Self.
+    // Memory Usage: 50.2 MB, less than 22.63% of Java online submissions for Product of Array Except Self.
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
         int[] res = new int[n];
@@ -20,6 +27,20 @@ public class ProductofArrayExceptSelf {
         }
         return res;
     }
+
+/*
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length, total=1;
+        int[] res = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            total *= nums[i];
+        }
+        for (int i = 0; i < n; i++) {
+            res[i] = total / nums[i];
+        }
+        return res;
+    }*/
 
     public static void main(String[] args) {
         int[] testData = {1,2,3,4,5};
