07c79d87a8b5d9e318d673937c584a5644b55883
Simon Jia <sjia@nisum.com>
2020-11-22 09:53:54 -0800

    Leetcode #139 WordBreak, #fb #dp  list to HashSet

--- src/main/java/com/sjia/Leetcode/WordBreak.java
+++ src/main/java/com/sjia/Leetcode/WordBreak.java
@@ -3,11 +3,33 @@ package com.sjia.Leetcode;
 import java.util.*;
 
 public class WordBreak {
-    // #139 https://leetcode.com/problems/word-break/   #fb
+    // #139 https://leetcode.com/problems/word-break/   #fb #dp
 
     // dp[i] 表示字符串 s 前 i 个字符组成的字符串 s[0..i−1]是否能被空格拆分成若干个字典中出现的单词
 
+    // Input: s = "applepenapple", wordDict = ["apple", "pen"]
+    // Output: true     Note that you are allowed to reuse a dictionary word
+
+
+    // list to Hashset, dynamic Programming
+    // Runtime: 6 ms, faster than 60.71% of Java online submissions for Word Break.
+    //Memory Usage: 39.4 MB, less than 43.23% of Java online submissions for Word Break.
     public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordSet = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() +1];
+        dp[0] = true;
+        for (int i=1; i< s.length() + 1 ; i++) {
+            for (int j=0; j < i; j++) {
+                if(dp[j] && wordSet.contains(s.substring(j,i))){
+                    dp[i] = true;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+
+
+    public boolean wordBreak2(String s, List<String> wordDict) {
         Set<String> wordDictSet = new HashSet(wordDict);
         boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;
