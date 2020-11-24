4be762109453221f8a3b8a88e96390c9dc4480b0
Simon Jia <sjia@nisum.com>
2020-11-24 13:27:07 -0800

    Leetcode #88 MergeSortedArray, #fb two pointers, from last to start

--- src/main/java/com/sjia/Leetcode/MergeSortedArray.java
+++ src/main/java/com/sjia/Leetcode/MergeSortedArray.java
@@ -5,16 +5,20 @@ import java.util.List;
 
 public class MergeSortedArray {
 
-    // #88 https://leetcode.com/problems/merge-sorted-array/
+    // #88 https://leetcode.com/problems/merge-sorted-array/    #fb
+    // Given two sorted integer arrays nums1 and nums2
+    // merge nums2 into nums1 as one sorted array
+    // The number of elements initialized in nums1 and nums2 are m and n
+    // nums1 has enough space (size that is equal to m + n) to hold additional elements from nums2
+    // -10^9 <= nums1[i], nums2[i] <= 10^9
+    // nums1.length == m + n
+    // nums2.length == n
 
-    //Runtime: 1 ms, faster than 20.93% of Java online submissions for Merge Sorted Array.
-    //Memory Usage: 40.3 MB, less than 5.07% of Java online submissions for Merge Sorted Array.
 
 
 
-
-
-
+    // Two pointers, from end to begin
+    // time: O(n+m)  space:O(1)
     // Runtime: 0 ms, faster than 100.00% of Java online submissions for Merge Sorted Array.
     //Memory Usage: 39.8 MB, less than 18.65% of Java online submissions for Merge Sorted Array.
 
@@ -46,12 +50,26 @@ public class MergeSortedArray {
     }
 
 
+
+    // Runtime: 0 ms, faster than 100.00% of Java online submissions for Merge Sorted Array.
+    //Memory Usage: 39.4 MB, less than 10.42% of Java online submissions for Merge Sorted Array.
+    public void merge3(int[] nums1, int m, int[] nums2, int n) {
+        int i = m-1, j = n-1 , k = m + n - 1;
+        while (i>=0 && j>=0) {
+            if(nums1[i] > nums2[j]) nums1[k--] = nums1[i--];
+            else nums1[k--] = nums2[j--];
+        }
+        while (j>=0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+
     public static void main(String[] args) {
         int[] m = {1, 2, 3, 0, 0, 0};
         int[] n = {2, 5, 6};
 
         MergeSortedArray solution = new MergeSortedArray();
-        solution.merge(m, 3, n, 3);
+        solution.merge3(m, 3, n, 3);
 
         System.out.printf(" After merge is %s \n", Arrays.toString(m));
     }
