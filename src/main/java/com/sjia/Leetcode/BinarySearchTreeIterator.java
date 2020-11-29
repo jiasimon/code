bec881f939e60f762d39d9aa8a6c46c738b827d0
Simon Jia <sjia@nisum.com>
2020-11-29 08:26:29 -0800

    Leetcode #173 PreOrder Iterator, left-right-root

--- src/main/java/com/sjia/Leetcode/BinarySearchTreeIterator.java
+++ src/main/java/com/sjia/Leetcode/BinarySearchTreeIterator.java
@@ -1,5 +1,6 @@
 package com.sjia.Leetcode;
 
+import java.util.NoSuchElementException;
 import java.util.Stack;
 
 public class BinarySearchTreeIterator {
@@ -76,10 +77,10 @@ public class BinarySearchTreeIterator {
 
 
     // PreOrder Iterator
-    class BSTPreOrderIterator {
+    class PreOrderBSTIterator {
 
         Stack<TreeNode> stack = new Stack<>();
-        public BSTPreOrderIterator(TreeNode root) {
+        public PreOrderBSTIterator(TreeNode root) {
             stack.add(root);
         }
 
@@ -96,6 +97,59 @@ public class BinarySearchTreeIterator {
             return !stack.isEmpty();
         }
 
+    }
+
+
+
+    // Post Order
+    public class PostOrderBinaryTreeIterator {
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+
+        /** find the first leaf in a tree rooted at cur and store intermediate nodes */
+        private void findNextLeaf(TreeNode cur) {
+            while (cur != null) {
+                stack.push(cur);
+                if (cur.left != null) {
+                    cur = cur.left;
+                } else {
+                    cur = cur.right;
+                }
+            }
+        }
+
+        /** Constructor */
+        public PostOrderBinaryTreeIterator(TreeNode root) {
+            findNextLeaf(root);
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public Integer next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException("All nodes have been visited!");
+            }
+
+            TreeNode res = stack.pop();
+            if (!stack.isEmpty()) {
+                TreeNode top = stack.peek();
+                if (res == top.left) {
+                    findNextLeaf(top.right); // find next leaf in right sub-tree
+                }
+            }
+
+            return res.val;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException("remove() is not supported.");
+        }
     }
 
 
