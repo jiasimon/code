ccc3efef8d5c5d00b94d4be84c0e6276c4414384
Simon Jia <sjia@nisum.com>
2023-05-02 09:38:28 -0700

    Leetcode #6, Zigzag convert2

--- src/main/java/com/sjia/Leetcode/ZigZag.java
+++ src/main/java/com/sjia/Leetcode/ZigZag.java
@@ -1,5 +1,16 @@
 package com.sjia.Leetcode;
 
+// 6. Zigzag Conversion  https://leetcode.com/problems/zigzag-conversion/description/
+/*
+        Input: s = "PAYPALISHIRING", numRows = 3
+        Output: "PAHNAPLSIIGYIR"
+
+        Input: s = "PAYPALISHIRING", numRows = 4
+        Output: "PINALSIGYAHRPI"
+
+*/
+
+
 public class ZigZag {
     static String convert(String s, int nRows) {
         if (nRows <= 1) return s;
@@ -15,6 +26,26 @@ public class ZigZag {
         return res;
     }
 
+    public static String convert2(String s, int nRows) {
+        char[] c = s.toCharArray();
+        int len = c.length;
+        StringBuffer[] sb = new StringBuffer[nRows];
+        for (int i = 0; i < sb.length; i++) sb[i] = new StringBuffer();
+
+        int i = 0;
+        while (i < len) {
+            for (int idx = 0; idx < nRows && i < len; idx++) // vertically down
+                sb[idx].append(c[i++]);
+            for (int idx = nRows-2; idx >= 1 && i < len; idx--) // obliquely up
+                sb[idx].append(c[i++]);
+        }
+        for (int idx = 1; idx < sb.length; idx++)
+            sb[0].append(sb[idx]);
+        return sb[0].toString();
+    }
+
+
+
     //0     6        12
     //
     //1   5 7   11   13
@@ -27,9 +58,9 @@ public class ZigZag {
     // n=4, PINALSIGYAHRPI
 
     public static void main(String[] args) {
-        //String testData = "PAYPALISHIRING";
-        String testData2 = "ABCDEFGHIJKLMN";
-        String result = convert(testData2 ,4);
+        String testData = "PAYPALISHIRING";
+//        String testData2 = "ABCDEFGHIJKLMN";
+        String result = convert2(testData ,4);
         System.out.println("Converted Zigzag is: " + result);
     }
 
