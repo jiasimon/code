564ce267470769a169b3e8749c34b561a1dc358a
Simon Jia <sjia@nisum.com>
2023-05-02 14:36:22 -0700

    Leetcode #10, RegularExpressionMatching,  Recursion

--- src/main/java/com/sjia/Leetcode/RegularExpression.java
+++ src/main/java/com/sjia/Leetcode/RegularExpression.java
@@ -74,7 +74,51 @@ public class RegularExpression {
     //
 
 
+    //
 
+    public boolean isMatch2(String s, String p) {
+        return isMatch(0,s,0,p);
+    }
+    private boolean isMatch(int i, String s, int j, String p) {
+        int sn = s.length(), pn = p.length();
+        if(j==pn) { // since * in p can match 0 of previous char, so empty string(i==sn) may match p
+            return i==sn;
+        }
+        char pj = p.charAt(j);
+        if(j+1<pn && p.charAt(j+1)=='*') { //match *, needs to look at the next char to repeate current char
+            if(isMatch(i,s,j+2,p)) {
+                return true;
+            }
+            while(i<sn && (pj == '.'||pj==s.charAt(i))) {
+                if(isMatch(++i,s,j+2,p)) {
+                    return true;
+                }
+            }
+        } else if(i<sn && (s.charAt(i) == pj ||    //match char
+                pj=='.')) {              //match dot
+            return isMatch(i+1, s, j+1, p);
+        }
+        return false;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        // int range:  -2147483648～2147483647
+
+//        String s = "baa";
+//        String p = "ba*";
+
+        String s = "baaccc";
+        String p = "ba*c*";
+
+        RegularExpression solution = new RegularExpression();
+
+        boolean result = solution.isMatch2(s,p);
+
+        System.out.printf(" input %s and %s RegularExpressionMatch: %b ", s,p, result);
+    }
 
 
 
