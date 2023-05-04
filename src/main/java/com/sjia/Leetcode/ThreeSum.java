c560fcbc805be03cfac1f1a0ce62d34ecf065e19
Simon Jia <sjia@nisum.com>
2023-05-04 09:55:14 -0700

    Leetcode #15, ThreeSum,  HashSet

--- src/main/java/com/sjia/Leetcode/ThreeSum.java
+++ src/main/java/com/sjia/Leetcode/ThreeSum.java
@@ -1,17 +1,14 @@
 package com.sjia.Leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class ThreeSum {
-    // #15 https://leetcode.com/problems/3sum/
+    // #15 3Sum, ThreeSum  https://leetcode.com/problems/3sum/
     // Given an array nums of n integers, are there elements a, b, c in nums
     // such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
 
 
-    // 0 <= nums.length <= 3000
+    // 0 <= nums.length <= 3000, this condition updated to 3 <= nums.length <= 3000
     // -10^5 <= nums[i] <= 10^5
 
 
@@ -29,24 +26,26 @@ public class ThreeSum {
     // if (num[0] > 0 || num[num.length -1] < 0 ) return new ArrayList<>();
     // Memory Usage: 43.4 MB, less than 74.51% of Java online submissions for 3Sum.
 /*
-    static List<List<Integer>> threeSum(int[] num) {
-        if (num.length < 2 ) return new ArrayList<>();
-        Arrays.sort(num);
-        if (num[0] > 0 || num[num.length-1] < 0 || (num[0]==0 && num[1]!=0) ||
-                (num[num.length-1] ==0 && num[num.length-2] != 0) ) return new ArrayList<>();
+    static List<List<Integer>> threeSum(int[] nums) {
+        if (nums.length < 2 ) return new ArrayList<>();
+        Arrays.sort(nums);
+        if (nums[0] > 0 || nums[nums.length-1] < 0 ) return new ArrayList<>();
+        int n = nums.length;
         List<List<Integer>> res = new ArrayList<>();
-        for (int i = 0; i < num.length-2; i++) {
-            if (i == 0 || (i > 0 && num[i] != num[i-1])) {
-                int lo = i+1, hi = num.length-1, sum = 0 - num[i];
-                while (lo < hi) {
-                    if (num[lo] + num[hi] == sum) {
-                        res.add(Arrays.asList(num[i], num[lo], num[hi]));
-                        //System.out.printf("[ %d %d %d ]", num[i], num[lo], num[hi]);
-                        while (lo < hi && num[lo] == num[lo+1]) lo++;
-                        while (lo < hi && num[hi] == num[hi-1]) hi--;
-                        lo++; hi--;
-                    } else if (num[lo] + num[hi] < sum) lo++;
-                    else hi--;
+        for (int i = 0; i < n - 2; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) continue;
+            int l = i + 1, r = n - 1;
+            while (l < r) {
+                if (nums[l] + nums[r] == 0 - nums[i]) {
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    while (l < r && nums[l + 1] == nums[l]) l++;
+                    while (l < r && nums[r - 1] == nums[r]) r--;
+                    l++;
+                    r--;
+                } else if (nums[l] + nums[r] < 0 - nums[i]) {
+                    l++;
+                } else  {
+                    r--;
                 }
             }
         }
@@ -85,6 +84,34 @@ public class ThreeSum {
 
 
 
+    // HashSet
+    public static List<List<Integer>> threeSum2(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+
+//        List<List<Integer>> res = new ArrayList<>();
+        Set<List<Integer>> result=new HashSet<>();
+        for (int i=0; i < n-2; i++) {
+            int l = i+1, r = n-1;
+            while (l < r) {
+                if (nums[i] + nums[l] + nums[r] == 0) {
+                    ArrayList<Integer> tmp = new ArrayList<>();
+                    tmp.add(nums[i]);
+                    tmp.add(nums[l]);
+                    tmp.add(nums[r]);
+                    result.add(tmp);
+//                    result.add(Arrays.asList(nums[i],nums[l],nums[r]));
+                    l++;
+                } else if (nums[i] + nums[l] + nums[r] < 0) {
+                    l++;
+                } else r--;
+            }
+        }
+        return new ArrayList<>(result);
+    }
+
+
+
 
     public static void main(String[] args) {
 
@@ -93,7 +120,7 @@ public class ThreeSum {
 
         // int[] testData = {1, 1, 2, 4 ,8 ,10};
         int[] testData  = {-1, 0, 1, 2, -1, -4};
-        List<List<Integer>> result = threeSum(testData);
+        List<List<Integer>> result = threeSum2(testData);
 
         System.out.printf(" Three sum result is %s \n", result);
     }
