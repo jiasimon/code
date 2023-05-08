fcf6379352a80e9ccf6728c232754569781d6b44
Simon Jia <sjia@nisum.com>
2023-05-08 11:56:28 -0700

    Leetcode #21, MergeTwoSortedLists, recursive

--- src/main/java/com/sjia/Leetcode/MergeTwoSortedLists.java
+++ src/main/java/com/sjia/Leetcode/MergeTwoSortedLists.java
@@ -15,6 +15,9 @@ public class MergeTwoSortedLists {
     // Input: 1->2->4, 1->3->4
     //Output: 1->1->2->3->4->4
 
+    // Input: list1 = [], list2 = []
+    // Output: []
+
     //Runtime: 1 ms, faster than 30.42% of Java online submissions for Merge Two Sorted Lists.
     //Memory Usage: 40.1 MB, less than 15.70% of Java online submissions for Merge Two Sorted Lists.
 
@@ -32,7 +35,8 @@ public class MergeTwoSortedLists {
 
     }*/
 
-/*    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    // recursive 
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null) return l2;
         if (l2 == null) return l1;
         if (l1.val < l2.val) {
@@ -41,10 +45,10 @@ public class MergeTwoSortedLists {
         } else {
             l2.next = mergeTwoLists(l1,l2.next);
             return l2;
-        }*/
+        }
 
 
-
+/*
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null || (l2 != null && l1.val > l2.val)) {
             ListNode t = l1; l1 = l2; l2 = t;
@@ -52,7 +56,7 @@ public class MergeTwoSortedLists {
         if (l1 != null) l1.next = mergeTwoLists(l1.next, l2);
         return l1;
 
-    }
+    }*/
 
 
 }
