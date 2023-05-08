8eb94830214522a57088f60a21f86f033b3a7ac8
Simon Jia <sjia@nisum.com>
2023-05-08 08:49:32 -0700

    Leetcode #19, RemoveNodeFromEnd,   two pointer, fast, slow

--- src/main/java/com/sjia/Leetcode/RemoveNodeFromEnd.java
+++ src/main/java/com/sjia/Leetcode/RemoveNodeFromEnd.java
@@ -12,14 +12,24 @@ public class RemoveNodeFromEnd {
     }
 
 
-    // #19 https://leetcode.com/problems/remove-nth-node-from-end-of-list/submissions/
+    // #19 Remove Nth Node From End of List https://leetcode.com/problems/remove-nth-node-from-end-of-list/submissions/
     // Runtime: 0 ms, faster than 100.00% of Java online submissions for Remove Nth Node From End of List.
     //Memory Usage: 39.7 MB, less than 6.50% of Java online submissions for Remove Nth Node From End of List.
 
 
-    //[1,2,3,4,5]
-    //2
+    // [1,2,3,4,5]  n=2
+    // output: [1,2,3,5]
 
+    // Input: head = [1], n = 1
+    // Output: []
+
+    // The number of nodes in the list is sz.
+    // 1 <= sz <= 30
+    // 0 <= Node.val <= 100
+    // 1 <= n <= sz
+
+
+    // two pointer: fast slow
     public ListNode removeNthFromEnd(ListNode head, int n) {
         if(head == null)
             return null;
