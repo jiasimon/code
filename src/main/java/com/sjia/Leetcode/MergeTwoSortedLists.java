722a8eb09d7884fe2a62007e16cf8aab26c79307
Simon Jia <sjia@nisum.com>
2023-05-08 15:05:26 -0700

    Leetcode #21, MergeTwoSortedLists, dummy node

--- src/main/java/com/sjia/Leetcode/MergeTwoSortedLists.java
+++ src/main/java/com/sjia/Leetcode/MergeTwoSortedLists.java
@@ -5,9 +5,18 @@ public class MergeTwoSortedLists {
     public class ListNode {
         int val;
         ListNode next;
-        ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
 
     // #21 Merge Two Sorted Lists
@@ -17,6 +26,16 @@ public class MergeTwoSortedLists {
 
     // Input: list1 = [], list2 = []
     // Output: []
+
+    /*
+    The number of nodes in both lists is in the range [0, 50].
+    -100 <= Node.val <= 100
+    Both list1 and list2 are sorted in non-decreasing order.
+
+     */
+
+
+
 
     //Runtime: 1 ms, faster than 30.42% of Java online submissions for Merge Two Sorted Lists.
     //Memory Usage: 40.1 MB, less than 15.70% of Java online submissions for Merge Two Sorted Lists.
@@ -35,7 +54,7 @@ public class MergeTwoSortedLists {
 
     }*/
 
-    // recursive 
+    // recursive
     private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null) return l2;
         if (l2 == null) return l1;
@@ -43,8 +62,36 @@ public class MergeTwoSortedLists {
             l1.next = mergeTwoLists(l1.next, l2);
             return l1;
         } else {
-            l2.next = mergeTwoLists(l1,l2.next);
+            l2.next = mergeTwoLists(l1, l2.next);
             return l2;
+        }
+    }
+
+
+
+        //  dummy node
+        //  [1,2,4] and [1,3,4], Output [1,1,2,3,4], expected: [1,1,2,3,4,4]
+        private ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+            if (l1 == null) return l2;
+            if (l2 == null) return l1;
+
+            ListNode prehead = new ListNode(-101);
+            ListNode cur = prehead;
+
+            while (l1 != null && l2 != null) {
+                if (l1.val <= l2.val) {
+                    cur.next = l1;
+                    l1 = l1.next;
+                } else {
+                    cur.next = l2;
+                    l2 = l2.next;
+                }
+                cur = cur.next;
+
+            }
+            cur.next = l1 == null ? l2 : l1;
+            return prehead.next;
+
         }
 
 
@@ -57,6 +104,7 @@ public class MergeTwoSortedLists {
         return l1;
 
     }*/
+
 
 
 }
