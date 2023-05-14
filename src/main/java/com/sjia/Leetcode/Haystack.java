31d202fa3afd03dafec846260af2302b1958ed5d
Simon Jia <sjia@nisum.com>
2023-05-13 19:13:12 -0700

    Leetcode #28, KMP, longest_border, lps

--- src/main/java/com/sjia/Leetcode/Haystack.java
+++ src/main/java/com/sjia/Leetcode/Haystack.java
@@ -107,6 +107,75 @@ public class Haystack {
 
 
 
+    // KMP longest_border, lps
+    public int strStr5(String haystack, String needle) {
+
+        int m = needle.length();
+        int n = haystack.length();
+
+        if (n < m)
+            return -1;
+
+        // PREPROCESSING
+        // longest_border array
+        int[] longest_border = new int[m];
+        // Length of Longest Border for prefix before it.
+        int prev = 0;
+        // Iterating from index-1. longest_border[0] will always be 0
+        int i = 1;
+
+        while (i < m) {
+            if (needle.charAt(i) == needle.charAt(prev)) {
+                // Length of Longest Border Increased
+                prev += 1;
+                longest_border[i] = prev;
+                i += 1;
+            } else {
+                // Only empty border exist
+                if (prev == 0) {
+                    longest_border[i] = 0;
+                    i += 1;
+                }
+                // Try finding longest border for this i with reduced prev
+                else {
+                    prev = longest_border[prev - 1];
+                }
+            }
+        }
+
+        // SEARCHING
+        // Pointer for haystack
+        int haystackPointer = 0;
+        // Pointer for needle.
+        // Also indicates number of characters matched in current window.
+        int needlePointer = 0;
+
+        while (haystackPointer < n) {
+            if (haystack.charAt(haystackPointer) == needle.charAt(needlePointer)) {
+                // Matched Increment Both
+                needlePointer += 1;
+                haystackPointer += 1;
+                // All characters matched
+                if (needlePointer == m) {
+                    // m characters behind last matching will be start of window
+                    return haystackPointer - m;
+                }
+            } else {
+                if (needlePointer == 0) {
+                    // Zero Matched
+                    haystackPointer += 1;
+                } else {
+                    // Optimally shift left needlePointer. Don't change haystackPointer
+                    needlePointer = longest_border[needlePointer - 1];
+                }
+            }
+        }
+
+        return -1;
+    }
+
+
+
     public static void main(String[] args) {
         String haystack = "sadbutsad";
         String needle = "db";
