7e77f9c199f3e0e6a3d71b129aa0b672b6f118cf
Simon Jia <sjia@nisum.com>
2023-05-14 19:42:27 -0700

    Leetcode #30, SubstringConcatenation, two-map

---
+++ src/main/java/com/sjia/Leetcode/SubstringConcatenation.java
@@ -0,0 +1,45 @@
+package com.sjia.Leetcode;
+
+import java.util.*;
+
+public class SubstringConcatenation {
+
+
+    // #30 Substring with Concatenation of All Words https://leetcode.com/problems/substring-with-concatenation-of-all-words/
+    // 1 <= s.length <= 104
+    // 1 <= words.length <= 5000
+    // 1 <= words[i].length <= 30
+    // s and words[i] consist of lowercase English letters.
+
+
+    //
+    public List<Integer> findSubstring(String s, String[] words) {
+        final Map<String, Integer> counts = new HashMap<>();
+        for (final String word : words) {
+            counts.put(word, counts.getOrDefault(word, 0) + 1);
+        }
+        final List<Integer> indexes = new ArrayList<>();
+        final int n = s.length(), num = words.length, len = words[0].length();
+        for (int i = 0; i < n - num * len + 1; i++) {
+            final Map<String, Integer> seen = new HashMap<>();
+            int j = 0;
+            while (j < num) {
+                final String word = s.substring(i + j * len, i + (j + 1) * len);
+                if (counts.containsKey(word)) {
+                    seen.put(word, seen.getOrDefault(word, 0) + 1);
+                    if (seen.get(word) > counts.getOrDefault(word, 0)) {
+                        break;
+                    }
+                } else {
+                    break;
+                }
+                j++;
+            }
+            if (j == num) {
+                indexes.add(i);
+            }
+        }
+        return indexes;
+    }
+
+}
