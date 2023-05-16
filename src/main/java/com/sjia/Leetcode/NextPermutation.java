6d57d0f0b561ea860b987626f5f364df5a04f400
Simon Jia <sjia@nisum.com>
2023-05-15 18:58:38 -0700

    Leetcode #31, NextPermutation, nums[i] >= nums[i+1]

--- src/main/java/com/sjia/Leetcode/NextPermutation.java
+++ src/main/java/com/sjia/Leetcode/NextPermutation.java
@@ -11,6 +11,8 @@ public class NextPermutation {
     // {1,2,7,4,3,1}   {0,1,2,5,3,3,0}
     // Input: nums = [1,2,3], Output: [1,3,2]
     // Input: nums = [3,2,1], Output: [1,2,3]
+    // 1 <= nums.length <= 100
+    // 0 <= nums[i] <= 100
 
     // Runtime: 1 ms, faster than 51.91% of Java online submissions for Next Permutation.
     // Memory Usage: 41 MB, less than 5.00% of Java online submissions for Next Permutation.
@@ -21,6 +23,26 @@ public class NextPermutation {
     // 3) reverse i+1 to the end
     // Runtime: 0 ms, faster than 100.00% of Java online submissions for Next Permutation.
     //Memory Usage: 39 MB, less than 75.18% of Java online submissions for Next Permutation.
+    public void nextPermutation2(int[] nums) {
+        if (nums.length <=1) return;
+        int i = nums.length -2;
+        while ( i >=0 && nums[i] >= nums[i+1]) {
+            i--;
+        }
+        if ( i<0 ) reverse(nums,0);
+        else {
+            int j = nums.length - 1;
+            while ( j >=0 && nums[j] <=nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i+1);
+
+
+    }
+
+
     public void nextPermutation(int[] nums) {
         int i = nums.length - 2;
         while (i >= 0 && nums[i] >= nums[i + 1] ) {
@@ -54,9 +76,10 @@ public class NextPermutation {
 
     public static void main(String[] args) {
         NextPermutation solution = new NextPermutation();
-        int[] testData = {1,2,7,4,3,1};
+        int[] testData2 = {1,2,7,4,3,1};
+        int[] testData = {0,1,2,5,3,3,0};
         System.out.println("testData is: " + Arrays.toString(testData));
-        solution.nextPermutation(testData);
+        solution.nextPermutation2(testData);
         System.out.println("next Permutation is: " + Arrays.toString(testData));
     }
 
