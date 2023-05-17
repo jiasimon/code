3ea5c4ff664563b7a647d12a2d572055adc1cc50
Simon Jia <sjia@nisum.com>
2023-05-16 19:58:02 -0700

    Leetcode #36, SudokuSolver, recursive

---
+++ src/main/java/com/sjia/Leetcode/SudokuSolver.java
@@ -0,0 +1,97 @@
+package com.sjia.Leetcode;
+
+public class SudokuSolver {
+    public static void main(String[] args) {
+        char[][] board = {
+                {'5','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}
+        };
+
+
+        char[][] board2 = {
+                {'5','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}
+        };
+
+        solveSudoku(board);
+
+        System.out.println("Sudoku board solution is :");
+        printBoard(board);
+    }
+
+    public static void solveSudoku(char[][] board) {
+        if (board == null || board.length == 0) {
+            return;
+        }
+
+        solve(board);
+    }
+
+    private static boolean solve(char[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == '.') {
+                    for (char num = '1'; num <= '9'; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+
+                            if (solve(board)) {
+                                return true;
+                            } else {
+                                board[row][col] = '.';
+                            }
+                        }
+                    }
+
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private static boolean isValid(char[][] board, int row, int col, char num) {
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == num) {
+                return false;
+            }
+
+            if (board[row][i] == num) {
+                return false;
+            }
+
+            int boxRow = 3 * (row / 3) + i / 3;
+            int boxCol = 3 * (col / 3) + i % 3;
+
+            if (board[boxRow][boxCol] == num) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static void printBoard(char[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
