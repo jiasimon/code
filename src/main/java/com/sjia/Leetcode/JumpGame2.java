f25d7397b447098ab0c290703c9ef8a8b82f3981
Simon Jia <sjia@nisum.com>
2023-05-21 09:58:00 -0700

    Leetcode #45, JumpGame,  greedy, i < n-1

---
+++ src/main/java/com/sjia/Leetcode/JumpGame2.java
@@ -0,0 +1,58 @@
+package com.sjia.Leetcode;
+
+public class JumpGame2 {
+    // #45. Jump Game II  https://leetcode.com/problems/jump-game-ii/
+    /*
+        You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+        Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at nums[i], you can jump to any nums[i + j] where:
+        0 <= j <= nums[i] and
+        i + j < n
+        Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
+    */
+
+    // Input: nums = [2,3,1,1,4] Output: 2
+    // Input: nums = [2,3,0,1,4] Output: 2
+    // 1 <= nums.length <= 104
+    // 0 <= nums[i] <= 1000
+    // It's guaranteed that you can reach nums[n - 1].
+
+
+    // Greedy
+    public static int jump(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int jumps = 0;
+        int currentPosition = 0; // farthest index that can be reached with the current number of jumps
+        int maxReach = 0;  // farthest index that can be reached from the current index
+
+        // i < nums.length - 1  instead of i < nums.length
+        for (int i = 0; i < nums.length - 1; i++) {
+            maxReach = Math.max(maxReach, i + nums[i]);
+
+            if (i == currentPosition) {
+                jumps++;
+                currentPosition = maxReach;
+            }
+        }
+
+        return jumps;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {2, 3, 1, 1, 4};
+        System.out.println("Minimum jumps: " + jump(nums1)); // Output: 2
+
+        int[] nums2 = {3, 2, 1, 0, 4};
+        System.out.println("Minimum jumps: " + jump(nums2)); // Output: -1
+
+        int[] nums3 = {2,3,0,1,4};
+        System.out.println("Minimum jumps: " + jump(nums3)); // Output: 2
+    }
+
+
+
+
+
+}
