d3ab608cfe7a726bc231a2574b7ae276454b6780
Simon Jia <sjia@nisum.com>
2023-05-21 08:33:44 -0700

    Leetcode #55, JumpGame,  from end to start, record lastPosition

---
+++ src/main/java/com/sjia/Leetcode/JumpGame.java
@@ -0,0 +1,45 @@
+package com.sjia.Leetcode;
+
+public class JumpGame {
+    // #55. Jump Game  https://leetcode.com/problems/jump-game/
+
+    // You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+    // Return true if you can reach the last index, or false otherwise.
+
+    // Input: nums = [2,3,1,1,4]  Output: true
+    // Input: nums = [3,2,1,0,4]  Output: false
+
+    // 1 <= nums.length <= 104
+    // 0 <= nums[i] <= 105
+
+    // from end to start, record lastPosition
+    public static boolean canJump(int[] nums) {
+        /* can comment out based on the condition
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        */
+
+        int lastPosition = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPosition) {
+                lastPosition = i;
+            }
+        }
+
+        return lastPosition == 0;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        int[] nums1 = {2, 3, 1, 1, 4};
+        System.out.println("Can jump? " + canJump(nums1)); // Output: true
+
+        int[] nums2 = {3, 2, 1, 0, 4};
+        System.out.println("Can jump? " + canJump(nums2)); // Output: false
+    }
+
+
+}
