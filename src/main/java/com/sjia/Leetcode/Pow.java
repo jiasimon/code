3d2f27f33ef2799c06ed84760d038583a15ee4e1
Simon Jia <sjia@nisum.com>
2023-05-23 08:23:25 -0700

    Leetcode #50, Pow,  fastPow, recursive, half

--- src/main/java/com/sjia/Leetcode/Pow.java
+++ src/main/java/com/sjia/Leetcode/Pow.java
@@ -22,8 +22,24 @@ public class Pow {
             n = -n;
         }
 
-        return slowPow(x, n);
+//        return slowPow(x, n);
+
+        return fastPow(x, n);
     }
+
+    private static double fastPow(double x, int n) {
+        if (n ==0) return 1.0;
+
+        double half = fastPow(x, n/2);
+
+        if ( n%2 == 0) {
+            return half * half;
+        } else {
+            return x * half * half;
+        }
+
+    }
+
 
     private static double slowPow(double x, int n) {
         double res =1;
