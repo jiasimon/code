2298567aa8b6cb418ac3eb67fbfd2b31bfb085d3
Simon Jia <sjia@nisum.com>
2023-05-23 09:50:51 -0700

    Leetcode #51, NQueens,  backtrack

---
+++ src/main/java/com/sjia/Leetcode/NQueens.java
@@ -0,0 +1,86 @@
+package com.sjia.Leetcode;
+
+import java.util.*;
+
+public class NQueens {
+    // 51. N-Queens  https://leetcode.com/problems/n-queens/
+    // Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any order.
+
+    // Input: n = 4
+    // Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+    // Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above
+
+    // 1 <= n <= 9
+
+    public static List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for (char[] row : board) {
+            Arrays.fill(row, '.');
+        }
+        backtrack(result, board, 0);
+        return result;
+    }
+
+    private static void backtrack(List<List<String>> result, char[][] board, int row) {
+        if (row == board.length) {
+            result.add(constructSolution(board));
+            return;
+        }
+
+        for (int col = 0; col < board.length; col++) {
+            if (isValidMove(board, row, col)) {
+                board[row][col] = 'Q';
+                backtrack(result, board, row + 1);  // follow up the idea
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    private static boolean isValidMove(char[][] board, int row, int col) {
+        // Check if there is a queen in the same column
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check if there is a queen in the diagonal (\)
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check if there is a queen in the diagonal (/)
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static List<String> constructSolution(char[][] board) {
+        List<String> solution = new ArrayList<>();
+        for (char[] row : board) {
+            solution.add(new String(row));
+        }
+        return solution;
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        List<List<String>> solutions = solveNQueens(n);
+        for (List<String> solution : solutions) {
+            for (String row : solution) {
+                System.out.println(row);
+            }
+            System.out.println();
+        }
+    }
+
+
+
+}
