7b3c3a5ad9391eafdfb860a6469f16090a9afb50
Simon Jia <sjia@nisum.com>
2023-05-22 20:58:52 -0700

    Leetcode #48, RotateImage, matrix reflect

--- src/main/java/com/sjia/Leetcode/RotateImage.java
+++ src/main/java/com/sjia/Leetcode/RotateImage.java
@@ -37,6 +37,17 @@ public class RotateImage {
         }
     }
 
+    public void reflect(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-1 - j];
+                matrix[i][n-1 - j] = tmp;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int[][] matrix = {
                 {1, 2, 3},
