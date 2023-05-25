5cf6ce30a955f11b982583f48ee705e1bd31dcb9
Simon Jia <sjia@nisum.com>
2023-05-25 07:03:18 -0700

    Leetcode #56, MergeIntervals, merged.toArray(new int[merged.size()][])

--- src/main/java/com/sjia/Leetcode/MergeIntervals.java
+++ src/main/java/com/sjia/Leetcode/MergeIntervals.java
@@ -15,46 +15,35 @@ public class MergeIntervals {
 
 
 
+    public static int[][] mergeIntervals(int[][] intervals) {
+        if (intervals == null || intervals.length <= 1) {
+            return intervals;
+        }
 
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
+        List<int[]> merged = new ArrayList<>();
+        int[] currentInterval = intervals[0];
+        merged.add(currentInterval);
 
+        for (int[] interval : intervals) {
+            int currentEnd = currentInterval[1];
+            int nextStart = interval[0];
+            int nextEnd = interval[1];
 
-    // Runtime: 5 ms, faster than 96.05% of Java online submissions for Merge Intervals.
-    //Memory Usage: 42.4 MB, less than 40.86% of Java online submissions for Merge Intervals.
-/*    public int[][] merge(int[][] intervals) {
-        if (intervals.length <= 1) return intervals;
-        List<int[]> result = new ArrayList<>();
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-        // Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
-        int N = intervals.length, min = intervals[0][0], max = intervals[0][1];
-        for (int i = 1; i < N; i++) {
-            if (intervals[i][0] > max) {
-                int[] tmp = new int[] {min, max};
-                result.add(tmp);
-                min = intervals[i][0];
-                max = intervals[i][1];
-                if (i == N-1) {
-                    int[] tmp2 = new int[] {min, max};
-                    result.add(tmp2);
-                }
+            if (currentEnd >= nextStart) {
+                currentInterval[1] = Math.max(currentEnd, nextEnd);
             } else {
-                max = Math.max (max, intervals[i][1]); // [[1,4],[2,3]]
-                if (i == N-1) {
-                    int[] tmp = new int[] {min, max};
-                    result.add(tmp);
-                }
+                currentInterval = interval;
+                merged.add(currentInterval);
             }
         }
-        return result.toArray(new int[result.size()][]);
-    }*/
+
+        return merged.toArray(new int[merged.size()][]);
+    }
 
 
 
-
-    // [1,3],[2,6]
-    // [1,4],[4,5]
-    // [1,6], [2,4]
-    // [1,3], [5,8]
 
 
     // res.toArray(new int[res.size()][])
