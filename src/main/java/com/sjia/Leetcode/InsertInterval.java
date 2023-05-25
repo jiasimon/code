10faa70b9f40344e2e86c7c3b07fcf9f8146bb34
Simon Jia <sjia@nisum.com>
2023-05-25 09:57:33 -0700

    Leetcode #57, InsertIntervals,  3 while, before , overlap, remaining

--- src/main/java/com/sjia/Leetcode/InsertInterval.java
+++ src/main/java/com/sjia/Leetcode/InsertInterval.java
@@ -19,23 +19,48 @@ public class InsertInterval {
     // Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]  Output: [[1,2],[3,10],[12,16]]
 
 
+    // 3 while, before , overlap, remaining
     public static int[][] insert(int[][] intervals, int[] newInterval) {
         List<int[]> merged = new ArrayList<>();
+        int i=0;
+        int n = intervals.length;
 
+        //  Add intervals which end before the new interval
+        while ( i < n && intervals[i][1] < newInterval[0]) {
+            merged.add(intervals[i]);
+            i++;
+        }
 
-        return merged.toArray(new int[merged.size()][]);
+        // when iterate item startTime <= newInterval EndTime
+        while ( i < n && intervals[i][0] <= newInterval[1]) {
+            // below changed the newInterval input
+            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+            newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+            i++;
+        }
+        merged.add(newInterval);
+
+        // Add remaining intervals
+        while (i < n) {
+            merged.add(intervals[i]);
+            i++;
+        }
+
+        return merged.toArray(new int[merged.size()][2] );
+
+//        return merged.toArray(new int[merged.size()][]);
     }
 
     public static void main(String[] args) {
-        int[][] intervals0 = {{1, 3}, {2, 4}};
-        int[][] intervals = {{1, 3}, {6, 9}};
+        int[][] intervals0 = {{1, 3}, {2, 4}};  // output: [[1, 5]]
+        int[][] intervals = {{1, 3}, {6, 9}}; // Output: [1, 5], [6, 9]
         int[] newInterval = {2, 5};
         int[][] mergedIntervals = insert(intervals, newInterval);
 
         for (int[] interval : mergedIntervals) {
             System.out.println("[" + interval[0] + ", " + interval[1] + "]");
         }
-        // Output: [1, 5], [6, 9]
+
 
         System.out.println( "After InsertInterval: " + Arrays.deepToString(mergedIntervals));
 
