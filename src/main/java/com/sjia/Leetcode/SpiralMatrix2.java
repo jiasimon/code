79eb411980445ca335beecf61a6bcca997b7e8cd
Simon Jia <sjia@nisum.com>
2023-05-26 11:46:09 -0700

    Leetcode #59, SpiralMatrix2,  add tests

---
+++ src/main/java/com/sjia/Leetcode/SpiralMatrix2.java
@@ -0,0 +1,31 @@
+package com.sjia.Leetcode;
+
+import java.util.Arrays;
+
+public class SpiralMatrix2 {
+    // #59. Spiral Matrix II  https://leetcode.com/problems/spiral-matrix-ii/
+
+    // Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.
+    // Input: n = 3  Output: [[1,2,3],[8,9,4],[7,6,5]]
+    // Input: n = 1  Output: [[1]]
+
+    // 1 <= n <= 20
+
+    public static int[][] generateMatrix(int n) {
+
+
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+        int n = 3;
+        int[][] matrix = generateMatrix(n);
+        System.out.println("n= " + n + "generated Spiral Matrix is :" + Arrays.deepToString(matrix));
+    }
+
+
+
+
+
+}
