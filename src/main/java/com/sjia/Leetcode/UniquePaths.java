6a2717d9cec88899ffd614050861e7002fb7e1b8
Simon Jia <sjia@nisum.com>
2023-05-28 07:52:38 -0700

    Leetcode #62, UniquePaths, DP, fill row 1, column 1

--- src/main/java/com/sjia/Leetcode/UniquePaths.java
+++ src/main/java/com/sjia/Leetcode/UniquePaths.java
@@ -14,6 +14,28 @@ public class UniquePaths {
     // 1 <= m, n <= 100
 
 
+    // DP
+    public static int uniquePaths2(int m, int n) {
+        int[][] dp = new int[m][n];
+
+        // Fill the first row and first column with 1 since there is only one way to reach any cell in the first row or first column
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = 1;
+        }
+
+        // Fill the rest of the dp array using dynamic programming
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        // Return the number of unique paths to reach the bottom-right cell
+        return dp[m - 1][n - 1];
+    }
 
 
     // math, (m-1 + n-1)!  / (m-1)!(n-1)!
@@ -28,7 +50,7 @@ public class UniquePaths {
     public static void main(String[] args) {
         int m = 3;
         int n = 7;
-        int uniquePaths = uniquePaths(m, n);
+        int uniquePaths = uniquePaths2(m, n);
         System.out.println("Number of unique paths: " + uniquePaths); // Output: 28
     }
 
