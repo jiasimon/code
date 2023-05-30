c7bfc5481aa958de8719a718e6e06f44879f33e8
Simon Jia <sjia@nisum.com>
2023-05-30 08:33:47 -0700

    Leetcode #70, ClimbingStairs, DP, dp = new int[n+1]

--- src/main/java/com/sjia/Leetcode/ClimbingStairs.java
+++ src/main/java/com/sjia/Leetcode/ClimbingStairs.java
@@ -2,7 +2,7 @@ package com.sjia.Leetcode;
 
 public class ClimbingStairs {
 
-    // #70 Climbing Stairs https://leetcode.com/problems/climbing-stairs/
+    // #70. Climbing Stairs  Climbing Stairs https://leetcode.com/problems/climbing-stairs/
 
     // Runtime: 0 ms, faster than 100.00% of Java online submissions for Climbing Stairs.
     //Memory Usage: 38 MB, less than 5.03% of Java online submissions for Climbing Stairs.
@@ -19,15 +19,16 @@ public class ClimbingStairs {
     }*/
 
 
+    // DP, dp = new int[n+1]
     static int climbStairs(int n) {
-        if (n == 1 || n==2) return n;
-        int x=1, y=2, result=3;
-        while ( n-- >=3) {
-            result = x + y;
-            x = y;
-            y = result;
+        if (n < 2) return n;
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i=3; i <= n ; i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        return result;
+        return dp[n];
     }
 
 
@@ -37,7 +38,14 @@ public class ClimbingStairs {
         int testData  = 6;
         int result = climbStairs(testData);
 
-        System.out.printf(" testData %s th fibo is %s \n", testData,result);
+        System.out.printf(" testData %s  fibo is %s \n", testData,result);
+
+        int n1 = 2;
+        System.out.println("Number of distinct ways to climb " + n1 + " stairs: " + climbStairs(n1)); // Output: 2
+
+        int n2 = 3;
+        System.out.println("Number of distinct ways to climb " + n2 + " stairs: " + climbStairs(n2)); // Output: 3
+
     }
 
 }
