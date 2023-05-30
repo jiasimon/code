969855cb84b7c12c2d7f23c986ad6deb074a965b
Simon Jia <sjia@nisum.com>
2023-05-30 08:41:27 -0700

    Leetcode #70, ClimbingStairs, recursive

--- src/main/java/com/sjia/Leetcode/ClimbingStairs.java
+++ src/main/java/com/sjia/Leetcode/ClimbingStairs.java
@@ -3,6 +3,27 @@ package com.sjia.Leetcode;
 public class ClimbingStairs {
 
     // #70. Climbing Stairs  Climbing Stairs https://leetcode.com/problems/climbing-stairs/
+    /*
+    You are climbing a staircase. It takes n steps to reach the top.
+
+    Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+     */
+
+    // 1 <= n <= 45
+
+
+
+    // recursive, O(2^n) Space: O(n)
+    static int climbStairs2(int n) {
+        if ( n == 1) return 1;
+        if ( n == 2 ) return 2;
+
+        return climbStairs2(n-1) + climbStairs2(n-2);
+
+    }
+
+
+
 
     // Runtime: 0 ms, faster than 100.00% of Java online submissions for Climbing Stairs.
     //Memory Usage: 38 MB, less than 5.03% of Java online submissions for Climbing Stairs.
@@ -36,7 +57,7 @@ public class ClimbingStairs {
 
         //testData  = 10 :89, 6 :13
         int testData  = 6;
-        int result = climbStairs(testData);
+        int result = climbStairs2(testData);
 
         System.out.printf(" testData %s  fibo is %s \n", testData,result);
 
