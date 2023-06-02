8af4b35bce5aff97bc2ac03c65e63b0b4e292388
Simon Jia <sjia@nisum.com>
2023-06-03 05:47:42 +0800

    Leetcode #73, SetMatrixZeroes, O(1) set matrix[0][j] and matrix[i][0]

--- src/main/java/com/sjia/Leetcode/SetMatrixZeroes.java
+++ src/main/java/com/sjia/Leetcode/SetMatrixZeroes.java
@@ -39,9 +39,70 @@ public class SetMatrixZeroes {
             }
         }
 
-
-
     }
+
+
+    // O(1)  modify in place
+    // matrix[i][0] ,  matrix[0][j]
+    public static void setZeroes2(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        // Use the first row and first column to store the information about zero rows and zero columns
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
+
+        // Check if the first row contains zero
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+
+        // Check if the first column contains zero
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+
+        // Use the first row and first column to mark the zero rows and zero columns
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // Set the values in the matrix based on the marked zero rows and zero columns
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Set the first row to zeros if necessary
+        if (firstRowZero) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Set the first column to zeros if necessary
+        if (firstColZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
+
 
     public static void main(String[] args) {
         int[][] matrix1 = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
