8dd12696fc093717f657ca835e0348a1c50dd305
Simon Jia <sjia@nisum.com>
2023-06-05 10:11:48 +0800

    Leetcode #80, RemoveDuplicatesSortedArray2, if (i < 2 || n > nums[i - 2])

--- src/main/java/com/sjia/Leetcode/RemoveDuplicatesSortedArray2.java
+++ src/main/java/com/sjia/Leetcode/RemoveDuplicatesSortedArray2.java
@@ -28,9 +28,22 @@ public class RemoveDuplicatesSortedArray2 {
         return slow;
     }
 
+    //
+    public static int removeDuplicates2(int[] nums) {
+        int i = 0;
+        for (int n : nums)
+            if (i < 2 || n > nums[i - 2]) {
+                nums[i] = n;
+                i++;
+            }
+
+        return i;
+    }
+
+
     public static void main(String[] args) {
         int[] nums = {1, 1, 1, 2, 2, 3};
-        int newLength = removeDuplicates(nums);
+        int newLength = removeDuplicates2(nums);
 
         System.out.println("New length: " + newLength); // Output: 5
         System.out.print("Updated array: ");
