76408dd6ee660a157760d07a5413ecc8c21e93a9
Simon Jia <sjia@nisum.com>
2023-06-08 10:18:10 +0800

    Leetcode #88, MergeSortedArray, two pointers, from end to start

--- src/main/java/com/sjia/Leetcode/MergeSortedArray.java
+++ src/main/java/com/sjia/Leetcode/MergeSortedArray.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class MergeSortedArray {
 
-    // #88 https://leetcode.com/problems/merge-sorted-array/    #fb
+    // #88. Merge Sorted Array https://leetcode.com/problems/merge-sorted-array/    #fb
     // Given two sorted integer arrays nums1 and nums2
     // merge nums2 into nums1 as one sorted array
     // The number of elements initialized in nums1 and nums2 are m and n
