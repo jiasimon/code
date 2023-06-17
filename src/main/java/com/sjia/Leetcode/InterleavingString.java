7a544d7fb7de82dc6e7471e3408442a36393a163
Simon Jia <sjia@nisum.com>
2023-06-18 05:58:47 +0800

    Leetcode #97, InterleavingString, recursive, Time Limit Exceeded

--- src/main/java/com/sjia/Leetcode/InterleavingString.java
+++ src/main/java/com/sjia/Leetcode/InterleavingString.java
@@ -19,7 +19,7 @@ public class InterleavingString {
 
 
     // DP, first row, first column
-    public boolean isInterleave(String s1, String s2, String s3) {
+    public boolean isInterleave2(String s1, String s2, String s3) {
         if (s1.length() + s2.length() != s3.length()) {
             return false;
         }
@@ -51,6 +51,26 @@ public class InterleavingString {
 
         return dp[m][n];
     }
+
+
+    // recursive, Time Limit Exceeded
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if (s1.length() == 0) return s2.equals(s3);
+        if (s2.length() == 0) return s1.equals(s3);
+        if (s3.charAt(0) != s1.charAt(0) && s3.charAt(0) != s2.charAt(0)) {
+            return false;
+        }
+        if (s3.charAt(0) == s1.charAt(0) && s3.charAt(0) == s2.charAt(0)) {
+            return isInterleave(s1.substring(1), s2, s3.substring(1)) ||
+                    isInterleave(s1, s2.substring(1), s3.substring(1));
+        } else if (s3.charAt(0) == s1.charAt(0)) {
+            return isInterleave(s1.substring(1), s2, s3.substring(1));
+        } else if (s3.charAt(0) == s2.charAt(0)) {
+            return isInterleave(s1, s2.substring(1), s3.substring(1));
+        }
+        return false;
+    }
+
 
     public static void main(String[] args) {
         InterleavingString solution = new InterleavingString();
