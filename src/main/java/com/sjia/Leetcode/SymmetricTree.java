2a10e8ba6b3f60aa2bd2eeeca07a3f1f9ca80b63
Simon Jia <sjia@nisum.com>
2023-06-23 06:53:09 +0800

    Leetcode #101, SymmetricTree, add test cases

--- src/main/java/com/sjia/Leetcode/SymmetricTree.java
+++ src/main/java/com/sjia/Leetcode/SymmetricTree.java
@@ -2,7 +2,7 @@ package com.sjia.Leetcode;
 
 public class SymmetricTree {
 
-    public class TreeNode {
+/*    public class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
@@ -19,8 +19,14 @@ public class SymmetricTree {
             this.left = left;
             this.right = right;
         }
-    }
-    //#101 https://leetcode.com/problems/symmetric-tree/
+    }*/
+    // #101. Symmetric Tree https://leetcode.com/problems/symmetric-tree/
+    /*
+    Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+    The number of nodes in the tree is in the range [1, 1000].
+    -100 <= Node.val <= 100
+     */
+    
 
     // Runtime: 1 ms, faster than 39.46% of Java online submissions for Symmetric Tree.
     //Memory Usage: 40.3 MB, less than 5.05% of Java online submissions for Symmetric Tree.
@@ -40,5 +46,22 @@ public class SymmetricTree {
         else return (left.val == right.val) && checkSymmetric(left.left, right.right)
                     && checkSymmetric(left.right, right.left);
     }
+
+    public static void main(String[] args) {
+        SymmetricTree solution = new SymmetricTree();
+
+        // Example tree: [1,2,2,3,4,4,3]
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(3);
+
+        boolean isSymmetric = solution.isSymmetric(root);
+        System.out.println("Is the tree symmetric? " + isSymmetric);
+    }
+
 
 }
