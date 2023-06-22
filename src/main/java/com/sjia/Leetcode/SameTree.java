ec5a47b7136c6a54390bec9510fe5bef773a50ae
Simon Jia <sjia@nisum.com>
2023-06-22 19:12:24 +0800

    Leetcode #100, SameTree, add test cases

--- src/main/java/com/sjia/Leetcode/SameTree.java
+++ src/main/java/com/sjia/Leetcode/SameTree.java
@@ -3,24 +3,7 @@ package com.sjia.Leetcode;
 public class SameTree {
     // #100 https://leetcode.com/problems/same-tree/
 
-    public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
 
-        TreeNode() {
-        }
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
 
 
     // Runtime: 0 ms, faster than 100.00% of Java online submissions for Same Tree.
@@ -34,6 +17,23 @@ public class SameTree {
             return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         }
 
+    }
+
+
+    public static void main(String[] args) {
+        SameTree solution = new SameTree();
+
+        // Example trees: [1,2,3] and [1,2,3]
+        TreeNode p = new TreeNode(1);
+        p.left = new TreeNode(2);
+        p.right = new TreeNode(3);
+
+        TreeNode q = new TreeNode(1);
+        q.left = new TreeNode(2);
+        q.right = new TreeNode(3);
+
+        boolean isSame = solution.isSameTree(p, q);
+        System.out.println("Are the given trees the same? " + isSame);
     }
 
 
