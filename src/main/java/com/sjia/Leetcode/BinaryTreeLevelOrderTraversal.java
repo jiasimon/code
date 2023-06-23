158ddeff624a687c3091fe97091e8dc30a434efd
Simon Jia <sjia@nisum.com>
2023-06-23 09:25:35 +0800

    Leetcode #102, BinaryTreeLevelOrderTraversal, queue, levelSize

---
+++ src/main/java/com/sjia/Leetcode/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,64 @@
+package com.sjia.Leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeLevelOrderTraversal {
+    // #102. Binary Tree Level Order Traversal  https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+    /*
+    Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+    Input: root = [3,9,20,null,null,15,7]   Output: [[3],[9,20],[15,7]]
+    Input: root = [1]   Output: [[1]]
+     */
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> levelNodes = new ArrayList<>();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                levelNodes.add(node.val);
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+
+            result.add(levelNodes);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        BinaryTreeLevelOrderTraversal solution = new BinaryTreeLevelOrderTraversal();
+
+        // Example tree: [3,9,20,null,null,15,7]
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        List<List<Integer>> levelOrderTraversal = solution.levelOrder(root);
+        System.out.println("Level order traversal: " + levelOrderTraversal);
+    }
+
+
+}
