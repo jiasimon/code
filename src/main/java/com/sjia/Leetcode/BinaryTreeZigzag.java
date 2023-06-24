b763fe507d8c470efd434fb904cd2bde28ed56d0
Simon Jia <sjia@nisum.com>
2023-06-25 06:35:26 +0800

    Leetcode #103, BinaryTreeZigzag,  recursive, less memory

--- src/main/java/com/sjia/Leetcode/BinaryTreeZigzag.java
+++ src/main/java/com/sjia/Leetcode/BinaryTreeZigzag.java
@@ -58,4 +58,32 @@ public class BinaryTreeZigzag {
     }
 
 
+    // recursive
+    public List<List<Integer>> zigzagLevelOrder2(TreeNode root)
+    {
+        List<List<Integer>> res = new ArrayList<>();
+        travel(root, res, 0);
+        return res;
+    }
+
+    private void travel(TreeNode curr, List<List<Integer>> res, int level)
+    {
+        if(curr == null) return;
+
+        if(res.size() <= level)
+        {
+            List<Integer> newLevel = new LinkedList<>();
+            res.add(newLevel);
+        }
+
+        List<Integer> collection  = res.get(level);
+        if(level % 2 == 0) collection.add(curr.val);
+        else collection.add(0, curr.val);
+
+        travel(curr.left, res, level + 1);
+        travel(curr.right, res, level + 1);
+    }
+    
+
+
 }
