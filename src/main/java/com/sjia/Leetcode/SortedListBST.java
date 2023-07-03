44ef1b232ec89428fcb2feb84204f24c8fac75ed
Simon Jia <sjia@nisum.com>
2023-07-03 12:13:25 -0700

    Leetcode #109, SortedListToBST,  recursive with only head, initial fast pointer

--- src/main/java/com/sjia/Leetcode/SortedListBST.java
+++ src/main/java/com/sjia/Leetcode/SortedListBST.java
@@ -66,4 +66,33 @@ public class SortedListBST {
 
 
 
+    // initial slow pointer is one step after fast. to get the previous of middle
+    public TreeNode sortedListToBSTRecursive(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        if (head.next == null) {
+            return new TreeNode(head.val);
+        }
+
+        ListNode slow = head;
+        //   ListNode fast = head;
+        ListNode fast=head.next.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        TreeNode res = new TreeNode(slow.next.val);
+        ListNode righthalf = slow.next.next;
+        slow.next = null; // cut left sub list here
+        res.left = sortedListToBSTRecursive(head);
+        res.right = sortedListToBSTRecursive(righthalf);
+        return res;
+
+    }
+
+
 }
