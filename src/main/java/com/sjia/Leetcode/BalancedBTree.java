4d66e0b57ac491598b1322d60104c05eed1b8dbf
Simon Jia <sjia@nisum.com>
2023-07-03 19:43:31 -0700

    Leetcode #110, BalancedBTree,  l == -1 || r == -1 || Math.abs(l - r) > 1

--- src/main/java/com/sjia/Leetcode/BalancedBTree.java
+++ src/main/java/com/sjia/Leetcode/BalancedBTree.java
@@ -31,14 +31,14 @@ public class BalancedBTree {
         return helper(root)>=0;
     }
 
-    public int helper(TreeNode root){
-        if(root == null){
+    public int helper(TreeNode root) {
+        if (root == null) {
             return 0;
         }
         int l = helper(root.left);
         int r = helper(root.right);
-        if(l==-1 || r==-1 || Math.abs(l-r)>1) return -1;
-        return Math.max(l,r) +1;
+        if (l == -1 || r == -1 || Math.abs(l - r) > 1) return -1;
+        return Math.max(l, r) + 1;
     }
 
 
