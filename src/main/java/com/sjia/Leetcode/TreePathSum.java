441d482efbc213080236dcc10348b63f557e3217
Simon Jia <sjia@nisum.com>
2023-07-04 08:32:37 -0700

    Leetcode #112, PathSum,  recursive

--- src/main/java/com/sjia/Leetcode/TreePathSum.java
+++ src/main/java/com/sjia/Leetcode/TreePathSum.java
@@ -1,11 +1,12 @@
 package com.sjia.Leetcode;
 
 public class TreePathSum {
-    // #112   https://leetcode.com/problems/path-sum/
+    // #112. Path Sum  https://leetcode.com/problems/path-sum/
 
     // Runtime: 0 ms, faster than 100.00% of Java online submissions for Path Sum.
     //Memory Usage: 39.3 MB, less than 57.05% of Java online submissions for Path Sum.
 
+    // recursive
     public boolean hasPathSum(TreeNode root, int sum) {
         if ( root == null) return false;
         if (root.left == null & root.right ==null) {
@@ -13,5 +14,19 @@ public class TreePathSum {
         }
         return hasPathSum(root.left, sum-root.val) || hasPathSum(root.right, sum-root.val);
     }
+
+
+/*
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return sum == root.val;
+        }
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+
+    }
+    */
 
 }
