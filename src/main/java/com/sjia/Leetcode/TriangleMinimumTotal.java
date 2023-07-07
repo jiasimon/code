d027eaa17427f3870dcebfaae3a117223304e0b5
Simon Jia <sjia@nisum.com>
2023-07-06 18:51:24 -0700

    Leetcode #120, TriangleMinimumTotal,  top down, memoization

--- src/main/java/com/sjia/Leetcode/TriangleMinimumTotal.java
+++ src/main/java/com/sjia/Leetcode/TriangleMinimumTotal.java
@@ -58,11 +58,52 @@ public class TriangleMinimumTotal {
     }
 
     int dfs(List<List<Integer>> triangle, int row, int pos) {
+/*
         if (row >= triangle.size() -1) {
             return triangle.get(row).get(pos);
         }
         return  triangle.get(row).get(pos) + Math.min (dfs(triangle,row+1, pos), dfs(triangle, row+1, pos+1));
+        */
+        int path = triangle.get(row).get(pos);
+        if (row < triangle.size() -1) {
+            path += Math.min (dfs(triangle,row+1, pos), dfs(triangle, row+1, pos+1));
+        }
+        return path;
     }
+
+
+    //Top down, 1 ms, 99.95%, Memory 44.3 MB 27.49%
+    public int minimumTotalTopDown(List<List<Integer>> triangle) {
+        if (triangle == null || triangle.size() == 0) return 0;
+        Integer[][] cache = new Integer[triangle.size()][triangle.size()];  // use Integer instead of int, so default to null
+        return dfs(0, 0,triangle, cache);
+    }
+
+    int dfs(int row, int pos, List<List<Integer>> triangle, Integer[][] cache){
+        if (cache[row][pos] != null) return cache[row][pos];
+
+/*
+
+        //Out of bounds so just return sum previous value must be leaf node
+        cache[row][pos] = triangle.get(row).get(pos);
+        if (row >= triangle.size() - 1) {
+            return cache[row][pos];
+        }
+
+        cache[row][pos] =  triangle.get(row).get(pos) + Math.min ( dfs(row+1, pos,triangle, cache), dfs(row+1, pos+1,triangle, cache));
+*/
+        int path = triangle.get(row).get(pos);
+        if (row < triangle.size() -1) {
+            path += Math.min (dfs(row+1, pos, triangle, cache), dfs( row+1, pos+1, triangle, cache));
+        }
+
+        cache[row][pos] = path;
+
+        return cache[row][pos];
+
+    }
+
+
 
 
 
@@ -73,11 +114,11 @@ public class TriangleMinimumTotal {
         List<List<Integer>> triangle = new ArrayList<>();
         triangle.add(Arrays.asList(2));
         triangle.add(Arrays.asList(3, 4));
-//        triangle.add(Arrays.asList(6, 5, 7));
-//        triangle.add(Arrays.asList(4, 1, 8, 3));
+        triangle.add(Arrays.asList(6, 5, 7));
+        triangle.add(Arrays.asList(4, 1, 8, 3));
 
         // Calculate the minimum path sum
-        int result = solution.minimumTotalRecursive(triangle);
+        int result = solution.minimumTotalTopDown(triangle);
 
         // Print the result
         System.out.println("Minimum Path Sum: " + result);  // Output: 11
