99203044361e35a12b6bdf66337d66a66549bece
Simon Jia <sjia@nisum.com>
2023-07-06 20:36:58 -0700

    Leetcode #121, BuySellStock,  straight, two loops, TLE

--- src/main/java/com/sjia/Leetcode/BuySellStock.java
+++ src/main/java/com/sjia/Leetcode/BuySellStock.java
@@ -3,7 +3,17 @@ package com.sjia.Leetcode;
 import java.util.Arrays;
 
 public class BuySellStock {
-    // #121 https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+    // #121. Best Time to Buy and Sell Stock  https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+    /*
+    You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+    Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+    Input: prices = [7,1,5,3,6,4]   Output: 5
+    Input: prices = [7,6,4,3,1]     Output: 0
+
+    1 <= prices.length <= 105
+    0 <= prices[i] <= 10^4
+     */
 
     // Runtime: 3 ms, faster than 21.97% of Java online submissions for Best Time to Buy and Sell Stock.
     //Memory Usage: 41.5 MB, less than 5.09% of Java online submissions for Best Time to Buy and Sell Stock.
@@ -17,6 +27,20 @@ public class BuySellStock {
         }
         return maxProfit;
     }
+
+
+    // Time Limit Exceeded, 202 / 211 testcases passed
+    public int maxProfitTwoLoops(int[] prices) {
+        if (prices.length <2) return 0;
+        int res = 0;
+        for (int i=0; i< prices.length; i++) {
+            for (int j=i+1; j < prices.length; j++) {
+                res = Math.max(res, prices[j] - prices[i] );
+            }
+        }
+        return res;
+    }
+
 
     public static void main(String[] args) {
 
