67713cd762185502783322ff69bfb73f085c4ed8
Simon Jia <sjia@nisum.com>
2023-07-10 06:49:53 -0700

    Leetcode #136, SingleNumber, sort, nums[i-1]

--- src/main/java/com/sjia/Leetcode/SingleNumberInArray.java
+++ src/main/java/com/sjia/Leetcode/SingleNumberInArray.java
@@ -6,7 +6,14 @@ import java.util.Set;
 
 public class SingleNumberInArray {
 
-    // #136 https://leetcode.com/problems/single-number/
+    // #136. Single Number  https://leetcode.com/problems/single-number/
+    /*
+    Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+    You must implement a solution with a linear runtime complexity and use only constant extra space.
+    Input: nums = [2,2,1]   Output: 1
+    Input: nums = [4,1,2,1,2]   Output: 4
+
+     */
 
     // leetcode was down for some time today. Runtime: 6 ms, faster than 39.12% of Java online submissions for Single Number.
     //Memory Usage: 40.3 MB, less than 59.60% of Java online submissions for Single Number.
@@ -30,6 +37,18 @@ public class SingleNumberInArray {
             result ^= num;
         }
         return result;
+    }
+
+
+    // sort, 6 ms, 42.20%; 44.7 MB, 49.4%
+    public int singleNumberSort(int[] nums) {
+        Arrays.sort(nums);
+        for (int i=1; i< nums.length; i+=2){
+            if(nums[i] != nums[i-1]) {
+                return nums[i-1];
+            }
+        }
+        return nums[nums.length-1];
     }
 
 
