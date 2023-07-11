33277459549075474f43ff200b91bda17301d530
Simon Jia <sjia@nisum.com>
2023-07-11 06:40:25 -0700

    Leetcode #134, GasStation, 1 pass

--- src/main/java/com/sjia/Leetcode/GasStation.java
+++ src/main/java/com/sjia/Leetcode/GasStation.java
@@ -62,7 +62,23 @@ public class GasStation {
     }
 
 
+    // 1 pass
+    public int canCompleteCircuitGreedy(int[] gas, int[] cost) {
+        int n = gas.length;
+        int total_surplus = 0;
+        int tank = 0;
+        int start = 0;
 
+        for(int i = 0; i < n; i++){
+            total_surplus += gas[i] - cost[i];
+            tank  += gas[i] - cost[i];
+            if(tank < 0){
+                tank  = 0;
+                start = i + 1;
+            }
+        }
+        return (total_surplus < 0) ? -1 : start;
+    }
 
 
 
