b876e12d968c0ace6997205dd2189814c41536f5
Simon Jia <sjia@nisum.com>
2023-07-11 06:09:59 -0700

    Leetcode #714, BuySellStock6, DP, less space

--- src/main/java/com/sjia/Leetcode/BuySellStock6.java
+++ src/main/java/com/sjia/Leetcode/BuySellStock6.java
@@ -31,6 +31,22 @@ public class BuySellStock6 {
 
     }
 
+
+    // free: sell;  hold: buy in other code
+    public int maxProfitWithoutDPArray(int[] prices, int fee) {
+        int n = prices.length;
+        int free = 0;
+        int hold = -prices[0];
+
+        for (int i = 1; i < n; i++) {
+            int tmp = hold;
+            hold = Math.max(hold, free - prices[i]);
+            free = Math.max(free, tmp + prices[i] - fee);
+        }
+        return free;
+    }
+
+
     public static void main(String[] args) {
         BuySellStock6 solution = new BuySellStock6();
 
