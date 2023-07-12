87f196b60fb063471533974d0b44b8ba996c058d
Simon Jia <sjia@nisum.com>
2023-07-11 20:43:24 -0700

    Leetcode #145, BinaryTreePostorderTraversal, stack, ArrayList

--- src/main/java/com/sjia/Leetcode/BinaryTreePostorderTraversal.java
+++ src/main/java/com/sjia/Leetcode/BinaryTreePostorderTraversal.java
@@ -2,10 +2,12 @@ package com.sjia.Leetcode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class BinaryTreePostorderTraversal {
     // #145. Binary Tree Postorder Traversal    https://leetcode.com/problems/binary-tree-postorder-traversal/
     /*
+    left -> right -> root
     Given the root of a binary tree, return the postorder traversal of its nodes' values.
     Input: root = [1,null,2,3]  Output: [3,2,1]
 
@@ -26,4 +28,29 @@ public class BinaryTreePostorderTraversal {
         helperPostorder(root.right, res);
         res.add(root.val);
     }
+
+
+    // stack, ArrayList
+    // 1 ms, 18.12% ; 40.7 MB, 85.41%
+    public List<Integer> postorderTraversalStack(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if(root == null) return res;
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()) {
+            TreeNode curr = stack.pop();
+            res.add(0,curr.val);
+            if(curr.left!=null) {
+                stack.push(curr.left);
+            }
+            if(curr.right!=null) {
+                stack.push(curr.right);
+            }
+        }
+        return res;
+    }
+
+
+
 }
