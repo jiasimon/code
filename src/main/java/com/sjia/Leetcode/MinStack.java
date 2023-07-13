f3cb8c7f36fc5d03d1bfd216915d30311b07dac2
Simon Jia <sjia@nisum.com>
2023-07-13 08:00:40 -0700

    Leetcode #155, MinStack, double stack

---
+++ src/main/java/com/sjia/Leetcode/MinStack.java
@@ -0,0 +1,55 @@
+package com.sjia.Leetcode;
+
+import java.util.Stack;
+
+public class MinStack {
+    // #155. Min Stack  https://leetcode.com/problems/min-stack/
+    /*
+    Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+    Implement the MinStack class:
+
+    MinStack() initializes the stack object.
+    void push(int val) pushes the element val onto the stack.
+    void pop() removes the element on the top of the stack.
+    int top() gets the top element of the stack.
+    int getMin() retrieves the minimum element in the stack.
+    You must implement a solution with O(1) time complexity for each function.
+     */
+
+
+
+
+    //  double stack
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        if (stack.peek().equals(minStack.peek())) {
+            minStack.pop();
+        }
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+}
