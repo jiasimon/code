4a814ece0f644b549b47e5f89328fb8ea00a10fc
Simon Jia <sjia@nisum.com>
2023-07-14 16:51:15 -0700

    Leetcode #172, FactorialTrailingZeroes, recursive

--- src/main/java/com/sjia/Leetcode/FactorialTrailingZeroes.java
+++ src/main/java/com/sjia/Leetcode/FactorialTrailingZeroes.java
@@ -2,7 +2,19 @@ package com.sjia.Leetcode;
 
 public class FactorialTrailingZeroes {
 
-    // #172 https://leetcode.com/problems/factorial-trailing-zeroes/
+    // #172. Factorial Trailing Zeroes https://leetcode.com/problems/factorial-trailing-zeroes/
+    /*
+    Given an integer n, return the number of trailing zeroes in n!.
+
+    Note that n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1.
+
+    Input: n = 3    Output: 0
+    Explanation: 3! = 6, no trailing zero.
+
+    Input: n = 5    Output: 1
+    Explanation: 5! = 120, one trailing zero.
+
+     */
 
     // Runtime: 1 ms, faster than 47.58% of Java online submissions for Factorial Trailing Zeroes.
     //Memory Usage: 38.6 MB, less than 5.65% of Java online submissions for Factorial Trailing Zeroes.
@@ -13,6 +25,15 @@ public class FactorialTrailingZeroes {
             n = n/5;
         }
         return result;
+    }
+
+    // recursive
+    // 0 ms, 100% ; 39.8 MB, 36.24%
+    static int trailingZeroesRecursive(int n) {
+        if ( n < 5) return 0;
+        else {
+            return n / 5 + trailingZeroesRecursive(n/5);
+        }
     }
 
 
