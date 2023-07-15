402970decfd3c83afdf39fccedf760ef3ede0f57
Simon Jia <sjia@nisum.com>
2023-07-15 08:20:23 -0700

    Leetcode #178, SQLRankScores, DENSE_RANK

--- src/main/java/com/sjia/Leetcode/SQLRankScores.java
+++ src/main/java/com/sjia/Leetcode/SQLRankScores.java
@@ -49,6 +49,21 @@ FROM
 
 
 
+SELECT
+  S1.score,
+  (
+    SELECT
+      COUNT(DISTINCT S2.score)
+    FROM
+      Scores S2
+    WHERE
+      S2.score >= S1.score
+  ) AS 'rank'
+FROM
+  Scores S1
+ORDER BY
+  S1.score DESC;
+
      */
 
 }
