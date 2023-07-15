57a695e90e8d811675fc7091c6d3784c26583ae8
Simon Jia <sjia@nisum.com>
2023-07-15 07:05:18 -0700

    Leetcode #174, SQL, CombineTwoTables

--- src/main/java/com/sjia/Leetcode/CombineTwoTables.java
+++ src/main/java/com/sjia/Leetcode/CombineTwoTables.java
@@ -2,9 +2,30 @@ package com.sjia.Leetcode;
 
 public class CombineTwoTables {
 
-    // #175 https://leetcode.com/problems/combine-two-tables/
+    // #175. Combine Two Tables https://leetcode.com/problems/combine-two-tables/
     // Write a SQL query for a report that provides the  information for each person in the Person table,
     // regardless if there is an address for each of those people:
+    /*
+    Table: Person
+
+    +-------------+---------+
+    | Column Name | Type    |
+    +-------------+---------+
+    | personId    | int     |
+    | lastName    | varchar |
+    | firstName   | varchar |
+    +-------------+---------+
+
+    Table: Address
+
+    +-------------+---------+
+    | Column Name | Type    |
+    +-------------+---------+
+    | addressId   | int     |
+    | personId    | int     |
+    | city        | varchar |
+    | state       | varchar |
+     */
 
 /*
     select FirstName, LastName, City, State
