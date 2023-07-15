349caeba6040b4e74c305539704b0089415ecb93
Simon Jia <sjia@nisum.com>
2023-07-15 05:34:39 -0700

    Leetcode #174, DungeonGame, bottom up

--- src/main/java/com/sjia/Leetcode/DungeonGame.java
+++ src/main/java/com/sjia/Leetcode/DungeonGame.java
@@ -11,7 +11,7 @@ public class DungeonGame {
      */
 
 
-    // dp, last row, last col
+    // dp, last row, last col, bottom up
     public int calculateMinimumHP(int[][] dungeon) {
         int m = dungeon.length;
         int n = dungeon[0].length;
@@ -41,6 +41,32 @@ public class DungeonGame {
 
         return dp[0][0]; // Return the minimum initial health needed for the knight
     }
+
+
+
+    // bottom up
+    public int calculateMinimumHPBottomUp(int[][] dungeon) {
+        int m = dungeon.length;
+        int n = dungeon[0].length;
+
+        int dp[][] = new int[m][n];
+
+        for (int i=m-1; i>=0; i--){
+            for ( int j=n-1; j>=0; j--) {
+                if ( i==m-1 && j==n-1) {
+                    dp[i][j] = Math.max(1, 1-dungeon[i][j]);
+                } else if ( i==m-1 ) {
+                    dp[i][j] = Math.max(1, dp[i][j+1]-dungeon[i][j]);
+                } else if ( j==n-1) {
+                    dp[i][j] = Math.max(1, dp[i+1][j]-dungeon[i][j]);
+                } else {
+                    dp[i][j] = Math.max(1, Math.min(dp[i+1][j],dp[i][j+1])-dungeon[i][j]);
+                }
+            }
+        }
+        return dp[0][0];
+    }
+
 
     public static void main(String[] args) {
         DungeonGame solution = new DungeonGame();
