4fb780f1bdd7c22fdd0ee84e044d25724f98d685
Simon Jia <sjia@nisum.com>
2023-07-16 10:23:14 -0700

    Leetcode #191, Numberof1Bits, add tests

--- src/main/java/com/sjia/Leetcode/Numberof1Bits.java
+++ src/main/java/com/sjia/Leetcode/Numberof1Bits.java
@@ -1,7 +1,12 @@
 package com.sjia.Leetcode;
 
 public class Numberof1Bits {
-    // #191 https://leetcode.com/problems/number-of-1-bits/
+    // #191. Number of 1 Bits https://leetcode.com/problems/number-of-1-bits/
+
+    /*
+    Write a function that takes the binary representation of an unsigned integer
+    and returns the number of '1' bits it has (also known as the Hamming weight).
+     */
 
     // Runtime: 1 ms, faster than 54.21% of Java online submissions for Number of 1 Bits.
     //Memory Usage: 38.8 MB, less than 5.01% of Java online submissions for Number of 1 Bits.
@@ -19,6 +24,16 @@ public class Numberof1Bits {
         }
         return result;
         // return Integer.bitCount();
+    }
+
+
+    public static void main(String[] args) {
+        Numberof1Bits solution = new Numberof1Bits();
+
+        // Test case
+        int n = 00000000000000000000000000001011;
+        int count = solution.hammingWeight(n);
+        System.out.println("Number of 1 Bits: " + count);  // Expected output: 3
     }
 
 
