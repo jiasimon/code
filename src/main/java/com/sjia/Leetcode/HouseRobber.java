c80664761372e9c9d055e929bb889c8a900be2e0
Simon Jia <sjia@nisum.com>
2023-07-16 12:39:19 -0700

    Leetcode #198, HouseRobber, recursive, TLE

--- src/main/java/com/sjia/Leetcode/HouseRobber.java
+++ src/main/java/com/sjia/Leetcode/HouseRobber.java
@@ -3,7 +3,16 @@ package com.sjia.Leetcode;
 import java.util.Arrays;
 
 public class HouseRobber {
-    // #198 https://leetcode.com/problems/house-robber/
+    // #198. House Robber https://leetcode.com/problems/house-robber/
+
+    /*
+    Given an integer array nums representing the amount of money of each house,
+    return the maximum amount of money you can rob tonight without alerting the police.
+
+    Input: nums = [1,2,3,1]     Output: 4
+    Input: nums = [2,7,9,3,1]   Output: 12
+
+     */
 
     // DP, Runtime: 0 ms, faster than 100.00% of Java online submissions for House Robber.
     //Memory Usage: 38.8 MB, less than 5.09% of Java online submissions for House Robber.
@@ -18,9 +27,8 @@ public class HouseRobber {
         return result;
     }
 
-    // Recursive Time Limit Exceeded
-/*
-    public int rob(int[] nums) {
+    // Recursive, TLE, Time Limit Exceeded
+    public int robRecursive(int[] nums) {
         return rob(nums, nums.length - 1);
     }
     private int rob(int[] nums, int i) {
@@ -28,7 +36,7 @@ public class HouseRobber {
             return 0;
         }
         return Math.max(rob(nums, i - 2) + nums[i], rob(nums, i - 1));
-    }*/
+    }
 
 
     public static void main(String[] args) {
@@ -40,6 +48,16 @@ public class HouseRobber {
         int result = solution.rob(testData);
 
         System.out.printf(" testData  %s , rob max is %s \n",Arrays.toString(testData), result);
+
+        // Test case 1
+        int[] nums1 = {1, 2, 3, 1};
+        int maxAmount1 = solution.rob(nums1);
+        System.out.println("Maximum Amount Test Case 1: " + maxAmount1);  // Expected output: 4
+
+        // Test case 2
+        int[] nums2 = {2, 7, 9, 3, 1};
+        int maxAmount2 = solution.rob(nums2);
+        System.out.println("Maximum Amount Test Case 2: " + maxAmount2);  // Expected output: 12
     }
 
 }
