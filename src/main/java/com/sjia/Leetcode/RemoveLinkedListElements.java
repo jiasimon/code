9bdc62ed3eda73571f266dc4d025d6453895feeb
Simon Jia <sjia@nisum.com>
2023-07-17 14:37:53 -0700

    Leetcode #203, RemoveLinkedListElements, dummy and pre pointer

--- src/main/java/com/sjia/Leetcode/RemoveLinkedListElements.java
+++ src/main/java/com/sjia/Leetcode/RemoveLinkedListElements.java
@@ -2,28 +2,91 @@ package com.sjia.Leetcode;
 
 public class RemoveLinkedListElements {
 
-    // #203 https://leetcode.com/problems/remove-linked-list-elements/
+    // #203. Remove Linked List Elements https://leetcode.com/problems/remove-linked-list-elements/
     // Remove all elements from a linked list of integers that have value val
+
+    /*
+    Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+    Input: head = [1,2,6,3,4,5,6], val = 6  Output: [1,2,3,4,5]
+    Input: head = [], val = 1   Output: []
+    Input: head = [7,7,7,7], val = 7    Output: []
+
+     */
 
     //Runtime: 1 ms, faster than 93.29% of Java online submissions for Remove Linked List Elements.
     //Memory Usage: 46.4 MB, less than 5.24% of Java online submissions for Remove Linked List Elements.
 
-    public ListNode removeElements(ListNode head, int val) {
-        ListNode fakeHead = new ListNode(0);
-        fakeHead.next = head;
+    // iterative with pre pointer
+    public static ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
 
-        ListNode pre = fakeHead, current=head;
+        ListNode pre = dummy, current=head;
         while (current !=null) {
             if (current.val == val) pre.next = current.next;
             else {
                 pre = current;
-
             }
             current = current.next;
         }
-        return fakeHead.next;
+        return dummy.next;
 
     }
 
 
+    public static void main(String[] args) {
+        // Test Case 1: Remove all occurrences of 2 from the linked list: 1 -> 2 -> 6 -> 3 -> 4 -> 5 -> 2
+        ListNode head1 = new ListNode(1);
+        ListNode node1 = new ListNode(2);
+        ListNode node2 = new ListNode(6);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        ListNode node6 = new ListNode(2);
+
+        head1.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+
+        int val1 = 2;
+        ListNode result1 = removeElements(head1, val1);
+
+        System.out.print("After removing " + val1 + ": ");
+        printLinkedList(result1);
+
+        // Test Case 2: Remove all occurrences of 3 from the linked list: 3 -> 1 -> 3 -> 2 -> 3 -> 4 -> 5
+        ListNode head2 = new ListNode(3);
+        ListNode node7 = new ListNode(1);
+        ListNode node8 = new ListNode(3);
+        ListNode node9 = new ListNode(2);
+        ListNode node10 = new ListNode(3);
+        ListNode node11 = new ListNode(4);
+        ListNode node12 = new ListNode(5);
+
+        head2.next = node7;
+        node7.next = node8;
+        node8.next = node9;
+        node9.next = node10;
+        node10.next = node11;
+        node11.next = node12;
+
+        int val2 = 3;
+        ListNode result2 = removeElements(head2, val2);
+
+        System.out.print("After removing " + val2 + ": ");
+        printLinkedList(result2);
+    }
+
+    // Helper method to print the linked list
+    private static void printLinkedList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
 }
