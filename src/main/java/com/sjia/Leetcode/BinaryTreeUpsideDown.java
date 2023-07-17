517ec7a504bc99cb3814e39c4d84bd7b6cecb631
Simon Jia <sjia@nisum.com>
2023-07-17 10:00:21 -0700

    Leetcode #156, BinaryTreeUpsideDown, add graph and tests

--- src/main/java/com/sjia/Leetcode/BinaryTreeUpsideDown.java
+++ src/main/java/com/sjia/Leetcode/BinaryTreeUpsideDown.java
@@ -21,6 +21,14 @@ public class BinaryTreeUpsideDown {
 
 
     // recursive
+    /*
+    You can turn a binary tree upside down with the following steps:
+
+    The original left child becomes the new root.
+    The original root becomes the new right child.
+    The original right child becomes the new left child.
+
+     */
     public TreeNode upsideDownBinaryTreeRecursive(TreeNode root) {
         if (root == null || root.left == null) {
             return root;
@@ -37,5 +45,41 @@ public class BinaryTreeUpsideDown {
         return newRoot;
     }
 
+
+
+
+    public static void main(String[] args) {
+        BinaryTreeUpsideDown solution = new BinaryTreeUpsideDown();
+
+        // Create a binary tree
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        // Upside-down the binary tree
+        TreeNode newRoot = solution.upsideDownBinaryTreeRecursive(root);
+
+        // Print the upside-down binary tree
+        System.out.print("Upside-Down Binary Tree: ");
+        printTree(newRoot);  // Expected output: 4 5 2 3 1
+    }
+
+    public static void printTree(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        System.out.print(root.val + " ");
+
+        if (root.left != null) {
+            printTree(root.left);
+        }
+
+        if (root.right != null) {
+            printTree(root.right);
+        }
+    }
 
 }
