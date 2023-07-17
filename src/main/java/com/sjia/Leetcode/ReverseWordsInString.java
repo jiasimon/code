155e342ad9a07e60499ad065d1cb2d9aa67352ed
Simon Jia <sjia@nisum.com>
2023-07-17 05:35:48 -0700

    Leetcode #151, ReverseWordsInString, StringBuilder,append from end

--- src/main/java/com/sjia/Leetcode/ReverseWordsInString.java
+++ src/main/java/com/sjia/Leetcode/ReverseWordsInString.java
@@ -34,4 +34,20 @@ public class ReverseWordsInString {
     }
 
 
+    // StringBuilder
+    public String reverseWords(String s) {
+        s = s.trim();
+
+        String[] words = s.split(" +");
+
+        StringBuilder sb = new StringBuilder();
+        for ( int i = words.length-1 ; i>=0; i--) {
+            sb.append(words[i]);
+            if(i>0) sb.append(" ");
+        }
+        return sb.toString();
+
+    }
+
+
 }
