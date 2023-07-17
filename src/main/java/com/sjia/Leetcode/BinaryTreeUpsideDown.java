43edc14d4f4de99e8ceeeed963c911b93061b091
Simon Jia <sjia@nisum.com>
2023-07-17 10:36:42 -0700

    Leetcode #156, BinaryTreeUpsideDown, iterative

--- src/main/java/com/sjia/Leetcode/BinaryTreeUpsideDown.java
+++ src/main/java/com/sjia/Leetcode/BinaryTreeUpsideDown.java
@@ -48,6 +48,25 @@ public class BinaryTreeUpsideDown {
 
 
 
+    // iterative
+    //
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        TreeNode prevRoot = null;
+        TreeNode prevRightChild = null;
+
+        while (root != null) {
+            TreeNode nextRoot = root.left; // Cache next root
+            root.left = prevRightChild;
+            prevRightChild = root.right;
+            root.right = prevRoot;
+            prevRoot = root; // Record previous root
+            root = nextRoot; // Update root
+        }
+
+        return prevRoot;
+    }
+
+
     public static void main(String[] args) {
         BinaryTreeUpsideDown solution = new BinaryTreeUpsideDown();
 
