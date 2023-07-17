0c70d489a164684fe12e29977a78ab876212aaee
Simon Jia <sjia@nisum.com>
2023-07-17 06:36:09 -0700

    Leetcode #190, ReverseBits, Bit operation, revisit

--- src/main/java/com/sjia/Leetcode/ReverseBits.java
+++ src/main/java/com/sjia/Leetcode/ReverseBits.java
@@ -55,6 +55,20 @@ public class ReverseBits {
     }
 
 
+    // bit operation,
+    public int reverseBits(int n) {
+        if (n == 0) return 0;
+
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result <<= 1;
+            if ((n & 1) == 1) result++;
+            n >>= 1;
+        }
+        return result;
+    }
+
+
     public static void main(String[] args) {
         ReverseBits solution = new ReverseBits();
 
