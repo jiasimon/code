150d23fe27d5323c476979c2d40a800af9fc849d
Simon Jia <sjia@nisum.com>
2023-07-18 07:33:30 -0700

    Leetcode #198, HouseRobber, iterative, dp[1] = Math.max(nums[0], nums[1])

--- src/main/java/com/sjia/Leetcode/HouseRobber.java
+++ src/main/java/com/sjia/Leetcode/HouseRobber.java
@@ -59,6 +59,30 @@ public class HouseRobber {
     }
 
 
+
+    // Iterative
+    public int robIterative(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return nums[0];
+        } else if (n == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+        }
+
+        return dp[n - 1];
+    }
+
+
     public static void main(String[] args) {
 
         //int[] testData  = {0,0};
