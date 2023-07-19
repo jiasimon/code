254ba5de3c8169aef1156a92f25f7a8b0ee9dc91
Simon Jia <sjia@nisum.com>
2023-07-19 05:44:41 -0700

    Leetcode #208, ImplementTrie, subclass TrieNode

--- src/main/java/com/sjia/Leetcode/ImplementTrie.java
+++ src/main/java/com/sjia/Leetcode/ImplementTrie.java
@@ -15,6 +15,7 @@ public class ImplementTrie {
     trie.search("app");     // return True
      */
 
+/*
 
     class Trie {
         private boolean isEndOfWord;
@@ -63,7 +64,52 @@ public class ImplementTrie {
         }
     }
 
+*/
 
+
+    // use TrieNode class with Trie
+    class TrieNode {
+        public TrieNode[] children = new TrieNode[26];
+        public boolean isWord = false;
+    }
+
+    class Trie {
+
+        private TrieNode root = new TrieNode();
+
+        public void insert(String word) {
+            TrieNode node = root;
+            for (final char c : word.toCharArray()) {
+                final int i = c - 'a';
+                if (node.children[i] == null)
+                    node.children[i] = new TrieNode();
+                node = node.children[i];
+            }
+            node.isWord = true;
+        }
+
+        public boolean search(String word) {
+            TrieNode node = find(word);
+            return node != null && node.isWord;
+        }
+
+        public boolean startsWith(String prefix) {
+            return find(prefix) != null;
+        }
+
+
+
+        private TrieNode find(String prefix) {
+            TrieNode node = root;
+            for (final char c : prefix.toCharArray()) {
+                final int i = c - 'a';
+                if (node.children[i] == null)
+                    return null;
+                node = node.children[i];
+            }
+            return node;
+        }
+    }
 
 
 
