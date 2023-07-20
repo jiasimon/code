80acbb18fa106a4745ea8d5906f4bc721fb2c689
Simon Jia <sjia@nisum.com>
2023-07-20 05:35:39 -0700

    Leetcode #217, ContainsDuplicateK, HashSet, sliding window

--- src/main/java/com/sjia/Leetcode/ContainsDuplicateK.java
+++ src/main/java/com/sjia/Leetcode/ContainsDuplicateK.java
@@ -21,8 +21,9 @@ public class ContainsDuplicateK {
 
     // Runtime: 6 ms, faster than 76.44% of Java online submissions for Contains Duplicate II.
     //Memory Usage: 43.2 MB, less than 56.72% of Java online submissions for Contains Duplicate II.
-/*
-    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+    // HashSet, sliding window
+    public boolean containsNearbyDuplicateHashSet(int[] nums, int k) {
         HashSet<Integer> tmp = new HashSet<>();
         for (int i=0; i< nums.length; i++) {
             if(tmp.contains(nums[i])) return true;
@@ -30,7 +31,7 @@ public class ContainsDuplicateK {
             if (tmp.size() > k ) tmp.remove(nums[i-k]);
         }
         return false;
-    }*/
+    }
 
 
     // Runtime: 10 ms, faster than 46.12% of Java online submissions for Contains Duplicate II.
@@ -51,7 +52,7 @@ public class ContainsDuplicateK {
         int[] testData  = {1,2,3,1,2,3};
         int k =2;
         ContainsDuplicateK solution = new ContainsDuplicateK();
-        boolean result = solution.containsNearbyDuplicate(testData, k);
+        boolean result = solution.containsNearbyDuplicateHashSet(testData, k);
         System.out.printf("array %s  in %s contain duplicate : %s", Arrays.toString(testData), k, result);
         System.out.println();
 
