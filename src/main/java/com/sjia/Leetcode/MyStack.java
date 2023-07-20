d162a96b72a3dc9ee0920737f7d4df40b4bf492d
Simon Jia <sjia@nisum.com>
2023-07-20 13:13:15 -0700

    Leetcode #225, MyStack, single LinkedList, queue.offer(queue.poll()

--- src/main/java/com/sjia/Leetcode/MyStack.java
+++ src/main/java/com/sjia/Leetcode/MyStack.java
@@ -8,6 +8,46 @@ import java.util.Queue;
 public class MyStack {
     // #225. Implement Stack using Queues   https://leetcode.com/problems/implement-stack-using-queues/
 
+
+    // Single LinkedList
+    class MyStack2 {
+        Queue<Integer> queue;
+
+        /** Initialize your data structure here. */
+        public MyStack2() {
+            queue = new LinkedList<Integer>();
+        }
+
+        /** Push element x onto stack. */
+        public void push(int x) {
+            int n = queue.size();
+            queue.offer(x);
+            for (int i = 0; i < n; i++) {
+                queue.offer(queue.poll());
+            }
+        }
+
+        /** Removes the element on top of the stack and returns that element. */
+        public int pop() {
+            return queue.poll();
+        }
+
+        /** Get the top element. */
+        public int top() {
+            return queue.peek();
+        }
+
+        /** Returns whether the stack is empty. */
+        public boolean empty() {
+            return queue.isEmpty();
+        }
+    }
+
+
+
+
+    // queue1 the actual stack, queue2 is tmp
+    // push - O(n) pop O(1)
     Queue<Integer> queue1;
     Queue<Integer> queue2;
 
