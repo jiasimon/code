d368975a7219fcc93fb6f58129cb8e43d5056994
Simon Jia <sjia@nisum.com>
2023-07-21 10:08:00 -0700

    Leetcode #229, MajorityElement2, add tests

---
+++ src/main/java/com/sjia/Leetcode/MajorityElement2.java
@@ -0,0 +1,39 @@
+package com.sjia.Leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MajorityElement2 {
+    // #229. Majority Element II    https://leetcode.com/problems/majority-element-ii/
+    /*
+    Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
+    Input: nums = [3,2,3]   Output: [3]
+    Input: nums = [1]   Output: [1]
+    Input: nums = [1,2] Output: [1,2]
+    1 <= nums.length <= 5 * 10^4
+    -10^9 <= nums[i] <= 10^9
+
+     */
+
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+
+        return result;
+    }
+
+
+
+    // Test the code
+    public static void main(String[] args) {
+        MajorityElement2 solution = new MajorityElement2();
+
+        int[] nums = {3, 2, 3}; // Output: [3]
+        System.out.println(solution.majorityElement(nums));
+
+        int[] nums2 = {1, 1, 1, 3, 3, 2, 2, 2}; // Output: [1, 2]
+        System.out.println(solution.majorityElement(nums2));
+    }
+
+
+
+}
