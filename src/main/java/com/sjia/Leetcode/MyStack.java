775a85531065af5993cb6dc85a128ad758a40191
Simon Jia <sjia@nisum.com>
2023-07-21 07:17:04 -0700

    Leetcode #225, MyStack,  ArrayDeque

--- src/main/java/com/sjia/Leetcode/MyStack.java
+++ src/main/java/com/sjia/Leetcode/MyStack.java
@@ -1,12 +1,39 @@
 package com.sjia.Leetcode;
 
 
+import java.util.ArrayDeque;
 import java.util.LinkedList;
 import java.util.Queue;
 
 // Double Queues
 public class MyStack {
     // #225. Implement Stack using Queues   https://leetcode.com/problems/implement-stack-using-queues/
+
+
+
+
+    // ArrayDeque
+    class MyStackArrayDeque {
+        public void push(int x) {
+            q.offer(x);
+            for (int i = 0; i < q.size() - 1; i++)
+                q.offer(q.poll());
+        }
+
+        public int pop() {
+            return q.poll();
+        }
+
+        public int top() {
+            return q.peek();
+        }
+
+        public boolean empty() {
+            return q.isEmpty();
+        }
+
+        private Queue<Integer> q = new ArrayDeque<>();
+    }
 
 
     // Single LinkedList
