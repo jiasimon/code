13511955d37441bf0ee4abd3d8d60da0f7272227
Simon Jia <sjia@nisum.com>
2023-07-22 09:00:33 -0700

    Leetcode #236, LowestCommonAncestorBinaryTree, recursive, left != null && right !=null

--- src/main/java/com/sjia/Leetcode/LowestCommonAncestorBinaryTree.java
+++ src/main/java/com/sjia/Leetcode/LowestCommonAncestorBinaryTree.java
@@ -1,15 +1,19 @@
 package com.sjia.Leetcode;
 
 public class LowestCommonAncestorBinaryTree {
-    // #236 https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/  #fb
+    // #236. Lowest Common Ancestor of a Binary Tree https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/  #fb
     // The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants
     // (where we allow a node to be a descendant of itself).”
 
-    // The number of nodes in the tree is in the range [2, 105].
-    // -10^9 <= Node.val <= 10^9
-    // All Node.val are unique.
-    // p != q
-    // p and q will exist in the tree.
+
+    /*
+         The number of nodes in the tree is in the range [2, 105].
+         -10^9 <= Node.val <= 10^9
+         All Node.val are unique.
+         p != q
+         p and q will exist in the tree.
+     */
+
 
 
 
