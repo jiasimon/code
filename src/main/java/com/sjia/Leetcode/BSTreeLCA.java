8b119398dfb82ae1761bd9115b9ebf563026782b
Simon Jia <sjia@nisum.com>
2023-07-21 18:20:34 -0700

    Leetcode #235, BSTreeLCA, recursive

--- src/main/java/com/sjia/Leetcode/BSTreeLCA.java
+++ src/main/java/com/sjia/Leetcode/BSTreeLCA.java
@@ -1,8 +1,12 @@
 package com.sjia.Leetcode;
 
 public class BSTreeLCA {
-    // #235 LCA of BST (Lowest Common Ancestor of a Binary Search Tree)
+    // #235. Lowest Common Ancestor of a Binary Search Tree
     // https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+
+    /*
+    Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
+     */
 
 
     // Runtime: 11 ms, faster than 8.49% of Java online submissions for Lowest Common Ancestor of a Binary Search Tree.
@@ -11,11 +15,16 @@ public class BSTreeLCA {
     // Runtime: 6 ms, faster than 40.20% of Java online submissions for Lowest Common Ancestor of a Binary Search Tree.
     //Memory Usage: 47.5 MB, less than 5.01% of Java online submissions for Lowest Common Ancestor of a Binary Search Tree.
 
+    // recursive
+    // 6 ms, 97.37%; 44.1 MB, 52.31%
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        int r = root.val, pVal = p.val, qVal = q.val;
-        if ( pVal > r && qVal > r) return lowestCommonAncestor(root.right, p,q);
-        else if ( pVal < r && qVal < r)  return lowestCommonAncestor(root.left, p,q);
-        else return root;
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
     }
 
 
