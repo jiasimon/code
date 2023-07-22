0594b83c3191ed005fc08ed9c1f8cb0f348fe4a1
Simon Jia <sjia@nisum.com>
2023-07-22 08:23:20 -0700

    Leetcode #235, BSTreeLCA, iterative , test

--- src/main/java/com/sjia/Leetcode/BSTreeLCA.java
+++ src/main/java/com/sjia/Leetcode/BSTreeLCA.java
@@ -5,6 +5,7 @@ public class BSTreeLCA {
     // https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
 
     /*
+    LowestCommonAncestorBST : BSTreeLCA
     Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
      */
 
@@ -27,5 +28,44 @@ public class BSTreeLCA {
         return root;
     }
 
+
+
+    // iterative
+    public TreeNode lowestCommonAncestorIterative(TreeNode root, TreeNode p, TreeNode q) {
+        while (root != null) {
+            if (root.val > p.val && root.val > q.val) {
+                root = root.left;
+            }
+            else if (root.val < p.val && root.val < q.val) {
+                root = root.right;
+            } else {
+                return root;
+            }
+
+        }
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+        BSTreeLCA lcaBST = new BSTreeLCA();
+
+        // Create a Binary Search Tree
+        TreeNode root = new TreeNode(6);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(0);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(7);
+        root.right.right = new TreeNode(9);
+        root.left.right.left = new TreeNode(3);
+        root.left.right.right = new TreeNode(5);
+
+        TreeNode p = root.left; // Node with value 2
+        TreeNode q = root.left.right.right; // Node with value 5
+
+        TreeNode lowestCommonAncestor = lcaBST.lowestCommonAncestor(root, p, q);
+        System.out.println("Lowest Common Ancestor: " + lowestCommonAncestor.val); // Output: 2
+    }
 
 }
