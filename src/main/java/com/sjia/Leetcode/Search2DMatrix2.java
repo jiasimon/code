f745ed48743f4390c3a4fa04edd16ee725babc0b
Simon Jia <sjia@nisum.com>
2023-07-23 08:00:04 -0700

    Leetcode #240, Search2DMatrix2, binary search , top right

--- src/main/java/com/sjia/Leetcode/Search2DMatrix2.java
+++ src/main/java/com/sjia/Leetcode/Search2DMatrix2.java
@@ -14,7 +14,25 @@ public class Search2DMatrix2 {
 
 
     public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
 
+        int r = matrix.length;
+        int c = matrix[0].length;
+
+        int row = 0;
+        int col = c-1;
+
+         while (row < r && col >=0 ) {
+             if (matrix[row][col] == target) {
+                 return true;
+             } else if (matrix[row][col] < target) {
+                 row++;
+             } else {
+                 col--;
+             }
+         }
          return false;
     }
 
