98bed85d37245ad2461fd299346826720ba5156d
Simon Jia <sjia@nisum.com>
2023-07-24 11:28:28 -0700

    Leetcode #246, StrobogrammaticNumber, HashMap for 01689

--- src/main/java/com/sjia/Leetcode/StrobogrammaticNumber.java
+++ src/main/java/com/sjia/Leetcode/StrobogrammaticNumber.java
@@ -4,10 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class StrobogrammaticNumber {
-    // #246 https://leetcode.com/problems/strobogrammatic-number
+    // #246. Strobogrammatic Number https://leetcode.com/problems/strobogrammatic-number
+    // https://grandyang.com/leetcode/246/
+
+    /*
+    A strobogrammatic number is a number that looks the same when rotated 180 degrees (looked at upside down).
+     */
 
 
-
+    // HashMap for 01689
     public boolean isStrobogrammatic(String num) {
         if (num.length() ==0) return true;
         Map<Character, Character> tmp = new HashMap<>();
@@ -27,5 +32,17 @@ public class StrobogrammaticNumber {
             }
         }
         return true;
+    }
+
+
+
+    public static void main(String[] args) {
+        StrobogrammaticNumber strobogrammaticNumber = new StrobogrammaticNumber();
+
+        System.out.println(strobogrammaticNumber.isStrobogrammatic("69")); // Output: true
+        System.out.println(strobogrammaticNumber.isStrobogrammatic("88")); // Output: true
+        System.out.println(strobogrammaticNumber.isStrobogrammatic("818")); // Output: true
+        System.out.println(strobogrammaticNumber.isStrobogrammatic("8188")); // Output: false
+        System.out.println(strobogrammaticNumber.isStrobogrammatic("00")); // Output: true
     }
 }
