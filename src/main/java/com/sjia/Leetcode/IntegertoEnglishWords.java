ba52bfcac8f2b70c8d6e5d1de9c1108175275784
Simon Jia <sjia@nisum.com>
2023-07-25 14:24:20 -0700

    Leetcode #273, IntegertoEnglishWords, < 20, < 100, TENS, THOUSANDS

--- src/main/java/com/sjia/Leetcode/IntegertoEnglishWords.java
+++ src/main/java/com/sjia/Leetcode/IntegertoEnglishWords.java
@@ -1,7 +1,11 @@
 package com.sjia.Leetcode;
 
 public class IntegertoEnglishWords {
-    // #273 https://leetcode.com/problems/integer-to-english-words/  #fb
+    // #273. Integer to English Words https://leetcode.com/problems/integer-to-english-words/  #fb
+
+    /*
+    Convert a non-negative integer num to its English words representation.
+     */
 
     // 0 <= num <= 2^31 - 1
 
@@ -11,6 +15,8 @@ public class IntegertoEnglishWords {
     private final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
     private final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
 
+
+    // < 20, < 100, TENS, THOUSANDS
     public String numberToWords(int num) {
         if (num == 0) return "Zero";
 
@@ -19,7 +25,7 @@ public class IntegertoEnglishWords {
 
         while (num > 0) {
             if (num % 1000 != 0)
-                words = helper(num % 1000) +THOUSANDS[i] + " " + words;
+                words = helper(num % 1000) + THOUSANDS[i] + " " + words;
             num /= 1000;
             i++;
         }
@@ -39,6 +45,19 @@ public class IntegertoEnglishWords {
     }
 
 
+
+    public static void main(String[] args) {
+        IntegertoEnglishWords solution = new IntegertoEnglishWords();
+
+        int num = 1234567;
+        String result = solution.numberToWords(num);
+        System.out.println("The English representation of " + num + " is: " + result);
+        // Output: The English representation of 1234567 is: One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven
+
+        int num2 = 1234567892;
+        String result2 = solution.numberToWords(num2);
+        System.out.println("The English representation of " + num + " is: " + result2);
+    }
 
 
 }
