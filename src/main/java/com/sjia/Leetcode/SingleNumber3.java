f6f953fba67eafc22b6b743204f6c191fac8e80e
Simon Jia <sjia@nisum.com>
2023-07-25 09:24:32 -0700

    Leetcode #260, SingleNumber3, Hashset, !set.add(n), set to list to int[]

--- src/main/java/com/sjia/Leetcode/SingleNumber3.java
+++ src/main/java/com/sjia/Leetcode/SingleNumber3.java
@@ -1,6 +1,8 @@
 package com.sjia.Leetcode;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class SingleNumber3 {
@@ -13,8 +15,9 @@ public class SingleNumber3 {
      */
 
 
+    // Set contains and remove
     // 4ms, 46.96%; 44.69mb, 19.80%
-    public int[] singleNumber(int[] nums) {
+    public int[] singleNumber2(int[] nums) {
         int[] res = new int[2];
         Set<Integer> set = new HashSet<>();
         for (int n: nums) {
@@ -34,10 +37,26 @@ public class SingleNumber3 {
     }
 
 
+    // Hashset, !set.add(n), set to list to int[]
+    // 4ms, 46.96%, 44.54mb, 34.58%
+    public int[] singleNumberHashSet(int[] nums) {
+        int[] res = new int[2];
+        Set<Integer> tmp = new HashSet<>();
+        for (int n : nums) {
+            if (!tmp.add(n)) {
+                tmp.remove(n);
+            }
+        }
+        List list = new ArrayList<>(tmp);
+        res[0] = (int)list.get(0);
+        res[1] = (int)list.get(1);
+        return res;
+    }
+
     public static void main(String[] args) {
         int[] nums = {1, 2, 1, 3, 2, 5};
         SingleNumber3 solution = new SingleNumber3();
-        int[] result = solution.singleNumber(nums);
+        int[] result = solution.singleNumberHashSet(nums);
         System.out.println("The two distinct numbers are: " + result[0] + " and " + result[1]);
         // Output: The two distinct numbers are: 3 and 5
     }
