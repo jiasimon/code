a75366815bd92854dd0a94d409fdf9ef2ead1cd7
Simon Jia <sjia@nisum.com>
2023-07-25 08:57:55 -0700

    Leetcode #260, SingleNumber3, Hashset, convert Set of Integers to int[]

--- src/main/java/com/sjia/Leetcode/SingleNumber3.java
+++ src/main/java/com/sjia/Leetcode/SingleNumber3.java
@@ -1,5 +1,8 @@
 package com.sjia.Leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SingleNumber3 {
     // #260. Single Number III  https://leetcode.com/problems/single-number-iii/
     /*
@@ -9,10 +12,25 @@ public class SingleNumber3 {
     You must write an algorithm that runs in linear runtime complexity and uses only constant extra space.
      */
 
+
+    // 4ms, 46.96%; 44.69mb, 19.80%
     public int[] singleNumber(int[] nums) {
+        int[] res = new int[2];
+        Set<Integer> set = new HashSet<>();
+        for (int n: nums) {
+            if (set.contains(n)) {
+                set.remove(n);
+            } else {
+                set.add(n);
+            }
+        }
 
-
-        return new int[]{1, 3};
+        // convert Set of Integers to int[]
+        int i = 0;
+        for (int num : set) {
+            res[i++] = num;
+        }
+        return res;
     }
 
 
