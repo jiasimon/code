7874c8bb08773242aae70512dfbeef47109a4b2d
Simon Jia <sjia@nisum.com>
2023-07-27 15:29:44 -0700

    Leetcode #283, MoveZeroes,  insert non-zero, two pass

--- src/main/java/com/sjia/Leetcode/MoveZeroes.java
+++ src/main/java/com/sjia/Leetcode/MoveZeroes.java
@@ -4,24 +4,35 @@ import java.util.Arrays;
 
 public class MoveZeroes {
 
-    // #283 https://leetcode.com/problems/move-zeroes/
+    // #283. Move Zeroes https://leetcode.com/problems/move-zeroes/
 
-    // Runtime: 0 ms, faster than 100.00% of Java online submissions for Move Zeroes.
-    //Memory Usage: 39.5 MB, less than 84.61% of Java online submissions for Move Zeroes.
-/*
-    public void moveZeroes(int[] nums) {
-        if (nums == null || nums.length ==1) return;
+    /*
+    Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+    Note that you must do this in-place without making a copy of the array.
+    Input: nums = [0,1,0,3,12]  Output: [1,3,12,0,0]
+    Input: nums = [0]       Output: [0]
+
+     */
+
+
+    // insert non-zero, two pass
+    public void moveZeroesByInsert(int[] nums) {
+        if (nums == null || nums.length ==0 ) return;
+
         int p=0;
         for (int i=0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                nums[p]=nums[i];
+            if(nums[i] != 0) {
+                nums[p] = nums[i];
                 p++;
             }
         }
-        for (int i=p; i<nums.length; i++) {
-            nums[i]=0;
+
+        for ( int i=p; i < nums.length; i++ ) {
+            nums[i] = 0;
         }
-    }*/
+
+    }
 
 
     // Runtime: 0 ms, faster than 100.00% of Java online submissions for Move Zeroes.
@@ -58,10 +69,10 @@ public class MoveZeroes {
 
 
     public static void main(String[] args) {
-        // int[] testData = { 0,1,0,3,12 };
-        int[] testData = { 1,2,3,0,12 };
+         int[] testData = { 0,1,0,3,12 };
+//        int[] testData = { 1,2,3,0,12 };
         MoveZeroes solution = new MoveZeroes();
-        solution.moveZeroes(testData);
+        solution.moveZeroesByInsert(testData);
         System.out.println("The result is: " + Arrays.toString(testData));
 
     }
