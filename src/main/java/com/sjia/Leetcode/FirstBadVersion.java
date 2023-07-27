66c4c5ca346a665a6a943a0f539acd474ca12c26
Simon Jia <sjia@nisum.com>
2023-07-26 20:09:56 -0700

    Leetcode #278, FirstBadVersion,  binary search, add tests

--- src/main/java/com/sjia/Leetcode/FirstBadVersion.java
+++ src/main/java/com/sjia/Leetcode/FirstBadVersion.java
@@ -2,11 +2,13 @@ package com.sjia.Leetcode;
 
 public class FirstBadVersion {
 
-    // #278 https://leetcode.com/problems/first-bad-version/
+    // #278. First Bad Version https://leetcode.com/problems/first-bad-version/
     //
 
     // Runtime: 12 ms, faster than 99.38% of Java online submissions for First Bad Version.
     //Memory Usage: 36.4 MB, less than 8.03% of Java online submissions for First Bad Version.
+
+    // Binary search
     public int firstBadVersion(int n) {
         int left = 1, right = n;
         while (left < right) {
@@ -21,8 +23,16 @@ public class FirstBadVersion {
 
     }
 
-    private boolean isBadVersion(int mid) {
-        return mid >20;
+    private boolean isBadVersion(int version) {
+        return version >= 4;
+    }
+
+
+    public static void main(String[] args) {
+        int n = 50;
+        FirstBadVersion solution = new FirstBadVersion();
+        int firstBadVersion = solution.firstBadVersion(n);
+        System.out.println("The first bad version is: " + firstBadVersion); // Output: The first bad version is: 4
     }
 
 
