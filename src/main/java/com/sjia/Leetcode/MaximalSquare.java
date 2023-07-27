9503aed6ce644e90d6ba06053adec28a3f8fe8ee
Simon Jia <sjia@nisum.com>
2023-07-27 08:01:53 -0700

    Leetcode #221, MaximalSquare, matrix[i][j] == '1' ? 1 : 0

--- src/main/java/com/sjia/Leetcode/MaximalSquare.java
+++ src/main/java/com/sjia/Leetcode/MaximalSquare.java
@@ -49,6 +49,28 @@ public class MaximalSquare {
     }
 
 
+    // matrix[i][j] == '1' ? 1 : 0
+    public int maximalSquare2(char[][] matrix) {
+        final int m = matrix.length;
+        final int n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        int maxLength = 0;
+
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j) {
+                if (i == 0 || j == 0 || matrix[i][j] == '0')
+                    dp[i][j] = matrix[i][j] == '1' ? 1 : 0;
+                else
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                maxLength = Math.max(maxLength, dp[i][j]);
+            }
+
+        return maxLength * maxLength;
+
+    }
+
+
+
     public static void main(String[] args) {
         MaximalSquare maximalSquare = new MaximalSquare();
 
