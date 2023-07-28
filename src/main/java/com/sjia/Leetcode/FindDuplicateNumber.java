d9ae69884c5eb6918cb6237d2b091a435e41a808
Simon Jia <sjia@nisum.com>
2023-07-28 09:20:59 -0700

    Leetcode #287, FindDuplicateNumber,  brute force, 2 loop

--- src/main/java/com/sjia/Leetcode/FindDuplicateNumber.java
+++ src/main/java/com/sjia/Leetcode/FindDuplicateNumber.java
@@ -43,6 +43,22 @@ public class FindDuplicateNumber {
     }
 
 
+    // brute
+    public static int findDuplicate_2loops(int[] nums) {
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (nums[i] == nums[j]) {
+                    return nums[i];
+                }
+            }
+        }
+
+        return len;
+    }
+
+
+
 
     public static void main(String[] args) {
         int[] nums = {1, 3, 4, 2, 2};
