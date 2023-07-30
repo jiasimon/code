c6966fa3aeeb0ee63e944cd1b601516345ba8eb2
Simon Jia <sjia@nisum.com>
2023-07-30 07:50:48 -0700

    Leetcode #252, MeetingRooms, sort, (a, b) -> a[0] - b[0])

--- src/main/java/com/sjia/Leetcode/MeetingRooms.java
+++ src/main/java/com/sjia/Leetcode/MeetingRooms.java
@@ -1,5 +1,7 @@
 package com.sjia.Leetcode;
 
+import java.util.Arrays;
+
 public class MeetingRooms {
     // #252. Meeting Rooms https://leetcode.com/problems/meeting-rooms/
     // https://grandyang.com/leetcode/252/
@@ -14,7 +16,7 @@ public class MeetingRooms {
 
 
     // brute force
-    public boolean canAttendMeetings(int[][] intervals) {
+    public boolean canAttendMeetings_brute(int[][] intervals) {
         for (int i=0; i < intervals.length; i++) {
             for (int j= i+1; j < intervals.length; j++ ) {
                 if ( intervals[i][0] >= intervals[j][0] && intervals[i][0] < intervals[j][1]
@@ -25,6 +27,28 @@ public class MeetingRooms {
         }
         return true;
     }
+
+
+
+    // sort, (a, b) -> a[0] - b[0]
+    public boolean canAttendMeetings(int[][] intervals) {
+        if (intervals == null || intervals.length <= 1) {
+            return true;
+        }
+
+        // Sort the intervals based on the start times
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        // Check for overlapping intervals
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < intervals[i - 1][1]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
 
     public static void main(String[] args) {
         int[][] intervals = {{0, 30}, {5, 10}, {15, 20}};
