ae2d0de6087ab8b558c16600f0e101af9ba39fc1
Simon Jia <sjia@nisum.com>
2023-07-30 09:07:10 -0700

    Leetcode #253, MeetingRooms2,  PriorityQueue endtime

--- src/main/java/com/sjia/Leetcode/MeetingRooms2.java
+++ src/main/java/com/sjia/Leetcode/MeetingRooms2.java
@@ -4,14 +4,24 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class MeetingRooms2 {
-    // #253 https://leetcode.com/problems/meeting-rooms-ii/
+    // #253. Meeting Rooms II https://leetcode.com/problems/meeting-rooms-ii/
+    /*
+    Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei),
+    find the minimum number of conference rooms required.
 
-    // Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei),
-    // find the minimum number of conference rooms required.
+    https://grandyang.com/leetcode/253/
+
+    Input: [[0, 30],[5, 10],[15, 20]]   Output: 2
+    Input: [[7,10],[2,4]]       Output: 1
+
+     */
+
+
 
     // Input: [[0, 30],[5, 10],[15, 20]]
 
 
+    // PriorityQueue
     public int minMeetingRooms(int[][] intervals) {
         if (intervals.length <=1) return 1;
         Arrays.sort(intervals, (a,b) -> a[0] -b[0] );
