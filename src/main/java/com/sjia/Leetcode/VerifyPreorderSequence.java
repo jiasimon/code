099b82a41cdcbc83c42358b1cf1392a97a149839
Simon Jia <sjia@nisum.com>
2023-07-31 08:25:34 -0700

    Leetcode #255, VerifyPreorderSequence,  stack

---
+++ src/main/java/com/sjia/Leetcode/VerifyPreorderSequence.java
@@ -0,0 +1,54 @@
+package com.sjia.Leetcode;
+
+import java.util.Stack;
+
+public class VerifyPreorderSequence {
+    // #255. Verify Preorder Sequence in Binary Search Tree https://leetcode.com/problems/verify-preorder-sequence-in-binary-search-tree/
+    /*
+    https://grandyang.com/leetcode/255/
+
+         5
+        / \
+       2   6
+      / \
+     1   3
+
+     Input: [5,2,6,1,3]     Output: false
+     Input: [5,2,1,3,6]     Output: true
+
+     */
+
+    // stack
+    public boolean verifyPreorder(int[] preorder) {
+        int minValue = Integer.MIN_VALUE;
+        Stack<Integer> stack = new Stack<>();
+
+        for (int num : preorder) {
+            if (num < minValue) {
+                return false;
+            }
+
+            while (!stack.isEmpty() && num > stack.peek()) {
+                minValue = stack.pop();
+            }
+
+            stack.push(num);
+        }
+
+        return true;
+    }
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        int[] preorder = {5, 2, 6, 1, 3};
+        VerifyPreorderSequence solution = new VerifyPreorderSequence();
+        boolean result = solution.verifyPreorder(preorder);
+        System.out.println("Is the sequence a valid preorder traversal of a BST? " + result); // Output: Is the sequence a valid preorder traversal of a BST? false
+    }
+
+}
