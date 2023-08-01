40bfba8b5aa9efe303aad57e0009ad9f19a0b615
Simon Jia <sjia@nisum.com>
2023-08-01 12:07:08 -0700

    Leetcode #313, SuperUglyNumber,  PriorityQueue, Hashset, TLE 86/87

---
+++ src/main/java/com/sjia/Leetcode/SuperUglyNumber.java
@@ -0,0 +1,56 @@
+package com.sjia.Leetcode;
+
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+public class SuperUglyNumber {
+    // #313. Super Ugly Number  https://leetcode.com/problems/super-ugly-number/description/
+    /*
+    A super ugly number is a positive integer whose prime factors are in the array primes.
+
+    Given an integer n and an array of integers primes, return the nth super ugly number.
+
+    The nth super ugly number is guaranteed to fit in a 32-bit signed integer.
+
+    Input: n = 12, primes = [2,7,13,19] Output: 32
+    Input: n = 1, primes = [2,3,5]  Output: 1
+
+     */
+
+
+    // PriorityQueue and hashset
+    // TLE,  86 / 87  n = 10000
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        Set<Long> seen = new HashSet<>();
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+
+        seen.add(1L);
+        pq.offer(1L);
+        int ugly = 0;
+        for (int i=0; i < n; i++) {
+            long curr = pq.poll();
+            ugly = (int) curr;
+
+            for ( int prime : primes) {
+                if (seen.add(curr * prime)) pq.offer(curr * prime);
+            }
+
+        }
+        return ugly;
+    }
+
+
+    public static void main(String[] args) {
+        SuperUglyNumber solution = new SuperUglyNumber();
+        int[] primes = {2, 7, 13, 19};
+        int n = 12;
+        int result = solution.nthSuperUglyNumber(n, primes);
+        System.out.println(result); // Output: 32
+
+        n = 100;
+        result = solution.nthSuperUglyNumber(n, primes);
+        System.out.println(result);  // Output: 5408
+    }
+
+}
