adc210db641de92fc6caa7b86197c355d885440c
Simon Jia <sjia@nisum.com>
2023-08-01 18:20:36 -0700

    Leetcode #315, CountSmallerNumbersAfterSelf,  mergeSort, Pair(val, index)

--- src/main/java/com/sjia/Leetcode/CountSmallerNumbersAfterSelf.java
+++ src/main/java/com/sjia/Leetcode/CountSmallerNumbersAfterSelf.java
@@ -17,7 +17,7 @@ public class CountSmallerNumbersAfterSelf {
 
 
     // naive, 2 loop
-    public List<Integer> countSmaller(int[] nums) {
+    public List<Integer> countSmaller_brute(int[] nums) {
         int n = nums.length;
         List<Integer> res = new ArrayList<>();
         for ( int i =0; i< n; i++) {
@@ -34,11 +34,83 @@ public class CountSmallerNumbersAfterSelf {
 
 
 
+    // mergeSort
+
+    class Pair {
+        int val;
+        int index;
+
+        public Pair(int val, int index) {
+            this.val = val;
+            this.index = index;
+        }
+    }
+
+    public List<Integer> countSmaller_mergeSort(int[] nums) {
+        int n = nums.length;
+        Integer[] result = new Integer[n];
+        List<Pair> pairs = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            pairs.add(new Pair(nums[i], i));
+            result[i] = 0;
+        }
+
+        mergeSortAndCount(pairs, result);
+
+        return Arrays.asList(result);
+    }
+
+    private List<Pair> mergeSortAndCount(List<Pair> pairs, Integer[] result) {
+        int n = pairs.size();
+        if (n <= 1) {
+            return pairs;
+        }
+
+        int mid = n / 2;
+        List<Pair> left = mergeSortAndCount(pairs.subList(0, mid), result);
+        List<Pair> right = mergeSortAndCount(pairs.subList(mid, n), result);
+
+        return merge(left, right, result);
+    }
+
+    private List<Pair> merge(List<Pair> left, List<Pair> right, Integer[] result) {
+        int leftSize = left.size();
+        int rightSize = right.size();
+        List<Pair> merged = new ArrayList<>();
+
+        int i = 0, j = 0;
+
+        while (i < leftSize && j < rightSize) {
+            if (left.get(i).val <= right.get(j).val) {
+                result[left.get(i).index] += j; // Increment count of smaller numbers on the right
+                merged.add(left.get(i));
+                i++;
+            } else {
+                merged.add(right.get(j));
+                j++;
+            }
+        }
+
+        while (i < leftSize) {
+            result[left.get(i).index] += j; // Increment count of smaller numbers on the right
+            merged.add(left.get(i));
+            i++;
+        }
+
+        while (j < rightSize) {
+            merged.add(right.get(j));
+            j++;
+        }
+
+        return merged;
+    }
+
 
     public static void main(String[] args) {
         CountSmallerNumbersAfterSelf solution = new CountSmallerNumbersAfterSelf();
         int[] nums = {5, 2, 6, 1};
-        List<Integer> result = solution.countSmaller(nums);
+        List<Integer> result = solution.countSmaller_mergeSort(nums);
         System.out.println(result); // Output: [2, 1, 1, 0]
     }
 
