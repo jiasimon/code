9b812b068b9cbb22172772b14e9816d021cbc228
Simon Jia <sjia@nisum.com>
2023-08-03 08:34:40 -0700

    Leetcode #322, CoinChange,  dp, from total to coin

--- src/main/java/com/sjia/Leetcode/CoinChange.java
+++ src/main/java/com/sjia/Leetcode/CoinChange.java
@@ -51,6 +51,30 @@ public class CoinChange {
 
 
 
+    // dp , from total to coin
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp, amount +1);
+
+        dp[0] =0;
+        for (int i=1; i<= amount; i++) {
+            for(int coin: coins) {
+                if( i >= coin){
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        if(dp[amount] > amount) return -1;
+        else return dp[amount];
+    }
+
+
+
+
+
+
+
+
     private int coinChange_memo(int[] coins, int remain, int[] memo) {
         if ( remain < 0) return -1;
         if ( remain == 0) return 0;
