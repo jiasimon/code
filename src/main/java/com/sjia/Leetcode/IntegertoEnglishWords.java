fffe70158baf4dee76444f0cd3ddd869f40f7a02
Simon Jia <sjia@nisum.com>
2023-08-04 17:02:02 -0700

    Leetcode #273, IntegertoEnglishWords,  if Billion, million

--- src/main/java/com/sjia/Leetcode/IntegertoEnglishWords.java
+++ src/main/java/com/sjia/Leetcode/IntegertoEnglishWords.java
@@ -17,6 +17,7 @@ public class IntegertoEnglishWords {
 
 
     // < 20, < 100, TENS, THOUSANDS
+    // 13 ms, 18.87%, 41.4 MB, 45.50%
     public String numberToWords(int num) {
         if (num == 0) return "Zero";
 
@@ -46,17 +47,50 @@ public class IntegertoEnglishWords {
 
 
 
+
+
+    // 10ms, 41.35%; 41.28mb, 61.39%
+    public String numberToWords2(int num) {
+        if (num == 0) return "Zero";
+
+        String words = "";
+        if ( num >= 1000000000) {
+            int p1 = num/1000000000 ;
+            num = num % 1000000000 ;
+            words += helper(p1) + "Billion ";
+        }
+
+        if (num >= 1000000) {
+            int p2 = num/1000000 ;
+            num = num % 1000000 ;
+            words += helper(p2) + "Million ";
+        }
+
+        if (num >= 1000) {
+            int p3 = num/1000 ;
+            num = num % 1000 ;
+            words += helper(p3) + "Thousand ";
+        }
+
+        words += helper(num);
+
+//        return words;
+        return words.trim();
+
+    }
+
     public static void main(String[] args) {
         IntegertoEnglishWords solution = new IntegertoEnglishWords();
 
         int num = 1234567;
-        String result = solution.numberToWords(num);
+        String result = solution.numberToWords2(num);
         System.out.println("The English representation of " + num + " is: " + result);
         // Output: The English representation of 1234567 is: One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven
 
         int num2 = 1234567892;
-        String result2 = solution.numberToWords(num2);
+        String result2 = solution.numberToWords2(num2);
         System.out.println("The English representation of " + num + " is: " + result2);
+        // One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Thousand Eight Hundred Ninety Two
     }
 
 
