67454d45b12988f4aae071bf23659245d19ff1a8
Simon Jia <sjia@nisum.com>
2023-08-06 08:47:02 -0700

    Leetcode #337,  HouseRobber3, max(root.val + sunzi, leftKid + rightKid)

---
+++ src/main/java/com/sjia/Leetcode/HouseRobber3.java
@@ -0,0 +1,62 @@
+package com.sjia.Leetcode;
+
+public class HouseRobber3 {
+    // #337. House Robber III   https://leetcode.com/problems/house-robber-iii/
+    /*
+    Besides the root, each house has one and only one parent house. After a tour, the smart thief realized that all houses in this place form a binary tree. It will automatically contact the police if two directly-linked houses were broken into on the same night.
+
+    Given the root of the binary tree, return the maximum amount of money the thief can rob without alerting the police.
+         3
+        / \
+       2   3
+        \   \
+         3   1
+
+         3
+        / \
+       4   5
+      / \   \
+     1   3   1
+
+     */
+
+
+
+    // naive
+    // TLE 122/124
+    public int rob_naive(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftKid = rob_naive(root.left) ;
+        int rightKid = rob_naive(root.right);
+
+        int sunzi =0;
+        if (root.left != null) {
+            sunzi += rob_naive(root.left.left) + rob_naive(root.left.right);
+        }
+        if (root.right != null) {
+            sunzi += rob_naive(root.right.left) + rob_naive(root.right.right);
+        }
+        return Math.max(root.val + sunzi, leftKid + rightKid);
+
+
+    }
+
+
+
+    public static void main(String[] args) {
+        HouseRobber3 solution = new HouseRobber3();
+
+        // Construct the tree (example: [3,2,3,null,3,null,1])
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.right = new TreeNode(3);
+        root.right.right = new TreeNode(1);
+
+        System.out.println(solution.rob(root)); // Output: 7 (Rob nodes 3, 3, and 1)
+    }
+
+}
