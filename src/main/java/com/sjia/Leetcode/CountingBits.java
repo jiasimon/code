0e63ed0320c19586d536bd20c4975e634bdf76ef
Simon Jia <sjia@nisum.com>
2023-08-07 08:47:09 -0700

    Leetcode #338,  CountingBits, recursive, top down

--- src/main/java/com/sjia/Leetcode/CountingBits.java
+++ src/main/java/com/sjia/Leetcode/CountingBits.java
@@ -22,10 +22,33 @@ public class CountingBits {
         return res;
     }
 
+
+    // recursive, top down
+    public int[] countBits_odd_even(int num) {
+        int[] res = new int[num+1];
+        for (int i=0; i <= num; i++) {
+            res[i] = getBitCount(i);
+        }
+        return res;
+    }
+
+
+    private int getBitCount(int i) {
+        if( i==0 ) return 0;
+        if ( i== 1) return 1;
+
+        if (i %2==0) return getBitCount(i/2);
+        else return 1 + getBitCount(i/2);
+    }
+
+
+
+
+
     public static void main(String[] args) {
         CountingBits solution = new CountingBits();
         int num = 5;
-        int[] result = solution.countBits(num);
+        int[] result = solution.countBits_odd_even(num);
         System.out.println(Arrays.toString(result)); // Output: [0, 1, 1, 2, 1, 2]
     }
 
