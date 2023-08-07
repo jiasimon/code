b906bd390e571b244d974b9ec64fdb17c28fb201
Simon Jia <sjia@nisum.com>
2023-08-07 07:55:43 -0700

    Leetcode #256, PaintHouse, dp, Math.min(cost[i - 1][1], cost[i - 1][2])

--- src/main/java/com/sjia/Leetcode/PaintHouse.java
+++ src/main/java/com/sjia/Leetcode/PaintHouse.java
@@ -1,11 +1,20 @@
 package com.sjia.Leetcode;
 
 public class PaintHouse {
-    // #256 https://leetcode.com/problems/paint-house/
-    // There are a row of n houses, each house can be painted with one of the three colors: red, blue or green.
-    // The cost of painting each house with a certain color is different.
-    // You have to paint all the houses such that no two adjacent houses have the same color.
+    // #256. Paint House https://leetcode.com/problems/paint-house/
 
+    /*
+     There are a row of n houses, each house can be painted with one of the three colors: red, blue or green.
+     The cost of painting each house with a certain color is different.
+     You have to paint all the houses such that no two adjacent houses have the same color.
+
+     Input: [[17,2,17],[16,16,5],[14,3,19]]     Output: 10
+     */
+
+
+
+
+    // DP
     public static int getMinimumPrice(int[][] cost) {
         int result = 0;
 
@@ -15,7 +24,9 @@ public class PaintHouse {
             cost[i][2] += Math.min(cost[i - 1][1], cost[i - 1][0]);
         }
 
-        result = Math.min(Math.min(cost[cost.length - 1][0], cost[cost.length - 1][1]), cost[cost.length - 1][2]);
+//        result = Math.min(Math.min(cost[cost.length - 1][0], cost[cost.length - 1][1]), cost[cost.length - 1][2]);
+        int tmp = Math.min(cost[cost.length - 1][0], cost[cost.length - 1][1] );
+        result = Math.min(tmp,  cost[cost.length - 1][2] );
         return result;
     }
 
