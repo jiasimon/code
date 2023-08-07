89db5223e968cf554f4df56c14a4f0ed67a0f7c0
Simon Jia <sjia@nisum.com>
2023-08-07 06:49:54 -0700

    Leetcode #253, MeetingRooms2,  LIS, left, right

--- src/main/java/com/sjia/Leetcode/MeetingRooms2.java
+++ src/main/java/com/sjia/Leetcode/MeetingRooms2.java
@@ -37,6 +37,25 @@ public class MeetingRooms2 {
     }
 
 
+    // endTimes.poll(); Reuse the room
+    public int minMeetingRooms2(int[][] intervals) {
+        if (intervals == null || intervals.length == 0) {
+            return 0;
+        }
+        // Sort the intervals based on the start times
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        PriorityQueue<Integer> endTimes = new PriorityQueue<>();
+        endTimes.offer(intervals[0][1]);
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] >= endTimes.peek()) {
+                endTimes.poll(); // Reuse the room
+            }
+            endTimes.offer(intervals[i][1]); // Allocate a new room
+        }
+        return endTimes.size();
+    }
+
+
 
     public static void main(String[] args) {
         int[][] testData  =  { {0, 30}, {5, 10}, {15, 20}  };
