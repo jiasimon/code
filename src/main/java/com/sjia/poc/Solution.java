003e75597ab99772f6ba62249875d9dbf16fdb92
Simon Jia <sjia@nisum.com>
2023-08-08 10:13:41 -0700

    Other ,  Triplet, arr[i] == arr[j] + arr[k]

--- src/main/java/com/sjia/poc/Solution.java
+++ src/main/java/com/sjia/poc/Solution.java
@@ -1,6 +1,8 @@
 package com.sjia.poc;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Solution {
 
@@ -25,12 +27,45 @@ public class Solution {
     }
 
 
+    static List<List<Integer>> findTriplet(int arr[])
+    {
+        int n = arr.length;
+        List<List<Integer>> res = new ArrayList<>();
+        // sort the array
+        Arrays.sort(arr);
+
+
+        // two pointer
+        for (int i = n - 1; i >= 0; i--) {
+            int j = 0;
+            int k = i - 1;
+            while (j < k) {
+                if (arr[i] == arr[j] + arr[k]) {
+                    res.add(Arrays.asList(arr[j], arr[k], arr[i]));
+                    break;
+                }
+                else if (arr[i] > arr[j] + arr[k])
+                    j++;
+                else
+                    k--;
+            }
+        }
+
+        return res;
+    }
+
+
 
 
     public static void main(String[] args) {
         int[] input = {1, 5, 3, 2};
         int result = getCount(input);
         System.out.println("The count is: " + result);
+
+        int[] input2 = { 5, 32, 1, 7, 10, 50, 19, 21, 2 };
+        List<List<Integer>> res2 = new ArrayList<>();
+        res2 = findTriplet(input2);
+        System.out.println( Arrays.toString(input2) + " The results are: " + res2);
 
     }
 
