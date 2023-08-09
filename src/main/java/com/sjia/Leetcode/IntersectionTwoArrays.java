0ce1316d7df457e40c0e734de72f26ecd0f622c5
Simon Jia <sjia@nisum.com>
2023-08-08 19:44:58 -0700

    Leetcode #349,  IntersectionTwoArrays,  2 HashSets, set1 and intersect

--- src/main/java/com/sjia/Leetcode/IntersectionTwoArrays.java
+++ src/main/java/com/sjia/Leetcode/IntersectionTwoArrays.java
@@ -1,39 +1,51 @@
 package com.sjia.Leetcode;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 
 public class IntersectionTwoArrays {
-    // #349 https://leetcode.com/problems/intersection-of-two-arrays/
+    // #349. Intersection of Two Arrays https://leetcode.com/problems/intersection-of-two-arrays/
 
-    // Input: nums1 = [1,2,2,1], nums2 = [2,2]
-    //Output: [2]
-    // Input: [4,9,5], [9,4,9,8,4], Output [4,9,0], Expected [9,4]
+    /*
+    Input: nums1 = [1,2,2,1], nums2 = [2,2]     Output: [2]
+    Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]     Output: [9,4]
+     */
 
-    // Runtime: 2 ms, faster than 99.33% of Java online submissions for Intersection of Two Arrays.
-    //Memory Usage: 40.1 MB, less than 5.08% of Java online submissions for Intersection of Two Arrays.
-/*
+
+
+
+    // 2 HashSets, set1 and intersect
     public int[] intersection(int[] nums1, int[] nums2) {
         HashSet<Integer> set1 = new HashSet<>();
-        for (Integer n: nums1) set1.add(n);
+        HashSet<Integer> intersect = new HashSet<>();
 
-        HashSet<Integer> set2 = new HashSet<>();
-        for(Integer n: nums2) set2.add(n);
-
-        int i =0;
-        int size = Math.min(set1.size(), set2.size());
-        int [] result = new int[size];
-        for (Integer k :  set1) {
-            if(set2.contains(k)) result[i++]=k;
+        for (Integer n: nums1) {
+            set1.add(n);
         }
-        return  Arrays.copyOf(result, i);
-    }*/
+
+        for ( int n: nums2) {
+            if (set1.contains(n) ){
+                intersect.add(n);
+            }
+        }
+
+        // Set to ArrayList, then to int[]
+        List<Integer> tmp = new ArrayList<>(intersect);
+        int[] res = new int[tmp.size()];
+        for ( int i=0; i < tmp.size(); i++) {
+            res[i] = tmp.get(i);
+        }
+        return res;
+
+    }
 
 
 
     // Runtime: 4 ms, faster than 43.97% of Java online submissions for Intersection of Two Arrays.
     //Memory Usage: 41.4 MB, less than 5.08% of Java online submissions for Intersection of Two Arrays.
-    public int[] intersection(int[] nums1, int[] nums2) {
+    public int[] intersection_retainAll(int[] nums1, int[] nums2) {
         HashSet<Integer> set1 = new HashSet<>();
         for (Integer n: nums1) set1.add(n);
 
