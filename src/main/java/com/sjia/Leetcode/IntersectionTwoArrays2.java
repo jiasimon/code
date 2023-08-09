c25942b2ee8c95c816bbf1160cbd249f0828ced8
Simon Jia <sjia@nisum.com>
2023-08-08 20:07:35 -0700

    Leetcode #350,  IntersectionTwoArrays2,  add tests

--- src/main/java/com/sjia/Leetcode/IntersectionTwoArrays2.java
+++ src/main/java/com/sjia/Leetcode/IntersectionTwoArrays2.java
@@ -5,8 +5,16 @@ import java.util.HashMap;
 
 public class IntersectionTwoArrays2 {
 
-    // #350 https://leetcode.com/problems/intersection-of-two-arrays-ii/
-    // Input: nums1 = [1,2,2,1], nums2 = [2,2], Output: [2,2]
+    // #350. Intersection of Two Arrays II https://leetcode.com/problems/intersection-of-two-arrays-ii/
+
+    /*
+    Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+    Input: nums1 = [1,2,2,1], nums2 = [2,2]     Output: [2,2]
+    Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4] Output: [4,9]
+
+     */
+
 
     // Runtime: 2 ms, faster than 98.86% of Java online submissions for Intersection of Two Arrays II.
     //Memory Usage: 39.9 MB, less than 9.07% of Java online submissions for Intersection of Two Arrays II.
@@ -31,6 +39,15 @@ public class IntersectionTwoArrays2 {
         }
         return Arrays.copyOf(result, i);
 
+    }
+
+
+    public static void main(String[] args) {
+        IntersectionTwoArrays2 solution = new IntersectionTwoArrays2();
+        int[] nums1 = {1, 2, 2, 1};
+        int[] nums2 = {2, 2};
+        int[] result = solution.intersect(nums1, nums2);
+        System.out.println(Arrays.toString(result)); // Output: [2, 2]
     }
 
 
