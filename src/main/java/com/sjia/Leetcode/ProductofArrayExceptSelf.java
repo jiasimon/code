031ff031b34b200520be376414e380bd65436e18
Simon Jia <sjia@nisum.com>
2023-08-09 18:22:33 -0700

    Leetcode #238,  ProductofArrayExceptSelf,  prefix product, like memo

--- src/main/java/com/sjia/Leetcode/ProductofArrayExceptSelf.java
+++ src/main/java/com/sjia/Leetcode/ProductofArrayExceptSelf.java
@@ -101,6 +101,31 @@ public class ProductofArrayExceptSelf {
     }
 
 
+
+    // prefix product, like memo,
+    public int[] productExceptSelfPre(int[] nums) {
+        int n = nums.length;
+        int pre[] = new int[n];
+        int post[] = new int[n];
+        pre[0] = 1;
+        post[n - 1] = 1;
+
+        for(int i = 1; i < n; i++) {
+            pre[i] = pre[i - 1] * nums[i - 1];
+        }
+        for(int i = n - 2; i >= 0; i--) {
+            post[i] = post[i + 1] * nums[i + 1];
+        }
+
+        int res[] = new int[n];
+        for(int i = 0; i < n; i++) {
+            res[i] = pre[i] * post[i];
+        }
+        return res;
+    }
+
+
+
     public static void main(String[] args) {
 //        int[] testData = {1,2,3,4,5};
         int[] testData = {-1,1,0,-3,3};
