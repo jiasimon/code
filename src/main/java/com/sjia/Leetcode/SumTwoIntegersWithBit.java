50eb442ea1b7546ecc000367c74dd2f34aa86de3
Simon Jia <sjia@nisum.com>
2023-08-10 06:58:53 -0700

    Leetcode #371,  SumTwoIntegersWithBit,  & carry then XOR

---
+++ src/main/java/com/sjia/Leetcode/SumTwoIntegersWithBit.java
@@ -0,0 +1,28 @@
+package com.sjia.Leetcode;
+
+public class SumTwoIntegersWithBit {
+    // #371. Sum of Two Integers    https://leetcode.com/problems/sum-of-two-integers/description/
+    // #bit
+
+    /*
+    Given two integers a and b, return the sum of the two integers without using the operators + and -.
+
+     */
+
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        SumTwoIntegersWithBit solution = new SumTwoIntegersWithBit();
+        int a = 5, b = 7;
+        int sum = solution.getSum(a, b);
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum); // Output: 12
+    }
+
+}
