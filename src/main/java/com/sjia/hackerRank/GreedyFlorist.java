eb2623f201d630a45e28b927e50e0cd56b2a1474
Simon Jia <sjia@nisum.com>
2023-08-10 15:58:04 -0700

    hackerRank #Greedy, GreedyFlorist, int[] cannot sort desc directly, convert to List

--- src/main/java/com/sjia/hackerRank/GreedyFlorist.java
+++ src/main/java/com/sjia/hackerRank/GreedyFlorist.java
@@ -2,8 +2,7 @@ package com.sjia.hackerRank;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class GreedyFlorist {
     // https://www.hackerrank.com/challenges/greedy-florist/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=greedy-algorithms
@@ -17,19 +16,47 @@ public class GreedyFlorist {
     // k :  number of people;
     static int getMinimumCost(int k, int[] c) {
         int res = 0;
-        int mutiple =1;
+        int multiple =1;
         Arrays.sort(c);
 
         for (int i = c.length-1; i >= 0; i--) {
-            res += c[i] * mutiple;
+            res += c[i] * multiple;
             if(  (c.length - i ) % k ==0 ) {
-                mutiple++;
+                multiple++;
             }
         }
         return res;
 
+    }
+
+
+    // sort int[] desc
+    static int getMinimumCost2(int k, int[] c) {
+        int res = 0;
+        int multiple =1;
+
+        // int[] cannot sort desc  directly
+//        Arrays.sort(c, (a,b) -> (b-a) );
+//        Collections.sort(c, Collections.reverseOrder() );
+
+        List<Integer> list = new ArrayList<>();
+        for ( int num : c) {
+            list.add(num);
+        }
+
+        Collections.sort(list, (a,b) -> (b-a));
+
+        for (int i = 0; i <= list.size()-1; i++) {
+            res += list.get(i) * multiple;
+            if(  i % k == k-1 ) {
+                multiple++;
+            }
+        }
+        return res;
 
     }
+
+
 
     private static final Scanner scanner = new Scanner(System.in);
 
