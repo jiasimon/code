0046c4a55821e1b3b77a38d801ebb2ad3585e388
Simon Jia <sjia@nisum.com>
2023-08-10 10:18:04 -0700

    Leetcode #372,  SuperPow,  recursive

---
+++ src/main/java/com/sjia/Leetcode/SuperPow.java
@@ -0,0 +1,60 @@
+package com.sjia.Leetcode;
+
+public class SuperPow {
+    // #372. Super Pow  https://leetcode.com/problems/super-pow/description/
+
+    /*
+    Your task is to calculate ab mod 1337 where a is a positive integer and b is an extremely large positive integer given in the form of an array.
+    Input: a = 2, b = [3]   Output: 8
+    Input: a = 2, b = [1,0] Output: 1024
+
+    1 <= a <= 2^31 - 1
+    1 <= b.length <= 2000
+    0 <= b[i] <= 9
+    b does not contain leading zeros.
+     */
+
+    private static final int MOD = 1337;
+
+    public int superPow(int a, int[] b) {
+        if (b.length == 0) {
+            return 1;
+        }
+
+        int lastDigit = b[b.length - 1];
+        int[] newB = new int[b.length - 1];
+        for (int i = 0; i < newB.length; i++) {
+            newB[i] = b[i];
+        }
+
+        int part1 = powMod(a, lastDigit);
+        int part2 = powMod(superPow(a, newB), 10);
+
+        return (part1 * part2) % MOD;
+    }
+
+    private int powMod(int base, int exponent) {
+        base %= MOD;
+        int result = 1;
+
+        for (int i = 0; i < exponent; i++) {
+            result = (result * base) % MOD;
+        }
+
+        return result;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        SuperPow solution = new SuperPow();
+        int a = 2;
+        int[] b = {1,2};
+        int result = solution.superPow(a, b);
+        System.out.println("Result: " + result); // Output: 85
+    }
+
+
+
+}
