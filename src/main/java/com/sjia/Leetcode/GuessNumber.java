2f46142e4048b180c737ddb00ea5522112b9511c
Simon Jia <sjia@nisum.com>
2023-08-11 11:52:43 -0700

    Leetcode #374, GuessNumber, while ( l < r)

--- src/main/java/com/sjia/Leetcode/GuessNumber.java
+++ src/main/java/com/sjia/Leetcode/GuessNumber.java
@@ -1,7 +1,7 @@
 package com.sjia.Leetcode;
 
 public class GuessNumber {
-    // #374 https://leetcode.com/problems/guess-number-higher-or-lower/
+    // #374. Guess Number Higher or Lower https://leetcode.com/problems/guess-number-higher-or-lower/
 
     // binary search
     // Runtime: 0 ms, faster than 100.00% of Java online submissions for Guess Number Higher or Lower.
@@ -34,6 +34,21 @@ public class GuessNumber {
         }
         return -1;
     }*/
+
+
+    public int guessNumber_lessEqual(int n) {
+        int l = 1;
+        int r = n;
+        while (l < r) {
+            final int m = l + (r - l) / 2;
+            if(guess(m) == 0) return m;
+            if (guess(m) < 0) // -1
+                r = m;
+            else
+                l = m + 1;
+        }
+        return l;
+    }
 
 
     private int guess(int mid) {
