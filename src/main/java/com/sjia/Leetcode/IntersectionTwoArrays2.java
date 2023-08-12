6773e99d88cbb65fcb79029fcab4672e71dd57f8
Simon Jia <sjia@nisum.com>
2023-08-11 18:40:05 -0700

    Leetcode #350, IntersectionTwoArrays2, arrays sorted, two pointer

--- src/main/java/com/sjia/Leetcode/IntersectionTwoArrays2.java
+++ src/main/java/com/sjia/Leetcode/IntersectionTwoArrays2.java
@@ -76,6 +76,31 @@ public class IntersectionTwoArrays2 {
     }
 
 
+    // if arrays sorted, two pointer
+    public int[] intersect_sorted(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int length1 = nums1.length, length2 = nums2.length;
+        int[] intersection = new int[Math.min(length1, length2)];
+        int index1 = 0, index2 = 0, index = 0;
+        while (index1 < length1 && index2 < length2) {
+            if (nums1[index1] < nums2[index2]) {
+                index1++;
+            } else if (nums1[index1] > nums2[index2]) {
+                index2++;
+            } else {
+                intersection[index] = nums1[index1];
+                index1++;
+                index2++;
+                index++;
+            }
+        }
+        return Arrays.copyOfRange(intersection, 0, index);
+    }
+
+
+
+
     public static void main(String[] args) {
         IntersectionTwoArrays2 solution = new IntersectionTwoArrays2();
         int[] nums1 = {1, 2, 2, 1};
