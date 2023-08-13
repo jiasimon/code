ff6b89ba926de2d107ca7ba6ce9ad749c4ca93d0
Simon Jia <sjia@nisum.com>
2023-08-13 10:42:32 -0700

    Leetcode #377, CombinationSum4,  memo , TLE 14/15

--- src/main/java/com/sjia/Leetcode/CombinationSum4.java
+++ src/main/java/com/sjia/Leetcode/CombinationSum4.java
@@ -48,7 +48,7 @@ public class CombinationSum4 {
     }
 
 
-    // recursive , Time Limit Exceeded
+    // recursive , Time Limit Exceeded, TLE
     public int combinationSum4_recursive(int[] nums, int target) {
         if (target == 0) return 1;
 
@@ -62,15 +62,39 @@ public class CombinationSum4 {
     }
 
 
+
+    // memo, TLE 14/15
+    public int combinationSum4_memo(int[] nums, int target) {
+        int n = nums.length;
+        int[] memo = new int[target+1];
+        memo[0] = 1;
+        return recursive_memo(nums, target, memo);
+    }
+
+    public int recursive_memo(int[] nums, int target, int[] memo) {
+        if (target == 0) return 1;
+        if (memo[target] != 0) return memo[target];
+
+        int res =0;
+        for (int i : nums) {
+            if (i <= target) {  // it should be <=
+                res += recursive_memo(nums,target -i, memo);
+            }
+        }
+        memo[target] = res;
+        return res;
+    }
+
+
     public static void main(String[] args) {
         CombinationSum4 cs = new CombinationSum4();
         int[] nums = {1, 2, 3};
         int target = 4;
-        int count = cs.combinationSum4_recursive(nums, target);
+        int count = cs.combinationSum4_memo(nums, target);
         System.out.println("Number of combinations for target " + target + ": " + count);  // output: 7
 
         target = 5;
-        int count2 = cs.combinationSum4_recursive(nums, target);
+        int count2 = cs.combinationSum4_memo(nums, target);
         System.out.println("Number of combinations for target " + target + ": " + count2); // output 13
 
 
