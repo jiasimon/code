9ad19109d7a742087e4f520f974847dc9e79bf49
Simon Jia <sjia@nisum.com>
2023-08-14 07:36:39 -0700

    Leetcode #245, ShortestWordDistance3,  revisit

--- src/main/java/com/sjia/Leetcode/ShortestWordDistance3.java
+++ src/main/java/com/sjia/Leetcode/ShortestWordDistance3.java
@@ -36,13 +36,33 @@ public class ShortestWordDistance3 {
 
 
 
+    public int shortestWordDistance2(String[] words, String word1, String word2) {
+        int index1 = -1;
+        int index2 = -1;
+        int res = Integer.MAX_VALUE;
+        boolean sameWords = word1.equals(word2);
+
+        for (int i =0; i < words.length; i++) {
+            if(words[i].equals(word1)) {
+                index1 = word1.equals(word2) ? index2 : i;
+            }
+            if (words[i].equals(word2)) {
+                index2 = i;
+            }
+            if (index1 != -1 && index2 != -1) {
+                res = Math.min(res, Math.abs(index2 - index1));
+            }
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         ShortestWordDistance3 shortestWordDistance3 = new ShortestWordDistance3();
 
         String[] words = {"practice", "makes", "perfect", "coding", "makes"};
-        System.out.println("Shortest distance: " + shortestWordDistance3.shortestWordDistance(words, "coding", "practice")); // Output: 3
-        System.out.println("Shortest distance: " + shortestWordDistance3.shortestWordDistance(words, "makes", "coding")); // Output: 1
-        System.out.println("Shortest distance: " + shortestWordDistance3.shortestWordDistance(words, "makes", "makes")); // Output: 3
+        System.out.println("Shortest distance: " + shortestWordDistance3.shortestWordDistance2(words, "coding", "practice")); // Output: 3
+        System.out.println("Shortest distance: " + shortestWordDistance3.shortestWordDistance2(words, "makes", "coding")); // Output: 1
+        System.out.println("Shortest distance: " + shortestWordDistance3.shortestWordDistance2(words, "makes", "makes")); // Output: 3
 
 
     }
