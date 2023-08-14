f514fb8a1320eec12f29a9fa4766c1e2dbd456c5
Simon Jia <sjia@nisum.com>
2023-08-14 08:17:57 -0700

    Leetcode #376, WiggleSubsequence,  two if

--- src/main/java/com/sjia/Leetcode/WiggleSubsequence.java
+++ src/main/java/com/sjia/Leetcode/WiggleSubsequence.java
@@ -72,6 +72,27 @@ public class WiggleSubsequence {
 
 
 
+    public int wiggleMaxLength3(int[] nums) {
+        int n = nums.length;
+        if (n <1) return 0;
+
+        int up = 1;
+        int down = 1;
+
+        for (int i=1; i < n; i++) {
+            if( nums[i] > nums[i-1]  ) {
+                up = down + 1;
+            }
+            if (nums[i] < nums[i-1] ) {
+                down = up + 1;
+            }
+
+        }
+        return Math.max(up, down);
+    }
+
+
+
     public static void main(String[] args) {
         WiggleSubsequence solution = new WiggleSubsequence();
         int[] nums = {1, 7, 4, 9, 2, 5};
