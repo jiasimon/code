5fa9524df615c419590010fbd1248f230450bc9b
Simon Jia <sjia@nisum.com>
2023-08-16 11:10:36 -0700

    Leetcode #382, LinkedListRandomNode, random.nextInt(list.size())

---
+++ src/main/java/com/sjia/Leetcode/LinkedListRandomNode.java
@@ -0,0 +1,45 @@
+package com.sjia.Leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class LinkedListRandomNode {
+    // #382. Linked List Random Node    https://leetcode.com/problems/linked-list-random-node/description/
+
+    /*
+    Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+
+    Implement the Solution class:
+
+    Solution(ListNode head) Initializes the object with the head of the singly-linked list head.
+    int getRandom() Chooses a node randomly from the list and returns its value. All the nodes of the list should be equally likely to be chosen.
+
+
+     */
+
+
+    List<Integer> list = new ArrayList<>();
+    Random random= new Random();
+
+    public LinkedListRandomNode(ListNode head) {
+        while ( head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+
+    }
+
+    // random.nextInt(list.size()),
+    // 10 ms, 98.88%; 44.6 MB, 22.42%;
+    public int getRandom() {
+        int index = random.nextInt(list.size());
+
+        return list.get(index);
+
+    }
+
+
+}
+
+
