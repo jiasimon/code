efe4175a056a2d87eef22cf1392a358250a9e30e
Simon Jia <sjia@nisum.com>
2023-08-18 22:07:48 -0700

    hackerRank #dp, Abbreviation, dp[m][n], 4 conditions

--- src/main/java/com/sjia/hackerRank/Abbreviation.java
+++ src/main/java/com/sjia/hackerRank/Abbreviation.java
@@ -64,17 +64,45 @@ public class Abbreviation {
 
 
 
+    static String abbreviation(String a, String b) {
+        boolean[][] isValid = new boolean[a.length()+1][b.length()+1];
+        isValid[0][0] = true;
+
+        for (int i= 1; i <= a.length(); i++) {
+            if (Character.isUpperCase(a.charAt(i - 1))) {
+                isValid[i][0] = false;
+            }
+            else isValid[i][0] = true;
+        }
+        // tabulation from start of string
+        for (int i = 1; i <= a.length(); i++) {
+            for (int j = 1; j <= b.length(); j++) {
+                if (a.charAt(i-1) == b.charAt(j-1)) {
+                    isValid[i][j] = isValid[i-1][j-1];
+                }else if (Character.toUpperCase(a.charAt(i-1))  ==  b.charAt(j-1)) {
+                    isValid[i][j] = isValid[i-1][j-1] || isValid[i-1][j];
+                }else if (Character.isUpperCase(a.charAt(i-1))) {
+                    isValid[i][j] = false;
+                }else {
+                    isValid[i][j] = isValid[i-1][j];
+                }
+            }
+        }
+        return isValid[a.length()][b.length()]? "YES" : "NO";
+
+
+    }
 
 
 
     public static void main(String[] args) {
         String a ="AbcBDE";
         String b ="ABDE";
-        System.out.println(abbreviation_dp_failed(a,b));
+        System.out.println(abbreviation(a,b));
 
         a ="KXzQ";
         b ="K";
-        System.out.println(abbreviation_dp_failed(a,b));
+        System.out.println(abbreviation(a,b));
 
 
 
