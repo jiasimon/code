e0ce4ba9f55a46de574e58eadcb9d2f39ef56695
Simon Jia <sjia@nisum.com>
2023-08-20 12:27:41 -0700

    Leetcode #384, ShuffleArray, Knuth shuffle, nums.clone()

--- src/main/java/com/sjia/Leetcode/ShuffleArray.java
+++ src/main/java/com/sjia/Leetcode/ShuffleArray.java
@@ -106,3 +106,37 @@ class ShuffleArray2 {
 }
 
 */
+
+
+/*
+class Solution {
+        private int[] original;
+        private Random rand;
+
+    public Solution(int[] nums) {
+        original = nums.clone();
+        rand = new Random();
+
+    }
+
+    public int[] reset() {
+        return original;
+    }
+
+    public int[] shuffle() {
+        int[] shuffled = original.clone();
+        for ( int i = shuffled.length -1 ; i >=0 ; i--) {
+            int j = rand.nextInt(i + 1) ;
+            swap( shuffled, i, j);
+        }
+        return shuffled;
+
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+ */
