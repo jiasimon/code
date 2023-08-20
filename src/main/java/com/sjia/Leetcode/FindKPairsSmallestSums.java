dc0dcea55d10f2b29c07db1f95a66f560cef4cee
Simon Jia <sjia@nisum.com>
2023-08-20 10:48:13 -0700

    Leetcode #373, FindKPairsSmallestSums, PriorityQueue<int[]> minHeap, mergeKSortedList

--- src/main/java/com/sjia/Leetcode/FindKPairsSmallestSums.java
+++ src/main/java/com/sjia/Leetcode/FindKPairsSmallestSums.java
@@ -77,12 +77,43 @@ public class FindKPairsSmallestSums {
     }
 
 
+
+    // PriorityQueue<int[]> minHeap, mergeKSortedList
+    // 28 ms, 98.48%; 56.4 MB, 87.57%
+    public List<List<Integer>> kSmallestPairs_mergeKSortedList(int[] nums1, int[] nums2, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (nums1.length == 0 || nums2.length == 0 || k == 0) {
+            return result;
+        }
+
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] + a[1] - b[0] - b[1]);
+
+        for (int i = 0; i < Math.min(nums1.length, k); i++) {
+            minHeap.offer(new int[]{nums1[i], nums2[0], 0});
+        }
+
+        while (k-- > 0 && !minHeap.isEmpty()) {
+            int[] curr = minHeap.poll();
+            int i = curr[2]; // index in nums2
+
+            result.add(Arrays.asList(curr[0], curr[1]));
+
+            if (i + 1 < nums2.length) {
+                minHeap.offer(new int[]{curr[0], nums2[i + 1], i + 1});
+            }
+        }
+
+        return result;
+    }
+
+
     public static void main(String[] args) {
         FindKPairsSmallestSums solution = new FindKPairsSmallestSums();
         int[] nums1 = {1, 7, 11};
         int[] nums2 = {2, 4, 6};
         int k = 3;
-        List<List<Integer>> result = solution.kSmallestPairs_brute(nums1, nums2, k);
+        List<List<Integer>> result = solution.kSmallestPairs_mergeKSortedList(nums1, nums2, k);
         for (List<Integer> pair : result) {
             System.out.println(pair);
         }
