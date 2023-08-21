c7460cd7b8a3b77621b371c8d951e35cd2bcd019
Simon Jia <sjia@nisum.com>
2023-08-21 06:53:55 -0700

    Leetcode #386, LexicographicalNumbers, for loop, current % 10 != 9

--- src/main/java/com/sjia/Leetcode/LexicographicalNumbers.java
+++ src/main/java/com/sjia/Leetcode/LexicographicalNumbers.java
@@ -68,6 +68,31 @@ public class LexicographicalNumbers {
 
 
 
+    // for loop,
+    public List<Integer> lexicalOrder_iterative(int n) {
+        List<Integer> result = new ArrayList<>();
+        int current = 1;
+
+        for (int i = 1; i <= n; i++) {
+            result.add(current);
+
+            if (current * 10 <= n) {
+                current *= 10;
+            } else if (current % 10 != 9 && current + 1 <= n) {
+                current++;
+            } else {
+                while ((current / 10) % 10 == 9) {
+                    current /= 10;
+                }
+                current = current / 10 + 1;
+            }
+        }
+
+        return result;
+    }
+
+
+
     public static void main(String[] args) {
         LexicographicalNumbers solution = new LexicographicalNumbers();
         int n = 13;  // n = 21
