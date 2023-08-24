52cda84afdb2cbd59d6b92b510950edeb6e77ca4
Simon Jia <sjia@nisum.com>
2023-08-24 07:52:30 -0700

    Leetcode #398, RandomPickIndex, hashmap, Reservoir sampling

--- src/main/java/com/sjia/Leetcode/RandomPickIndex.java
+++ src/main/java/com/sjia/Leetcode/RandomPickIndex.java
@@ -78,3 +78,44 @@ public class RandomPickIndex {
 
 
 }
+
+
+/*
+
+
+// Reservoir sampling
+// 1427 ms, 20.34%; 56.1 MB, 12.94%
+class Solution {
+
+    private int[] nums;
+    private Random rand;
+
+    public Solution(int[] nums) {
+        this.nums = nums;
+        this.rand = new Random();
+
+    }
+
+    public int pick(int target) {
+        int n = this.nums.length;
+        int count = 0;
+        int idx = 0;
+        for (int i = 0; i < n; ++i) {
+            // if nums[i] is equal to target, i is a potential candidate
+            // which needs to be chosen uniformly at random
+            if (this.nums[i] == target) {
+                // increment the count of total candidates
+                // available to be chosen uniformly at random
+                count++;
+                // we pick the current number with probability 1 / count (reservoir sampling)
+                if (rand.nextInt(count) == 0) {
+                    idx = i;
+                }
+            }
+        }
+        return idx;
+
+
+    }
+}
+ */
