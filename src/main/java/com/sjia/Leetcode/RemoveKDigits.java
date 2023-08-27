63584dae14092deff44db1065935feb8b1ea24ac
Simon Jia <sjia@nisum.com>
2023-08-27 08:41:59 -0700

    Leetcode #402, RemoveKDigits,   sb reverse instead of insert, faster

--- src/main/java/com/sjia/Leetcode/RemoveKDigits.java
+++ src/main/java/com/sjia/Leetcode/RemoveKDigits.java
@@ -59,6 +59,47 @@ public class RemoveKDigits {
     }
 
 
+
+
+    // sb reverse
+    public String removeKdigits_reverseSB(String num, int k) {
+        int len = num.length();
+        //corner case
+        if(k==len)
+            return "0";
+
+        Stack<Character> stack = new Stack<>();
+        int i =0;
+        while(i<num.length()){
+            //whenever meet a digit which is less than the previous digit, discard the previous one
+            while(k>0 && !stack.isEmpty() && stack.peek()>num.charAt(i)){
+                stack.pop();
+                k--;
+            }
+            stack.push(num.charAt(i));
+            i++;
+        }
+
+        // corner case like "1111"
+        while(k>0){
+            stack.pop();
+            k--;
+        }
+
+        //construct the number from the stack
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty())
+            sb.append(stack.pop());
+        sb.reverse();
+
+        //remove all the 0 at the head
+        while(sb.length()>1 && sb.charAt(0)=='0')
+            sb.deleteCharAt(0);
+        return sb.toString();
+
+    }
+
+
     public static void main(String[] args) {
         RemoveKDigits solution = new RemoveKDigits();
         String num = "1432219";
