b1ffb5799bc1d36d8dc08ff3fc568dc1374bc45f
Simon Jia <sjia@nisum.com>
2023-08-27 11:29:03 -0700

    Leetcode #403, FrogJump, DP,  jump to stones[i] with jump j

--- src/main/java/com/sjia/Leetcode/FrogJump.java
+++ src/main/java/com/sjia/Leetcode/FrogJump.java
@@ -1,9 +1,6 @@
 package com.sjia.Leetcode;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class FrogJump {
     // #403. Frog Jump  https://leetcode.com/problems/frog-jump/description/
@@ -18,6 +15,10 @@ public class FrogJump {
 
     Input: stones = [0,1,2,3,4,8,9,11]      Output: false
     Explanation: There is no way to jump to the last stone as the gap between the 5th and 6th stone is too large.
+
+    2 <= stones.length <= 2000
+    0 <= stones[i] <= 2^31 - 1
+    stones[0] == 0
 
      */
 
@@ -52,10 +53,42 @@ public class FrogJump {
     }
 
 
+
+    // DP,  jump to stones[i] with jump j
+    // 87 ms, 24.89%; 63 MB, 15.31%
+    public boolean canCross_dp(int[] stones) {
+        final int n = stones.length;
+        // dp[i][j] := 1 if a frog can make a size j jump to stones[i]
+        int[][] dp = new int[n][n + 1];
+        dp[0][0] = 1;
+
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                final int k = stones[i] - stones[j];
+                if (k > n)
+                    continue;
+                for (final int x : new int[] {k - 1, k, k + 1})
+                    if (0 <= x && x <= n)
+                        dp[i][k] |= dp[j][x];
+            }
+        }
+
+        for (int i=0; i <= n; i++) {
+            if (dp[n-1][i] == 1) {
+                return true;
+            }
+        }
+        return false;
+
+
+//        return Arrays.stream(dp[n - 1]).anyMatch(a -> a == 1);
+    }
+
+
     public static void main(String[] args) {
         FrogJump solution = new FrogJump();
         int[] stones = {0, 1, 3, 5, 6, 8, 12, 17};
-        boolean canCross = solution.canCross(stones);
+        boolean canCross = solution.canCross_dp(stones);
 
         if (canCross) {
             System.out.println("The frog can cross the river.");
