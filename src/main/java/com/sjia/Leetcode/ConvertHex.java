59cd4158f9eb5e63a2b870499e08675c5bda262d
Simon Jia <sjia@nisum.com>
2023-08-28 08:51:14 -0700

    Leetcode #405, ConvertHex, char[] and StringBuilder, num >>>= 4

--- src/main/java/com/sjia/Leetcode/ConvertHex.java
+++ src/main/java/com/sjia/Leetcode/ConvertHex.java
@@ -51,6 +51,23 @@ public class ConvertHex {
     }
 
 
+    // char[] and StringBuilder, num >>>= 4
+    // 0 ms, 100%; 39.3 MB, 94.98%
+    public String toHex_charArray(int num) {
+        if (num ==0) return "0";
+
+        final char[] hex = {'0', '1', '2', '3', '4', '5', '6', '7',
+                '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+        StringBuilder sb = new StringBuilder();
+
+        while (num != 0) {
+            sb.append(hex[num & 0xf]);
+            num >>>= 4;
+        }
+
+        return sb.reverse().toString();
+    }
+
 
 
 
