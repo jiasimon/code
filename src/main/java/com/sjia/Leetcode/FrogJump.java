a757090711fdb5604222eca05e7beb3a4267c7c7
Simon Jia <sjia@nisum.com>
2023-08-28 06:41:24 -0700

    Leetcode #403, FrogJump, Arrays.stream(dp[n - 1]).anyMatch(a -> a == 1)

--- src/main/java/com/sjia/Leetcode/FrogJump.java
+++ src/main/java/com/sjia/Leetcode/FrogJump.java
@@ -80,8 +80,31 @@ public class FrogJump {
         }
         return false;
 
+    }
 
-//        return Arrays.stream(dp[n - 1]).anyMatch(a -> a == 1);
+
+
+
+    // anyMatch(a -> a == 1)
+    // 95 ms, 21.96%; 62.8 MB, 15.64%
+    public boolean canCross_stream(int[] stones) {
+        final int n = stones.length;
+        // dp[i][j] := 1 if a frog can make a size j jump to stones[i]
+        int[][] dp = new int[n][n + 1];
+        dp[0][0] = 1;
+
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                final int k = stones[i] - stones[j];
+                if (k > n)
+                    continue;
+                for (final int x : new int[] {k - 1, k, k + 1})
+                    if (0 <= x && x <= n)
+                        dp[i][k] |= dp[j][x];
+            }
+        }
+
+        return Arrays.stream(dp[n - 1]).anyMatch(a -> a == 1);
     }
 
 
