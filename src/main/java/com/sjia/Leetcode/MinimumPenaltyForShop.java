6246d868ba7c5fb27d02f57f84d516bbd7375857
Simon Jia <sjia@nisum.com>
2023-08-28 19:13:08 -0700

    Leetcode #2483, MinimumPenaltyForShop, two pass

---
+++ src/main/java/com/sjia/Leetcode/MinimumPenaltyForShop.java
@@ -0,0 +1,73 @@
+package com.sjia.Leetcode;
+
+public class MinimumPenaltyForShop {
+    // #2483. Minimum Penalty for a Shop    https://leetcode.com/problems/minimum-penalty-for-a-shop/
+
+    /*
+    You are given the customer visit log of a shop represented by a 0-indexed string customers consisting only of characters 'N' and 'Y':
+
+    if the ith character is 'Y', it means that customers come at the ith hour
+    whereas 'N' indicates that no customers come at the ith hour.
+    If the shop closes at the jth hour (0 <= j <= n), the penalty is calculated as follows:
+
+    For every hour when the shop is open and no customers come, the penalty increases by 1.
+    For every hour when the shop is closed and customers come, the penalty increases by 1.
+    Return the earliest hour at which the shop must be closed to incur a minimum penalty.
+
+    Note that if a shop closes at the jth hour, it means the shop is closed at the hour j.
+
+    Input: customers = "YYNY"   Output: 2
+    Explanation:
+    - Closing the shop at the 0th hour incurs in 1+1+0+1 = 3 penalty.
+    - Closing the shop at the 1st hour incurs in 0+1+0+1 = 2 penalty.
+    - Closing the shop at the 2nd hour incurs in 0+0+0+1 = 1 penalty.
+    - Closing the shop at the 3rd hour incurs in 0+0+1+1 = 2 penalty.
+    - Closing the shop at the 4th hour incurs in 0+0+1+0 = 1 penalty.
+    Closing the shop at 2nd or 4th hour gives a minimum penalty. Since 2 is earlier, the optimal closing time is 2.
+
+
+     */
+
+    // two pass
+    // 11 ms, 76.43%; 44.2 MB, 58.59%
+    public int bestClosingTime(String customers) {
+        int n = customers.length();
+        int curPenalty = 0;
+
+        for (char c : customers.toCharArray()) {
+            if ( c =='Y') {
+                curPenalty++;
+            }
+        }
+
+        int minPenalty = curPenalty;
+        int earliest =0;
+        for (int closeHour = 0; closeHour < n; closeHour++) {
+            char ch = customers.charAt(closeHour);
+            if (ch == 'Y') {
+                curPenalty--;
+            } else  {
+                curPenalty++;
+            }
+
+            if ( curPenalty < minPenalty) {
+                earliest = closeHour + 1;
+                minPenalty = curPenalty;
+            }
+
+        }
+
+        return earliest;
+    }
+
+    public static void main(String[] args) {
+        MinimumPenaltyForShop solution = new MinimumPenaltyForShop();
+        String customers = "YYYYNYYY";
+        int earliestClosingHour = solution.bestClosingTime(customers);
+        System.out.println("Earliest closing hour for minimum penalty: " + earliestClosingHour);
+    }
+
+
+
+
+}
