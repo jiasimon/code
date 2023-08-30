6117a6366e686f88e990290d02ffe26a0b46488b
Simon Jia <sjia@nisum.com>
2023-08-30 07:24:48 -0700

    Leetcode #405, ConvertHex, String[], much faster than char[]

--- src/main/java/com/sjia/Leetcode/ConvertHex.java
+++ src/main/java/com/sjia/Leetcode/ConvertHex.java
@@ -88,6 +88,24 @@ public class ConvertHex {
     }
 
 
+    // String[], faster than char[]
+    // 1 ms, 28.17%; 40 MB, 45.14%
+    public String toHex_StringArray(int num) {
+        if (num ==0) return "0";
+        final char[] hex = {'0', '1', '2', '3', '4', '5', '6', '7',
+                '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+        String res = "";
+
+        while (num != 0) {
+            res = hex[num & 0xf] + res;
+            num = (num >>> 4);  // num >>>= 4;
+        }
+
+        return res;
+
+    }
+
+
 
     public static void main(String[] args) {
         int testData = 10;  //-1 ,32
