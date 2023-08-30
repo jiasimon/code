3dab2755f5f32d6f6d50b402f0782f8197965a60
Simon Jia <sjia@nisum.com>
2023-08-29 18:57:54 -0700

    Leetcode #405, ConvertHex, char[] and String, num >>>= 4, slow

--- src/main/java/com/sjia/Leetcode/ConvertHex.java
+++ src/main/java/com/sjia/Leetcode/ConvertHex.java
@@ -70,6 +70,24 @@ public class ConvertHex {
 
 
 
+    // char[] and String, num >>>= 4
+    // 7 ms, 19.77%, 40.7 MB, 7.52%
+    public String toHex_String(int num) {
+        if (num ==0) return "0";
+        final char[] hex = {'0', '1', '2', '3', '4', '5', '6', '7',
+                '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+        String res = "";
+
+        while (num != 0) {
+            res = hex[num & 0xf] + res;
+            num = (num >>> 4);  // num >>>= 4;
+        }
+
+        return res;
+
+    }
+
+
 
     public static void main(String[] args) {
         int testData = 10;  //-1 ,32
