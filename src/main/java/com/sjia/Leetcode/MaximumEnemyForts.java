76bd5be4b87935bb3286a240fe8d47f9eee54c95
Simon Jia <sjia@nisum.com>
2023-09-04 07:12:45 -0700

    Leetcode #2511, MaximumEnemyForts2511, two pass, add break

--- src/main/java/com/sjia/Leetcode/MaximumEnemyForts.java
+++ src/main/java/com/sjia/Leetcode/MaximumEnemyForts.java
@@ -20,6 +20,7 @@ public class MaximumEnemyForts {
 
 
 
+    // one pass, pre, i - pre - 1
     public int captureForts(int[] forts) {
         int n = forts.length;
         int res = 0, pre = -1;
@@ -36,12 +37,54 @@ public class MaximumEnemyForts {
 
 
 
+    // two pass
+    public int captureForts_twoPass(int[] forts) {
+        int n = forts.length;
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            if (forts[i] == 1 ) {
+                for ( int j = i+1; j < n; j++) {
+                    if (forts[j] == 1) {
+                        break;
+                    } else if (forts[j] == -1) {
+                        res = Math.max(res, j - i - 1);
+                        break;
+                    }
+                }
+
+            }
+        }
+
+        for (int i = n-1; i >=0; i--) {
+            if (forts[i] == 1 ) {
+                for ( int j = i-1; j >=0; j--) {
+                    if (forts[j] == 1) {
+                        break;
+                    } else if (forts[j] == -1) {
+                        res = Math.max(res, i-j - 1);
+                        break;
+                    }
+                }
+
+            }
+        }
+
+        return res;
+    }
+
+
+
 
     public static void main(String[] args) {
         MaximumEnemyForts solution = new MaximumEnemyForts();
         int[] forts = {1,0,0,-1,0,0,0,0,1};
-        int maxCaptured = solution.captureForts(forts);
+        int maxCaptured = solution.captureForts_twoPass(forts);
         System.out.println("Maximum enemy forts that can be captured: " + maxCaptured);
+
+        int[] forts2 = {1,0,0,-1,0,0,-1,0,0,1};
+        int maxCaptured2 = solution.captureForts_twoPass(forts2);
+        System.out.println("Maximum enemy forts that can be captured: " + maxCaptured2);
+
     }
 
 }
