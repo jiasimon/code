6ab31442b49b093b698c1252b00e3d9419c6d0ef
Simon Jia <sjia@nisum.com>
2023-09-07 18:58:14 -0700

    Leetcode #127, WordLadder,  bfs, set

--- src/main/java/com/sjia/Leetcode/WordLadder.java
+++ src/main/java/com/sjia/Leetcode/WordLadder.java
@@ -53,10 +53,45 @@ public class WordLadder {
                             wordSet.remove(newWord);
                         }
                     }
-                    wordChars[j] = originalChar;
+                    wordChars[j] = originalChar; // revert the change
                 }
             }
             ladderLength++;
+        }
+
+        return 0;
+    }
+
+
+    // bfs, set
+    // 68 ms, 76.66%; 44.5 MB, 86.44%
+    public int ladderLength_bfs(String beginWord, String endWord, List<String> wordList) {
+        Set<String> wordSet = new HashSet<>(wordList);
+        if (!wordSet.contains(endWord))
+            return 0;
+
+        int ans = 0;
+        Queue<String> q = new ArrayDeque<>(Arrays.asList(beginWord));
+
+        while (!q.isEmpty()) {
+            ++ans;
+            for (int sz = q.size(); sz > 0; --sz) {
+                StringBuilder sb = new StringBuilder(q.poll());
+                for (int i = 0; i < sb.length(); ++i) {
+                    final char cache = sb.charAt(i);
+                    for (char c = 'a'; c <= 'z'; ++c) {
+                        sb.setCharAt(i, c);
+                        final String word = sb.toString();
+                        if (word.equals(endWord))
+                            return ans + 1;
+                        if (wordSet.contains(word)) {
+                            q.offer(word);
+                            wordSet.remove(word);
+                        }
+                    }
+                    sb.setCharAt(i, cache);
+                }
+            }
         }
 
         return 0;
@@ -69,7 +104,7 @@ public class WordLadder {
         String beginWord = "hit";
         String endWord = "cog";
         List<String> wordList = Arrays.asList("hot", "dot", "dog", "lot", "log", "cog");
-        int result = solution.ladderLength(beginWord, endWord, wordList);
+        int result = solution.ladderLength_bfs(beginWord, endWord, wordList);
         System.out.println("Ladder Length: " + result);  // Output: 5
     }
 
