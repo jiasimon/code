22f0a91a4a1a4b1e939ce9592edbc4490657bf1e
Simon Jia <sjia@nisum.com>
2023-09-17 09:58:08 -0700

    Leetcode #447, NumberBoomerangs,   n*(n-1) , distanceFreq map, map.clear

--- src/main/java/com/sjia/Leetcode/NumberBoomerangs.java
+++ src/main/java/com/sjia/Leetcode/NumberBoomerangs.java
@@ -5,10 +5,24 @@ import java.util.Map;
 
 public class NumberBoomerangs {
 
-    // #447 https://leetcode.com/problems/number-of-boomerangs/
+    // #447. Number of Boomerangs https://leetcode.com/problems/number-of-boomerangs/
 
-    // Runtime: 95 ms, faster than 77.30% of Java online submissions for Number of Boomerangs.
-    //Memory Usage: 39.7 MB, less than 26.32% of Java online submissions for Number of Boomerangs.
+    /*
+    You are given n points in the plane that are all distinct, where points[i] = [xi, yi]. A boomerang is a tuple of points (i, j, k) such that the distance between i and j equals the distance between i and k (the order of the tuple matters).
+
+    Return the number of boomerangs.
+    Input: points = [[0,0],[1,0],[2,0]]     Output: 2
+    Input: points = [[1,1],[2,2],[3,3]]     Output: 2
+    Input: points = [[1,1]]                 Output: 0
+
+    1 <= n <= 500
+    -10000 <= xi, yi <= 10000
+     */
+
+
+
+    // n*(n-1) , distanceFreq map, map.clear
+    // 98 ms, 66.67%, 43.5 MB,76.6%
     public int numberOfBoomerangs(int[][] points) {
         int result =0;
         Map<Integer, Integer> map = new HashMap<>();
@@ -33,6 +47,15 @@ public class NumberBoomerangs {
         int a = x[0] - y[0];
         int b = x[1] - y[1];
         return a*a + b*b;
+    }
+
+
+    public static void main(String[] args) {
+        NumberBoomerangs solution = new NumberBoomerangs();
+        int[][] points = {{0, 0}, {1, 0}, {2, 0}};
+
+        int boomerangs = solution.numberOfBoomerangs(points);
+        System.out.println("Number of Boomerangs: " + boomerangs); // Output: 2
     }
 
 
