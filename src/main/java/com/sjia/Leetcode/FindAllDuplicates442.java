4712d6e76a0386788cbc274c477afac2b34fefce
Simon Jia <sjia@nisum.com>
2023-09-22 06:15:49 -0700

    Leetcode #442, FindAllDuplicates442, swap nums[i]  to value -1 position

--- src/main/java/com/sjia/Leetcode/FindAllDuplicates442.java
+++ src/main/java/com/sjia/Leetcode/FindAllDuplicates442.java
@@ -75,6 +75,31 @@ public class FindAllDuplicates442 {
     }
 
 
+
+    // swap nums[i]  to value -1 position
+    public List<Integer> findDuplicates_swap(int[] nums) {
+
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            while (nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        List<Integer> ans = new ArrayList<Integer>();
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] - 1 != i) {
+                ans.add(nums[i]);
+            }
+        }
+        return ans;
+    }
+
+    public void swap(int[] nums, int index1, int index2) {
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
+    }
+
     public static void main(String[] args) {
         FindAllDuplicates442 solution = new FindAllDuplicates442();
         int[] nums = {4, 3, 2, 7, 8, 2, 1};
