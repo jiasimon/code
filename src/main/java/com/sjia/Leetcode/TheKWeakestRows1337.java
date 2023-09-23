5a04d6b4b8118cd5278a202785efd801654d139b
Simon Jia <sjia@nisum.com>
2023-09-23 08:02:13 -0700

    Leetcode #1337, TheKWeakestRows1337, *100+i

--- src/main/java/com/sjia/Leetcode/TheKWeakestRows1337.java
+++ src/main/java/com/sjia/Leetcode/TheKWeakestRows1337.java
@@ -74,6 +74,37 @@ public class TheKWeakestRows1337 {
         }
     }
 
+
+
+
+    // sum *100 +row, sort then mod 100
+    // 1 ms, 99.31%; 44.6 MB, 42.52%
+    public int[] kWeakestRows_Multi100(int[][] mat, int k) {
+        int[] list = new int[mat.length];
+        int[] result = new int[k];
+        for(int i=0;i<mat.length;i++){
+            list[i] = count(mat[i])*100+i;
+        }
+        Arrays.sort(list);
+        for(int i=0;i<k;i++){
+            result[i] = list[i]%100;
+        }
+        return result;
+    }
+    public int count(int[] nums){
+        int sum=0;
+        for(int n:nums){
+            if(n!=1){
+                break;
+            }
+            sum+=n;
+        }
+        return sum;
+
+
+    }
+
+
     public static void main(String[] args) {
         TheKWeakestRows1337 solution = new TheKWeakestRows1337();
         int[][] mat = {
