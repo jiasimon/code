6be60d3baa60dbcfe4aa2880e17f501bdb18d619
Simon Jia <sjia@nisum.com>
2023-09-25 19:35:06 -0700

    Leetcode #415, AddStrings415, string concat, slow

--- src/main/java/com/sjia/Leetcode/AddStrings.java
+++ src/main/java/com/sjia/Leetcode/AddStrings.java
@@ -114,6 +114,29 @@ public class AddStrings {
 
 
 
+    // String concat, slow
+    // 10 ms, 31.12%; 44 MB, 17.21%
+    public String addStrings_string_concat(String num1, String num2) {
+        String res = "";
+        int carry = 0;
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int digit1 = i >= 0 ? num1.charAt(i) - '0' : 0;
+            int digit2 = j >= 0 ? num2.charAt(j) - '0' : 0;
+
+            int sum = digit1 + digit2 + carry;
+            carry = sum / 10;
+            int currentDigit = sum % 10;
+
+            res = currentDigit + res;
+            i--;
+            j--;
+        }
+
+        return res;
+    }
 
 
     public static void main(String[] args) {
