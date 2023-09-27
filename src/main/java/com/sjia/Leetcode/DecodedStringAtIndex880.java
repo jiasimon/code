0f11cc9c9b9efcc437465d3e115694d09f28b5d4
Simon Jia <sjia@nisum.com>
2023-09-27 06:44:00 -0700

    Leetcode #880, DecodedStringAtIndex880,  k %= size, size /= digit

---
+++ src/main/java/com/sjia/Leetcode/DecodedStringAtIndex880.java
@@ -0,0 +1,56 @@
+package com.sjia.Leetcode;
+
+public class DecodedStringAtIndex880 {
+    // #880. Decoded String at Index    https://leetcode.com/problems/decoded-string-at-index/description/
+    /*
+    You are given an encoded string s. To decode the string to a tape, the encoded string is read one character at a time and the following steps are taken:
+
+    If the character read is a letter, that letter is written onto the tape.
+    If the character read is a digit d, the entire current tape is repeatedly written d - 1 more times in total.
+    Given an integer k, return the kth letter (1-indexed) in the decoded string.
+
+    Input: s = "leet2code3", k = 10     Output: "o"
+    Explanation: The decoded string is "leetleetcodeleetleetcodeleetleetcode".
+    The 10th letter in the string is "o".
+
+    Input: s = "ha22", k = 5            Output: "h"
+    Explanation: The decoded string is "hahahaha". The 5th letter is "h".
+
+    Input: s = "a2345678999999999999999", k = 1     Output: "a"
+    Explanation: The decoded string is "a" repeated 8301530446056247680 times.
+
+     */
+
+    public String decodeAtIndex(String s, int k) {
+        long size = 0; // Length of decoded `s`
+        for (final char c : s.toCharArray())
+            if (Character.isDigit(c))
+                size *= c - '0';
+            else
+                ++size;
+        for (int i = s.length() - 1; i >= 0; --i) {
+            k %= size;
+            if (k == 0 && Character.isAlphabetic(s.charAt(i)))
+                return s.substring(i, i + 1);
+            if (Character.isDigit(s.charAt(i)))
+                size /= s.charAt(i) - '0';
+            else
+                --size;
+        }
+        throw new IllegalArgumentException();
+    }
+
+
+
+
+    public static void main(String[] args) {
+        DecodedStringAtIndex880 solution = new DecodedStringAtIndex880();
+        String s = "leet2code3";
+        int k = 10;
+        String decodedChar = solution.decodeAtIndex(s, k);
+        System.out.println("Character at position " + k + ": " + decodedChar); // Output: "o"
+    }
+
+
+
+}
