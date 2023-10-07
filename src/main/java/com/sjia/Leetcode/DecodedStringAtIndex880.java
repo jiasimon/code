287d743953d6b6ba2a4c33332bdb9c838d444dd3
Simon Jia <sjia@nisum.com>
2023-10-07 08:27:10 -0700

    Leetcode #880, DecodedStringAtIndex880, N /= S.charAt(i) - '0'

--- src/main/java/com/sjia/Leetcode/DecodedStringAtIndex880.java
+++ src/main/java/com/sjia/Leetcode/DecodedStringAtIndex880.java
@@ -42,6 +42,28 @@ public class DecodedStringAtIndex880 {
 
 
 
+    // K %= N
+    public String decodeAtIndex_long(String S, int K) {
+        int i;
+        long N = 0;
+        for (i = 0; N < K; i++) {
+            N = Character.isDigit(S.charAt(i)) ? N * (S.charAt(i) - '0') : N + 1;
+        }
+        for (i--; i > 0; i--) {
+            if (Character.isDigit(S.charAt(i))) {
+                N /= S.charAt(i) - '0';
+                K %= N;
+            }
+            else {
+                if (K % N == 0) {
+                    break;
+                }
+                N--;
+            }
+        }
+        return Character.toString(S.charAt(i));
+
+    }
 
     public static void main(String[] args) {
         DecodedStringAtIndex880 solution = new DecodedStringAtIndex880();
