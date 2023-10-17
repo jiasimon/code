141e5d51e56039f1ec3d51c3843fee3cf11b54b2
Simon Jia <sjia@nisum.com>
2023-10-16 20:25:40 -0700

    Leetcode #472, ConcatenatedWords472, dp

---
+++ src/main/java/com/sjia/Leetcode/ConcatenatedWords472.java
@@ -0,0 +1,78 @@
+package com.sjia.Leetcode;
+
+import java.util.*;
+
+public class ConcatenatedWords472 {
+    // #472. Concatenated Words     https://leetcode.com/problems/concatenated-words/description/
+
+    /*
+    Given an array of strings words (without duplicates), return all the concatenated words in the given list of words.
+
+    A concatenated word is defined as a string that is comprised entirely of at least two shorter words (not necessarily distinct) in the given array.
+
+    Input: words = ["cat","cats","catsdogcats","dog","dogcatsdog","hippopotamuses","rat","ratcatdogcat"]
+    Output: ["catsdogcats","dogcatsdog","ratcatdogcat"]
+
+    Input: words = ["cat","dog","catdog"]
+    Output: ["catdog"]
+
+    1 <= words.length <= 10000
+    1 <= words[i].length <= 30
+    words[i] consists of only lowercase English letters.
+    All the strings of words are unique.
+    1 <= sum(words[i].length) <= 100000
+
+    Refer to https://leetcode.com/problems/word-break/
+     */
+
+
+    // DP,
+    // 66 ms, 44.20%; 48.4 MB, 58.86%
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+        Set<String> wordSet = new HashSet<>(Arrays.asList(words));
+        List<String> result = new LinkedList<>();
+
+        for (String word : words) {
+            wordSet.remove(word);
+            if (canFormWord(word, wordSet)) {
+                result.add(word);
+            }
+            wordSet.add(word);
+        }
+
+        return result;
+    }
+
+    private boolean canFormWord(String word, Set<String> wordSet) {
+        int n = word.length();
+        if (n == 0) {
+            return false;
+        }
+
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordSet.contains(word.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+
+
+
+    public static void main(String[] args) {
+        ConcatenatedWords472 solution = new ConcatenatedWords472();
+        String[] words = {"cat", "cats", "catsdogcats", "dog", "dogcatsdog", "hippopotamuses", "rat", "ratcatsdogcat"};
+
+        List<String> concatenatedWords = solution.findAllConcatenatedWordsInADict(words);
+        System.out.println("Concatenated Words: " + concatenatedWords);
+        // Output: ["catsdogcats","dogcatsdog","ratcatsdogcat"]
+    }
+
+}
