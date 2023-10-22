3b200be46f237848c664919c73b4946bc5c373fb
Simon Jia <sjia@nisum.com>
2023-10-22 13:21:41 -0700

    Leetcode #473, MatchsticksToSquare473,  sort matchsticks desc, dfs

--- src/main/java/com/sjia/Leetcode/MatchsticksToSquare473.java
+++ src/main/java/com/sjia/Leetcode/MatchsticksToSquare473.java
@@ -94,11 +94,45 @@ public class MatchsticksToSquare473 {
 
 
 
+
+    // sort matchsticks, dfs
+    // 125 ms, 49.85%; 39.8 MB, 87.29%
+    public boolean makesquare_dfs2(int[] matchsticks) {
+        int totalLen = Arrays.stream(matchsticks).sum();
+        if (totalLen % 4 != 0) {
+            return false;
+        }
+        Arrays.sort(matchsticks);
+        for (int i = 0, j = matchsticks.length - 1; i < j; i++, j--) {
+            int temp = matchsticks[i];
+            matchsticks[i] = matchsticks[j];
+            matchsticks[j] = temp;
+        }
+
+        int[] edges = new int[4];
+        return dfs(0, matchsticks, edges, totalLen / 4);
+    }
+
+    public boolean dfs(int index, int[] matchsticks, int[] edges, int len) {
+        if (index == matchsticks.length) {
+            return true;
+        }
+        for (int i = 0; i < edges.length; i++) {
+            edges[i] += matchsticks[index];
+            if (edges[i] <= len && dfs(index + 1, matchsticks, edges, len)) {
+                return true;
+            }
+            edges[i] -= matchsticks[index];
+        }
+        return false;
+    }
+
+
     public static void main(String[] args) {
         MatchsticksToSquare473 solution = new MatchsticksToSquare473();
         int[] matchsticks = {1, 1, 2, 2, 2};
 
-        boolean canFormSquare = solution.makesquare_dfs(matchsticks);
+        boolean canFormSquare = solution.makesquare_dfs2(matchsticks);
         System.out.println("Can Form Square: " + canFormSquare); // Output: true
     }
 }
