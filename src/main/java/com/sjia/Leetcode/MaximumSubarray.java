f819640a7406ced77c9a72603c73bd68a6f9f968
Simon Jia <sjia@nisum.com>
2023-11-19 14:05:59 -0800

    Leetcode #53, MaximumSubarray, dp, stream

--- src/main/java/com/sjia/Leetcode/MaximumSubarray.java
+++ src/main/java/com/sjia/Leetcode/MaximumSubarray.java
@@ -70,6 +70,20 @@ public class MaximumSubarray {
 
     }
 
+    // dp, stream
+    // 5 ms, 7.53%; 56.3 MB, 95.17%
+    public int maxSubArray_dp(int[] nums) {
+        // dp[i] := max sum subarray ending w/ i
+        int[] dp = new int[nums.length];
+
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.length; ++i)
+            dp[i] = Math.max(nums[i], dp[i - 1] + nums[i]);
+
+        return Arrays.stream(dp).max().getAsInt();
+
+    }
+
 
     public static void main(String[] args) {
 
