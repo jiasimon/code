0246cce76482b8114da0107111194da3ee4137e2
Simon Jia <sjia@nisum.com>
2023-11-21 06:30:20 -0800

    Leetcode #496, NextGreaterElement, hashmap, stack

--- src/main/java/com/sjia/Leetcode/NextGreaterElement.java
+++ src/main/java/com/sjia/Leetcode/NextGreaterElement.java
@@ -1,5 +1,6 @@
 package com.sjia.Leetcode;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
@@ -79,6 +80,35 @@ public class NextGreaterElement {
         }
         return res;
     }*/
+
+
+    public int[] nextGreaterElement_stack(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                map.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = map.getOrDefault(nums1[i], -1);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        NextGreaterElement solution = new NextGreaterElement();
+        int[] nums1 = {4, 1, 2};
+        int[] nums2 = {1, 3, 4, 2};
+
+        int[] nextGreater = solution.nextGreaterElement(nums1, nums2);
+        System.out.println("Next Greater Elements: " + Arrays.toString(nextGreater));
+    }
 
 
 }
