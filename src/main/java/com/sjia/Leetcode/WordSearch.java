48915ae7e13f9bddc625a6d8da60ece8d00bb68c
Simon Jia <sjia@nisum.com>
2024-04-02 19:56:36 -0700

    Leetcode #79, WordSearch, dfs, memo

--- src/main/java/com/sjia/Leetcode/WordSearch.java
+++ src/main/java/com/sjia/Leetcode/WordSearch.java
@@ -50,6 +50,53 @@ public class WordSearch {
         return found;
     }
 
+
+    public boolean exist_dfs(char[][] board, String word) {
+        if (board == null || board.length == 0 || word == null || word.length() == 0) {
+            return false;
+        }
+
+        int m = board.length;
+        int n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(board, word, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean dfs(char[][] board, String word, int i, int j, int k) {
+        int m = board.length;
+        int n = board[0].length;
+
+        if (i < 0 || j < 0 || i >= m || j >= n || board[i][j] != word.charAt(k)) {
+            return false;
+        }
+
+        if (k == word.length() - 1) {
+            return true;
+        }
+
+        char temp = board[i][j];
+        board[i][j] = '#'; // Mark as visited
+
+        boolean found = dfs(board, word, i + 1, j, k + 1) ||
+                dfs(board, word, i - 1, j, k + 1) ||
+                dfs(board, word, i, j + 1, k + 1) ||
+                dfs(board, word, i, j - 1, k + 1);
+
+        board[i][j] = temp; // Restore the original character
+        return found;
+    }
+
+
+
+
     public static void main(String[] args) {
         char[][] board = {
                 {'A', 'B', 'C', 'E'},
