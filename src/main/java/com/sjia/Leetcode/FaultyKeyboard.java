219ffb3160863c9c26ef07a50539856038032415
Simon Jia <sjia@nisum.com>
2024-04-04 07:17:26 -0700

    Leetcode #2810, FaultyKeyboard, sb.append()

--- src/main/java/com/sjia/Leetcode/FaultyKeyboard.java
+++ src/main/java/com/sjia/Leetcode/FaultyKeyboard.java
@@ -34,7 +34,20 @@ public class FaultyKeyboard {
         return res;
     }
 
-    
+    // StringBuilder.append
+    // 3ms, 98.24%
+    public String finalString_StringBuilder(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i=0; i < s.length(); i++) {
+            if(s.charAt(i) != 'i'){
+                sb.append(s.charAt(i));
+            } else {
+                sb.reverse();
+            }
+        }
+        return sb.toString();
+    }
 
 
 }
