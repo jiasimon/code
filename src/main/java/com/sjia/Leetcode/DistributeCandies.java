9a85ed86d076e859709cf1365a80cd96c19620fb
Simon Jia <sjia@nisum.com>
2024-04-08 07:11:35 -0700

    Leetcode #575, DistributeCandies, Arrays.stream(candies).distinct().count())

--- src/main/java/com/sjia/Leetcode/DistributeCandies.java
+++ src/main/java/com/sjia/Leetcode/DistributeCandies.java
@@ -1,5 +1,6 @@
 package com.sjia.Leetcode;
 
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class DistributeCandies {
@@ -8,6 +9,13 @@ public class DistributeCandies {
     // Given an integer array with even length, where different numbers in this array represent different kinds of candies.
     // You need to distribute these candies equally in number to brother and sister.
     // Return the maximum number of kinds of candies the sister could gain.
+
+    /*
+    2 <= n <= 10000
+    -10^5 <= candyType[i] <= 10^5
+
+    #interview
+     */
 
     // Runtime: 33 ms, faster than 79.45% of Java online submissions for Distribute Candies.
     //Memory Usage: 41.3 MB, less than 76.05% of Java online submissions for Distribute Candies.
@@ -22,10 +30,10 @@ public class DistributeCandies {
 
     //Runtime: 85 ms, faster than 8.61% of Java online submissions for Distribute Candies.
     //Memory Usage: 114.6 MB, less than 25.00% of Java online submissions for Distribute Candies.
-/*
-    public int distributeCandies(int[] candies) {
+    // Arrays.stream(candies).distinct().count()
+    public int distributeCandies_stream(int[] candies) {
         return Math.min(candies.length / 2, ((int) Arrays.stream(candies).distinct().count()));
-    }*/
+    }
 
 
     // HashSet, 30ms
