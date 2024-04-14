21cb770eeb3ef4755db373d7f84c408ff6030809
Simon Jia <sjia@nisum.com>
2024-04-14 13:49:55 -0700

    Leetcode #592, FractionAddition, A = A * b + a * B

--- src/main/java/com/sjia/Leetcode/FractionAddition.java
+++ src/main/java/com/sjia/Leetcode/FractionAddition.java
@@ -30,5 +30,27 @@ public class FractionAddition {
     }
 
 
-    
+    public String fractionAddition_comment(String expression) {
+        Scanner sc = new Scanner(expression).useDelimiter("/|(?=[+-])");
+        int A = 0;
+        int B = 1;
+
+        // Init: A / B = 0 / 1
+        // A / B + a / b = (Ab + aB) / Bb
+        // So, in each round, set A = Ab + aB, B = Bb.
+        while (sc.hasNext()) {
+            final int a = sc.nextInt();
+            final int b = sc.nextInt();
+            A = A * b + a * B;
+            B *= b;
+            final int g = gcd(A, B);
+            A /= g;
+            B /= g;
+        }
+
+        return A + "/" + B;
+    }
+
+
+
 }
