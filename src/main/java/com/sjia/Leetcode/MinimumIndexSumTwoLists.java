7fa70e1a63cae937bc36aa11793a11cdd97af160
Simon Jia <sjia@nisum.com>
2024-04-16 19:18:23 -0700

    Leetcode #599, MinimumIndexSumTwoLists, map

--- src/main/java/com/sjia/Leetcode/MinimumIndexSumTwoLists.java
+++ src/main/java/com/sjia/Leetcode/MinimumIndexSumTwoLists.java
@@ -1,12 +1,9 @@
 package com.sjia.Leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class MinimumIndexSumTwoLists {
-    // #599 https://leetcode.com/problems/minimum-index-sum-of-two-lists/
+    // #599. Minimum Index Sum of Two Lists https://leetcode.com/problems/minimum-index-sum-of-two-lists/
 
     // Runtime: 8 ms, faster than 72.08% of Java online submissions for Minimum Index Sum of Two Lists.
     //Memory Usage: 40 MB, less than 72.07% of Java online submissions for Minimum Index Sum of Two Lists.
@@ -36,6 +33,44 @@ public class MinimumIndexSumTwoLists {
 
     }
 
+
+
+    public String[] findRestaurant_map(String[] list1, String[] list2) {
+        Map<String, Integer> map = new HashMap<>();
+        List<String> result = new ArrayList<>();
+        int minSum = Integer.MAX_VALUE;
+
+        // Create a map to store the index of each restaurant in list1
+        for (int i = 0; i < list1.length; i++) {
+            map.put(list1[i], i);
+        }
+
+        // Iterate through list2 and check if the restaurant exists in list1
+        // If it exists, calculate the index sum and update minSum and result accordingly
+        for (int i = 0; i < list2.length; i++) {
+            if (map.containsKey(list2[i])) {
+                int indexSum = i + map.get(list2[i]);
+                if (indexSum < minSum) {
+                    minSum = indexSum;
+                    result.clear();
+                    result.add(list2[i]);
+                } else if (indexSum == minSum) {
+                    result.add(list2[i]);
+                }
+            }
+        }
+
+        return result.toArray(new String[result.size()]);
+    }
+
+    public static void main(String[] args) {
+        MinimumIndexSumTwoLists solution = new MinimumIndexSumTwoLists();
+
+        String[] list1 = {"Shogun", "Tapioca Express", "Burger King", "KFC"};
+        String[] list2 = {"KFC", "Shogun", "Burger King"};
+
+        System.out.println("Test Case 1: " + Arrays.toString(solution.findRestaurant(list1, list2))); // Output: [Shogun, Burger King]
+    }
 
 
 }
