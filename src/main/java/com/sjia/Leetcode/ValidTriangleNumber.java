2196b96b0124eb55ffce049fcfc696bb699b074c
Simon Jia <sjia@nisum.com>
2024-04-21 18:16:27 -0700

    Leetcode #611, ValidTriangleNumber, 3 for loop

--- src/main/java/com/sjia/Leetcode/ValidTriangleNumber.java
+++ src/main/java/com/sjia/Leetcode/ValidTriangleNumber.java
@@ -38,12 +38,35 @@ public class ValidTriangleNumber {
         return res;
     }
 
+
+    // 3 for loop
+    // 640ms, 18.01%; 42.84MB, 93.36%
+    public int triangleNumber_threeloop(int[] nums) {
+        Arrays.sort(nums);
+        int res = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1 && nums[i] != 0; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (nums[i] + nums[j] > nums[k]) {
+                        res++;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+
     public static void main(String[] args) {
         ValidTriangleNumber solution = new ValidTriangleNumber();
 
         // Test Case
         int[] nums = {2, 2, 3, 4};
-        System.out.println("Test Case 1: " + solution.triangleNumber(nums)); // Output: 3
+        System.out.println("Test Case 1: " + solution.triangleNumber_threeloop(nums)); // Output: 3
     }
 
 }
