0b1fb31e3b1f7c6d80951d6c3994e9dd9afaa826
Simon Jia <sjia@nisum.com>
2024-04-23 07:14:24 -0700

    Leetcode #611, ValidTriangleNumber, two pointer

--- src/main/java/com/sjia/Leetcode/ValidTriangleNumber.java
+++ src/main/java/com/sjia/Leetcode/ValidTriangleNumber.java
@@ -61,6 +61,30 @@ public class ValidTriangleNumber {
         return res;
     }
 
+
+    // two pointer
+    public int triangleNumber_twoPointer(int[] nums) {
+
+        Arrays.sort(nums);
+
+        int n = nums.length;
+        int res = 0;
+
+        for (int i = n - 1; i >= 2; --i) {
+            int l = 0, r = i - 1;
+            while (l < r) {
+                if (nums[l] + nums[r] > nums[i]) {
+                    res += r - l;
+                    --r;
+                } else {
+                    ++l;
+                }
+            }
+        }
+        return res;
+    }
+
+
     public static void main(String[] args) {
         ValidTriangleNumber solution = new ValidTriangleNumber();
 
