1df468fcbda0b7cc18ff7ac3f11b36ac1fa6c5c0
Simon Jia <sjia@nisum.com>
2024-04-29 07:08:40 -0700

    Leetcode #628, MaxProductThreeNum, onePass, no sort

--- src/main/java/com/sjia/Leetcode/MaxProductThreeNum.java
+++ src/main/java/com/sjia/Leetcode/MaxProductThreeNum.java
@@ -24,6 +24,34 @@ public class MaxProductThreeNum {
     }
 
 
+
+    // find min1 and min2; max1, max2, max3
+    // 2ms, 98.99%
+    public int maximumProduct_onepass(int[] nums) {
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE, min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        for (int n : nums) {
+            if (n > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            } else if (n > max2) {
+                max3 = max2;
+                max2 = n;
+            } else if (n > max3) {
+                max3 = n;
+            }
+            if (n < min1) {
+                min2 = min1;
+                min1 = n;
+            } else if (n < min2) {
+                min2 = n;
+            }
+        }
+        return Math.max(max1*max2*max3, max1*min1*min2);
+    }
+
+
+
     public static void main(String[] args) {
         MaxProductThreeNum solution = new MaxProductThreeNum();
 
