581d5b759a2f476256b4d6fa3340a2c5e6be1270
sijia <jiasimon@gmail.com>
2026-09-05 07:48:19 -0700

    hackerRank, MinDiffInArray,  sort then diff

---
+++ src/main/java/com/sjia/hackerRank2026/MinDiffInArray.java
@@ -0,0 +1,22 @@
+package com.sjia.hackerRank2026;
+
+import java.util.Collections;
+import java.util.List;
+
+public class MinDiffInArray {
+    // https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array/problem
+
+    public static int minimumAbsoluteDifference(List<Integer> arr) {
+        // Write your code here
+        Collections.sort(arr);
+        long res= Long.MAX_VALUE;
+        int n = arr.size();
+
+        for(int i=0; i<n-1; i++){
+            res = Math.min(res, arr.get(i+1) - arr.get(i));
+        }
+        return (int)res;
+
+    }
+    
+}
