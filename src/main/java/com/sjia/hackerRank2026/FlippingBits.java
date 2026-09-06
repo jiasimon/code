09f76c45f801738e5cd2fab6b3346778a18235fa
sijia <jiasimon@gmail.com>
2026-09-06 11:40:34 -0700

    hackerRank, FlippingBits, Integer.toUnsignedLong

--- src/main/java/com/sjia/hackerRank2026/FlippingBits.java
+++ src/main/java/com/sjia/hackerRank2026/FlippingBits.java
@@ -9,6 +9,24 @@ public class FlippingBits {
         return (~n) & 0xFFFFFFFFL;
     }
 
-    
+    public static long flippingBitsByAdd(long n) {
+        long inverted = ~(int) n; // Cast to 32-bit int and invert
+
+        if (inverted < 0) {
+            // Add 2^32 to convert the negative signed integer to an unsigned long
+            inverted += (1L << 32);
+        }
+
+        return inverted;
+    }
+
+    public static long flippingBitsToUnsignedLong(long n) {
+        int inverted = ~(int) n;
+        return Integer.toUnsignedLong(inverted);
+    }
+
+    public static long flippingBitsByMinus(long n) {
+        return 4294967295L - n;
+    }
 
 }
