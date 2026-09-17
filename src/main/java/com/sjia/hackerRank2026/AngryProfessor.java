e0255512515fcd95706c61b0b9d81aa58f0bc224
sijia <jiasimon@gmail.com>
2026-09-17 09:52:21 -0700

    hackerRank, AngryProfessor

---
+++ src/main/java/com/sjia/hackerRank2026/AngryProfessor.java
@@ -0,0 +1,18 @@
+package com.sjia.hackerRank2026;
+
+import java.util.List;
+
+public class AngryProfessor {
+    // https://www.hackerrank.com/challenges/angry-professor
+
+    public static String angryProfessor(int k, List<Integer> a) {
+        int count =0;
+        for (int num : a) {
+            if (num <=0) count++;
+        }
+        if (count >= k) return "NO";
+        else return "YES";
+    }
+
+    
+}
