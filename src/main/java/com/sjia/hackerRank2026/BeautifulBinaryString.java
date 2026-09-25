a1366b1bedc6d7e612bbfbd764cc0d05713d91bf
sijia <jiasimon@gmail.com>
2026-09-25 14:21:42 -0700

    hackerRank, BeautifulBinaryString,b[i] b[i+1], b[i+2]

---
+++ src/main/java/com/sjia/hackerRank2026/BeautifulBinaryString.java
@@ -0,0 +1,24 @@
+package com.sjia.hackerRank2026;
+
+public class BeautifulBinaryString {
+    // https://www.hackerrank.com/challenges/beautiful-binary-string/problem?isFullScreen=true
+
+    public static int beautifulBinaryString(String b) {
+        int count = 0;
+        int i = 0;
+
+        // Loop through the string looking for "010"
+        while (i <= b.length() - 3) {
+            if (b.charAt(i) == '0' && b.charAt(i + 1) == '1' && b.charAt(i + 2) == '0') {
+                count++;
+                i += 3; // Skip past the fixed "010" substring
+            } else {
+                i++;
+            }
+        }
+
+        return count;
+    }
+
+    
+}
--- src/main/java/com/sjia/hackerRank2026/FunnyString.java
+++ src/main/java/com/sjia/hackerRank2026/FunnyString.java
@@ -34,6 +34,6 @@ public class FunnyString {
         }
         return "Funny";
     }
-    
+
 
 }
